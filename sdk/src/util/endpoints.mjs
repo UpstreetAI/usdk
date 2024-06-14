bc1d9ddab31207d7abc6b165ca92d28af8a6ea07
Avaer Kazmer <a@modules.io>
2024-06-14 13:20:39 -0700

    Update endpoints.js

--- sdk/src/util/endpoints.mjs
+++ sdk/src/util/endpoints.mjs
@@ -3,3 +3,4 @@ export const aiProxyHost = `ai-proxy.isekaichat.workers.dev`;
 export const metamaskHost = 'https://metamask.upstreet.ai';
 export const multiplayerEndpointUrl = 'wss://multiplayer.isekaichat.workers.dev';
 export const deployEndpointUrl = `https://deploy.upstreet.ai`;
+export const r2EndpointUrl = `https://r2.upstreet.ai`;
