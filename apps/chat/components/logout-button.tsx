6f2c47e87a630aa8d9eb2bb58b240e4edf2897c7
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2024-06-14 18:56:13 -0700

    Add logout button

---
+++ apps/chat/components/logout-button.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { logout } from '@/lib/logout'
+import { cn } from '@/lib/utils'
+import { Button, type ButtonProps } from '@/components/ui/button'
+import { IconGitHub, IconSpinner, IconUser } from '@/components/ui/icons'
+
+interface LoginButtonProps extends ButtonProps {
+  showGithubIcon?: boolean
+  text?: string
+}
+
+export function LogoutButton({
+  className,
+  ...props
+}: LoginButtonProps) {
+  return (
+    <Button
+      variant="ghost"
+      onClick={() => {logout()}}
+      className={cn(className)}
+      {...props}
+    >
+      <IconUser className="mr-2" />
+      Logout
+    </Button>
+  )
+}
