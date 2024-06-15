be1ca2c97da9f2f4a691e5a0031144b7c1ddcb62
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-06-15 16:04:14 +0400

    fix send message on "send button" click

--- apps/chat/components/prompt-form.tsx
+++ apps/chat/components/prompt-form.tsx
@@ -85,6 +85,7 @@ export function PromptForm({
       // ref={formRef}
       onSubmit={async (e: any) => {
         e.preventDefault()
+        submitMessage()
       }}
     >
       <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
