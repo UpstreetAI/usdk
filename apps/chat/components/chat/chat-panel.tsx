8c324456831010be1551067fb67b6a880ad9fd63
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2024-06-15 13:17:29 -0700

    Hide example messages

--- apps/chat/components/chat/chat-panel.tsx
+++ apps/chat/components/chat/chat-panel.tsx
@@ -42,7 +42,7 @@ export function ChatPanel({
     location.href = `/rooms/${crypto.randomUUID()}`;
   }
 
-  const exampleMessages = [
+  /*const exampleMessages = [
     {
       heading: 'Hi there!',
       subheading: 'What is your name?',
@@ -53,7 +53,7 @@ export function ChatPanel({
       subheading: `find some friends?`,
       message: `Can you help me find some friends?`
     }
-  ]
+  ]*/
 
   return (
     <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
@@ -64,7 +64,7 @@ export function ChatPanel({
 
       <div className="mx-auto sm:max-w-2xl sm:px-4">
         {room && (<>
-          <div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
+          {/*<div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
             {messages.length === 0 &&
               exampleMessages.map((example, index) => (
                 <div
@@ -99,7 +99,7 @@ export function ChatPanel({
                   </div>
                 </div>
               ))}
-          </div>
+          </div>*/}
 
           {messages?.length >= 2 ? (
             <div className="flex h-12 items-center justify-center">
