856d1678f6720a64cd05212fa92b75a4dbd7c18b
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2024-06-14 18:57:08 -0700

    Use default JWT from cookie in Supabase client

--- apps/chat/utils/supabase/client.ts
+++ apps/chat/utils/supabase/client.ts
@@ -4,23 +4,33 @@
 import { createBrowserClient } from '@supabase/ssr';
 import { Database } from '@/types_db';
 
+
+function getCookie(key: string) {
+  return document.cookie
+    .split("; ")
+    .find((row) => row.startsWith(`${key}=`))
+    ?.split("=")[1];
+}
+
+
 // Define a function to create a Supabase client for client-side operations
-export const createClient = (jwt?: string|null) =>
-  createBrowserClient<Database>(
+export const createClient = (jwt?: string|null) => {
+  const _jwt = jwt || getCookie('auth-jwt')
+
+  return createBrowserClient<Database>(
     // Pass Supabase URL and anonymous key from the environment to the client
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: {
-
-      },
+      cookies: {},
 
       global: {
         headers: {
-          ... jwt ? {
-            Authorization: `Bearer ${jwt}`
+          ..._jwt ? {
+            Authorization: `Bearer ${_jwt}`
           } : {},
         }
       }
     }
   );
+}
