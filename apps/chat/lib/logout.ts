bc50ab96cf71e1db62f4309f6b8f6d180604a627
Avaer Kazmer <a@modules.io>
2024-06-15 00:28:34 -0700

    Fix Max-Age=0

--- apps/chat/lib/logout.ts
+++ apps/chat/lib/logout.ts
@@ -2,6 +2,6 @@ import { redirect } from 'next/navigation'
 
 
 export function logout() {
-  document.cookie = 'auth-jwt=Max-Age=0';
+  document.cookie = 'auth-jwt=; Max-Age=0';
   location.href = '/'
 }
