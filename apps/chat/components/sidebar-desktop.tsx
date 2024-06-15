553ef2697b503b3ff509f87f43b6df0bc045be02
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2024-06-14 18:57:44 -0700

    Add logout functionality to Sidebar

--- apps/chat/components/sidebar-desktop.tsx
+++ apps/chat/components/sidebar-desktop.tsx
@@ -2,18 +2,25 @@ import { Sidebar } from '@/components/sidebar'
 
 import { auth } from '@/auth'
 import { ChatHistory } from '@/components/chat-history'
+import { getUser } from '@/utils/auth-helpers/getUser'
+import { createClient } from '@/utils/supabase/server'
 
 export async function SidebarDesktop() {
-  const session = await auth()
+  const
+    client = createClient(),
+    user = await getUser(client);
+  // const session = await auth()
 
-  if (!session?.user?.id) {
+  console.log( 'ID:', user?.id)
+
+  if (!user?.id) {
     return null
   }
 
   return (
     <Sidebar className="peer absolute inset-y-0 z-30 hidden -translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:flex lg:w-[250px] xl:w-[300px]">
       {/* @ts-ignore */}
-      <ChatHistory userId={session.user.id} />
+      <ChatHistory userId={user.id} />
     </Sidebar>
   )
 }
--- apps/chat/components/sidebar.tsx
+++ apps/chat/components/sidebar.tsx
@@ -1,14 +1,37 @@
 'use client'
 
+import { getUser } from '@/utils/auth-helpers/getUser'
+import { createClient } from '@/utils/supabase/client'
+import { useEffect, useState } from 'react'
 import * as React from 'react'
 
+import { LogoutButton } from '@/components/logout-button'
 import { useSidebar } from '@/lib/hooks/use-sidebar'
 import { cn } from '@/lib/utils'
+
+
+const _getUser = async () => {
+  const client = createClient()
+  return getUser(client);
+}
+
 
 export interface SidebarProps extends React.ComponentProps<'div'> {}
 
 export function Sidebar({ className, children }: SidebarProps) {
   const { isSidebarOpen, isLoading } = useSidebar()
+  const [ user, setUser ] = useState()
+
+  let isGettingUser = false
+
+  useEffect( () => {
+    if (!isGettingUser) {
+      isGettingUser = true;
+      _getUser()
+        .then( u => setUser( u ))
+        .catch(console.error);
+    }
+  }, [])
 
   return (
     <div
@@ -16,6 +39,11 @@ export function Sidebar({ className, children }: SidebarProps) {
       className={cn(className, 'h-full flex-col dark:bg-zinc-950')}
     >
       {children}
+      {
+        user
+          ? <LogoutButton />
+          : null
+      }
     </div>
   )
 }
