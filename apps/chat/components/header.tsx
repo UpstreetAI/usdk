eb381afeb751e9925bef73499e4d085a62d04624
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-06-16 23:15:39 +0400

    update header style, show logo icon on mobile

--- apps/chat/components/header.tsx
+++ apps/chat/components/header.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import Link from 'next/link'
 
-import { IconUpstreetChat } from '@/components/ui/icons'
+import { IconUpstreet, IconUpstreetChat } from '@/components/ui/icons'
 import { UserOrLogin } from '@/components/user-or-login'
 import { SearchBar } from '@/components/searchbar'
 
@@ -10,12 +10,18 @@ import { SearchBar } from '@/components/searchbar'
 export function Header() {
   return (
     <header
-      className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+      className="sticky top-0 z-50 flex items-center justify-between h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center">
-        <Link href="/" rel="nofollow">
+        <div className='m-w-12'>
+        <Link href="/" rel="nofollow" className='hidden md:block w-44'>
           <IconUpstreetChat className="mr-2 fill-black size-9 w-auto dark:hidden" inverted/>
           <IconUpstreetChat className="hidden mr-2 fill-white size-9 w-auto dark:block"/>
         </Link>
+        <Link href="/" rel="nofollow" className='block md:hidden w-10'>
+          <IconUpstreet className="mr-2 fill-black size-9 w-auto dark:hidden" inverted/>
+          <IconUpstreet className="hidden mr-2 fill-white size-9 w-auto dark:block"/>
+        </Link>
+        </div>
       </div>
       <SearchBar/>
       <React.Suspense fallback={<div className="flex-1 overflow-auto"/>}>
