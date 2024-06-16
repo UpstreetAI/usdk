6dfe556367a06ea7e6343297f6d255b345dff537
Avaer Kazmer <a@modules.io>
2024-06-16 01:34:00 -0700

    Add agent id to agents page

--- apps/chat/components/agent-profile.tsx
+++ apps/chat/components/agent-profile.tsx
@@ -13,7 +13,8 @@ export function AgentProfile({ agent }: AgentProps) {
       className="w-full max-w-2xl mx-auto"
     >
       <h1>Agent</h1>
-      <h1 className="text-[28px] mb-6">{agent?.name}</h1>
+      <h2 className="text-[28px]">{agent?.name}</h2>
+      <h3 className="text-sm mb-6">{agent?.id}</h3>
       <Image src={resolveRelativeUrl(agent?.preview_url)} alt="Profile picture" className="s-300" width={300} height={300} />
     </div>
   )
