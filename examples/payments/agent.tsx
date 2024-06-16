03c2565a86f450cbf38086f3e1c1c088a064aa18
Avaer Kazmer <a@modules.io>
2024-06-16 01:35:51 -0700

    Add payments example

---
+++ examples/payments/agent.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  Agent,
+  AgentAppProps,
+  DefaultAgentComponents,
+  DefaultActions,
+  DefaultPrompts,
+  DefaultParsers,
+  DefaultPerceptions,
+  DefaultSchedulers,
+  DefaultServers,
+
+  Personality,
+  Perception,
+
+  useAgents,
+  useCurrentAgent,
+
+  JsonAction,
+  PendingActionEvent,
+} from 'upstreet-sdk/agents';
+
+//
+
+const PaymentRequestAction = () => {
+  return (
+    <JsonAction
+      name="paymentRequest"
+      description={`Request a payment of a specific amount. Amount is in cents.`}
+      args={{
+        handlerType: 'paymentRequest',
+        amount: '2000',
+        currency: 'USD',
+        description: 'A description of what is being sold.'
+      }}
+      // handler={async (e: PendingActionEvent) => {
+      //   // const {
+      //   //   agent,
+      //   // } = e.data;
+      // }}
+    />
+  );
+};
+const PaymentResponsePerception = () => {
+  return (
+    <Perception
+      type="paymentResponse"
+      handler={async (e) => {
+        const { agent } = e.data;
+        await agent.think('Get notified of the payment success.');
+      }}
+    />
+  );
+};
+
+export default function render(props: AgentAppProps) {
+  return (
+    <Agent>
+      <Personality>A merchant selling various magical isekai anime items.</Personality>
+      <PaymentRequestAction />
+      <PaymentResponsePerception />
+    </Agent>
+  );
+}
