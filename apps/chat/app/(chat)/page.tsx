1331a4bb12354a191ed6cb42c792f88372ff8953
Avaer Kazmer <a@modules.io>
2024-06-15 17:23:40 -0700

    Clear out index page

--- apps/chat/app/(chat)/page.tsx
+++ apps/chat/app/(chat)/page.tsx
@@ -1,18 +1,9 @@
-import { Chat } from '@/components/chat/chat'
+// import { Chat } from '@/components/chat/chat'
 // import { AI } from '@/lib/chat/actions'
-import { nanoid } from '@/lib/utils'
-import { getMissingKeys } from '@/app/actions'
-import { getUser } from '@/utils/supabase/server'
-
+// import { nanoid } from '@/lib/utils'
+// import { getMissingKeys } from '@/app/actions'
+// import { getUser } from '@/utils/supabase/server'
 
 export default async function IndexPage() {
-  const id = nanoid()
-  const user = await getUser()
-  const missingKeys = await getMissingKeys()
-
-  return (
-    // <AI initialAIState={{ chatId: id, messages: [] }}>
-      <Chat id={id} user={user} missingKeys={missingKeys} />
-    // </AI>
-  )
+  return null;
 }
