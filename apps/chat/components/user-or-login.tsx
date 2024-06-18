789c605bc4c935df9c29196137be6ff038c7106a
Avaer Kazmer <a@modules.io>
2024-06-18 13:49:21 -0700

    Update user load

--- apps/chat/components/user-or-login.tsx
+++ apps/chat/components/user-or-login.tsx
@@ -5,11 +5,19 @@ import { HeaderLoginButton } from '@/components/header-login-button'
 import { SidebarMobile } from '@/components/sidebar-mobile'
 import { SidebarToggle } from '@/components/sidebar-toggle'
 // import { Session } from '@/lib/types'
-import { getUser } from '@/utils/supabase/server'
+import { getJWT, getUser } from '@/utils/supabase/server'
 
 
 export async function UserOrLogin() {
-  const user = await getUser();
+  const user = await (async () => {
+    const jwt = getJWT();
+    if (jwt) {
+      const user = await getUser(jwt);
+      return user;
+    } else {
+      return null;
+    }
+  })();
 
   return (
     <>
