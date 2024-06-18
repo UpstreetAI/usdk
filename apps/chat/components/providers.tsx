8b87623f0b2f878cfb9fdd45e61c26ba336ede66
Avaer Kazmer <a@modules.io>
2024-06-18 13:47:25 -0700

    Hook in supabase provider

--- apps/chat/components/providers.tsx
+++ apps/chat/components/providers.tsx
@@ -4,6 +4,7 @@ import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ThemeProviderProps } from 'next-themes/dist/types'
 import { SidebarProvider } from '@/lib/hooks/use-sidebar'
+import { SupabaseProvider } from '@/lib/hooks/use-supabase'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { ActionsProvider } from '@/components/ui/actions'
 import { MultiplayerActionsProvider } from '@/components/ui/multiplayer-actions'
@@ -12,15 +13,17 @@ import { MultiplayerActionsProvider } from '@/components/ui/multiplayer-actions'
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
-      <SidebarProvider>
-        <TooltipProvider>
-          <ActionsProvider>
-            <MultiplayerActionsProvider>
-              {children}
-            </MultiplayerActionsProvider>
-          </ActionsProvider>
-        </TooltipProvider>
-      </SidebarProvider>
+      <SupabaseProvider>
+        <SidebarProvider>
+          <TooltipProvider>
+            <ActionsProvider>
+              <MultiplayerActionsProvider>
+                {children}
+              </MultiplayerActionsProvider>
+            </ActionsProvider>
+          </TooltipProvider>
+        </SidebarProvider>
+      </SupabaseProvider>
     </NextThemesProvider>
   )
 }
