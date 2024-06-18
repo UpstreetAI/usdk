9c66aef97145ca967b52b409f83ccd1d08f0e840
Avaer Kazmer <a@modules.io>
2024-06-18 13:48:47 -0700

    Update jwt.ts

--- apps/chat/lib/jwt.ts
+++ apps/chat/lib/jwt.ts
@@ -1,6 +1,5 @@
 const cookieName = 'auth-jwt'
 
-
 export async function getJWT() {
   if (typeof window === 'undefined') { // Server
     const { cookies } = await import('next/headers')
@@ -11,5 +10,23 @@ export async function getJWT() {
     const cookie = await import('cookie')
 
     return cookie.parse(document.cookie)[cookieName]
+  }
+}
+
+const maxAgeMax = 365 * 24 * 60 * 60; // 1 year
+export async function setJWT(jwt: string) {
+  if (typeof window === 'undefined') { // Server
+    const { cookies } = await import('next/headers')
+    const cookieStore = cookies()
+
+    cookieStore.set(cookieName, jwt, {
+      maxAge: maxAgeMax,
+    });
+  } else { // Client
+    const cookie = await import('cookie')
+
+    document.cookie = cookie.serialize(cookieName, jwt, {
+      maxAge: maxAgeMax,
+    })
   }
 }
