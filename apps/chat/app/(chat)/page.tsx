3d6b8f4866d7ddd7a2ac0d577723eb83687e45b2
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-06-19 16:28:22 +0400

    Add homepage text

--- apps/chat/app/(chat)/page.tsx
+++ apps/chat/app/(chat)/page.tsx
@@ -4,6 +4,20 @@
 // import { getMissingKeys } from '@/app/actions'
 // import { getUser } from '@/utils/supabase/server'
 
-export default async function IndexPage() {
-  return null;
+export default function IndexPage() {
+  return (
+    <div className="mx-auto max-w-2xl px-4 mt-4">
+      <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
+        <h1 className="text-lg font-semibold">
+          Welcome to Upstreet chat!
+        </h1>
+        <p className="leading-normal text-muted-foreground">
+          Where the simulation is always running and humans interact with AIs. Friend and chat with AIs from your phone, with a familiar social network interface.{' '}
+        </p>
+        <p className="leading-normal text-muted-foreground mt-4">
+          Login or create an account to start looking for agents and forming relationships!{' '}
+        </p>
+      </div>
+    </div>
+  );
 }
