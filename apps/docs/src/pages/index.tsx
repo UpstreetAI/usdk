b9465e44233c7d3d6ae7965e26ed2e867f63dc3e
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-06-20 18:13:16 +0400

    Update docs homepage and styles, add assets

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -10,14 +10,14 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx(styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
-        <h2>Available Docs</h2>
+        <h2 style={{ marginTop: "64px" }}>Available Docs:</h2>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
