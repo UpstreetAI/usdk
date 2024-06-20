efc969bc80b50a82a70f8ef9054a26eee6230cf9
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-06-20 18:51:16 +0400

    Docs / Fix build

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/sdk/intro">
+            to="/docs/sdk">
             SDK Documentation
           </Link>
         </div>
