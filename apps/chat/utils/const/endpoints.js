f0c7f293e3f6ed7f9e671fe498660760668fed21
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2024-06-21 03:45:46 -0500

    Add r2 endpoint

--- apps/chat/utils/const/endpoints.js
+++ apps/chat/utils/const/endpoints.js
@@ -1,5 +1,6 @@
 export const aiHost = `https://ai.upstreet.ai`;
 export const aiProxyHost = `ai-proxy.isekaichat.workers.dev`;
+export const deployEndpointUrl = `https://deploy.upstreet.ai`;
 export const metamaskHost = 'https://metamask.upstreet.ai';
 export const multiplayerEndpointUrl = 'wss://multiplayer.isekaichat.workers.dev';
-export const deployEndpointUrl = `https://deploy.upstreet.ai`;
+export const r2Endpoint = `https://r2.upstreet.ai`;
