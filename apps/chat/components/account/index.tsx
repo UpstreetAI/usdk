57cb719c007cd9031f12db6326be4c243c5bee8f
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2024-06-21 03:48:46 -0500

    Add account components

---
+++ apps/chat/components/account/index.tsx
@@ -0,0 +1,52 @@
+import { Bio } from '@/components/account/bio'
+import { Name } from '@/components/account/name'
+import { ProfileImage } from '@/components/account/profile-image'
+import { EditableText } from '@/components/editable-text'
+import { env } from '@/lib/env'
+import { makeAnonymousClient } from '@/utils/supabase/supabase-client'
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { IconEdit } from '@/components/ui/icons'
+import { redirect } from 'next/navigation'
+import { resolveRelativeUrl } from '@/lib/utils'
+import { routes } from '@/routes'
+import { getUserAccount, waitForUser } from '@/utils/supabase/server'
+
+
+export interface AccountProps {
+  params: {
+    id: string
+  }
+}
+
+export async function Account({ params: { id }}: AccountProps) {
+  const currentUser = await waitForUser()
+
+  let user
+  let userIsCurrentUser = false
+
+  // Display user for given ID if provided, else get current user.
+  if (id) {
+    user = await getUserAccount(id, 'name,id,playerSpec,preview_url')
+
+    // Redirect if a user isn't found, otherwise check if it's the
+    // logged-in user.
+    if (!user) redirect( routes.home )
+    userIsCurrentUser = user?.id === currentUser.id
+  } else {
+    user = currentUser
+    userIsCurrentUser = true
+  }
+
+  return (
+    <div className="flex flex-col flex-nowrap p-4">
+      <ProfileImage user={user} userIsCurrentUser={userIsCurrentUser} />
+      <Name user={user} userIsCurrentUser={userIsCurrentUser} />
+      <Bio user={user} userIsCurrentUser={userIsCurrentUser} />
+
+      {/*<div className="whitespace-pre-wrap">
+        {JSON.stringify( user, null, ' ' )}
+      </div>*/}
+    </div>
+  );
+}
---
+++ apps/chat/components/account/profile-image.tsx
@@ -0,0 +1,106 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { IconEdit } from '@/components/ui/icons'
+import { r2Endpoint } from '@/utils/const/endpoints'
+import React from 'react'
+import Image from 'next/image'
+import { useSupabase } from '@/lib/hooks/use-supabase'
+import { resolveRelativeUrl } from '@/lib/utils'
+
+
+export interface ProfileImageProps {
+  user: any
+  userIsCurrentUser: boolean
+}
+
+export function ProfileImage({user, userIsCurrentUser}: ProfileImageProps) {
+  const { supabase } = useSupabase()
+
+  return (
+    <div className="m-auto mb-3 relative">
+      <Image
+        alt=""
+        className="border-2 rounded-3xl drop-shadow-xl"
+        src={resolveRelativeUrl( user.preview_url )}
+        height={100}
+        width={100}
+      />
+      {userIsCurrentUser ? (
+        <Button
+          className="absolute top-0 right-0 p-2 h-fit"
+          onClick={async () => {
+            // Get image from user.
+            const file = await getFile()
+
+            if (file) {
+              const fileExtensionRegex = /(?:\.([^.]+))?$/
+              const extension = fileExtensionRegex.exec(file.name)?.[1]
+
+              if (extension) {
+                const imageID = crypto.randomUUID()
+
+                const url = `${r2Endpoint}/${user.id}/${imageID}.${extension}`
+
+                try {
+                  const res = await fetch(url, {
+                    body: file,
+                    method: 'PUT',
+                  })
+                } catch(e) {
+                  if (e) {
+                    if (e.message! !== 'NetworkError when attempting to fetch resource.') {
+                      throw new Error(e.message!)
+                    }
+                  }
+                }
+
+                // Set the user's preview url to the new URL.
+                await supabase
+                  .from('accounts')
+                  .update({preview_url: url})
+                  .eq('id', user.id)
+
+                location.reload()
+              } else {
+                console.error( 'Missing file extension.' )
+              }
+            }
+          }}
+          variant="ghost"
+        >
+          <IconEdit />
+        </Button>
+        ): null}
+    </div>
+  )
+}
+
+
+export async function getFile(): Promise<File|null> {
+  return new Promise((resolve, reject) => {
+    const inputElemenet = document.createElement('input');
+    inputElemenet.accept='.jpeg,.jpg,.png,.webp'
+    inputElemenet.style.display = 'none';
+    inputElemenet.type = 'file';
+
+    inputElemenet.addEventListener('change', () => {
+      if (inputElemenet.files) {
+        resolve(inputElemenet.files[0]);
+      } else {
+        resolve(null)
+      }
+    });
+
+    const teardown = () => {
+      document.body.removeEventListener('focus', teardown, true);
+      setTimeout(() => {
+        document.body.removeChild(inputElemenet);
+      }, 1000);
+    }
+    document.body.addEventListener('focus', teardown, true);
+
+    document.body.appendChild(inputElemenet);
+    inputElemenet.click();
+  })
+}
