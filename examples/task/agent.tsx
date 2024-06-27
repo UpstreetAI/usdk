230948f7f3cfb24c81ef347f7f51843121d5a052
Avaer Kazmer <a@modules.io>
2024-06-27 10:17:54 +0700

    Add task example

---
+++ examples/task/agent.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  Agent,
+  AgentAppProps,
+  DefaultAgentComponents,
+  DefaultActions,
+  DefaultPrompts,
+  DefaultParsers,
+  DefaultPerceptions,
+  DefaultSchedulers,
+  DefaultServers,
+  Task,
+  TaskResult,
+  // WebBrowser,
+} from 'react-agents';
+
+//
+
+export default function render(props: AgentAppProps) {
+  return (
+    <Agent>
+      <Task
+        id="autonomous"
+        handler={async (e) => {
+          // console.log('autonomous task 1');
+          await e.data.agent.think();
+          // console.log('autonomous task 2');
+          return new TaskResult(TaskResult.SCHEDULE, {
+            timestamp: new Date(Date.now() + 2000),
+          });
+        }}
+      />
+    </Agent>
+  );
+}
