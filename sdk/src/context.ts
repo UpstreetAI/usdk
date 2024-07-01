62e2b2f02f728ed6c3ff4caf71b24fb2dfba05bf
Avaer Kazmer <a@modules.io>
2024-07-01 16:18:40 +0700

    Add missing configuration context

--- sdk/src/context.ts
+++ sdk/src/context.ts
@@ -1,7 +1,12 @@
 import { createContext } from 'react';
 import {
   AppContextValue,
+  ConfigurationContextValue,
 } from './types';
 
 export const AppContext = createContext<AppContextValue | null>(null);
 export const EpochContext = createContext<number>(0);
+export const ConfigurationContext = createContext<ConfigurationContextValue>({
+  get: (key: string) => null,
+  set: (key: string, value: any) => {},
+});
