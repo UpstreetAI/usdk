982f8dec8b108f3f65ee015e9f8256df2b9327f3
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-07-02 16:09:17 +0400

    Add isValidUrl helper

--- apps/chat/lib/utils.ts
+++ apps/chat/lib/utils.ts
@@ -104,3 +104,13 @@ export function getAgentPreviewImageUrl(agentObject: any) {
 export function getAgentEndpointUrl(agentObject: any) {
   return `https://user-agent-${agentObject.id}.isekaichat.workers.dev/`;
 }
+
+export function isValidUrl(urlString: string) {
+  const urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+  '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+return !!urlPattern.test(urlString);
+}
