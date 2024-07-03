c9a291758b0f8a5d0691f2df844599e0a11f6eb5
Avaer Kazmer <a@modules.io>
2024-07-03 16:35:40 +0700

    Update save messages to database

--- sdk/src/util/saveMessageToDatabase.js
+++ sdk/src/util/saveMessageToDatabase.js
@@ -1,21 +1,31 @@
-export async function saveMessageToDatabase(supabaseClient, playerID, message) {
-  const res = await supabaseClient
-    .from('agent_messages')
-    .insert(transformMessage(message, playerID));
+import {
+  lembed,
+} from './embedding.mjs';
 
-  if ( res.error ) {
-    throw new Error(`${res.error.code} ${res.error.message}`);
+export async function saveMessageToDatabase(supabaseClient, jwt, playerID, message) {
+  const encodedMessage = await encodeMessage(message, jwt, playerID);
+  const { error } = await supabaseClient
+    .from('agent_messages')
+    .insert(encodedMessage);
+  if (!error) {
+    // nothing
+  } else {
+    throw new Error(`${error.code} ${error.message}`);
   }
 }
 
-
-function transformMessage(message, playerID) {
-  return {
-    user_id: playerID,
+async function encodeMessage(message, jwt, playerID) {
+  const embedding = await lembed(JSON.stringify({
     method: message.method,
+    args: message.args,
+  }), { overridenJwt: jwt });
+  return {
+    method: message.method,
+    args: message.args,
+    text: message.args.text,
+    user_id: playerID,
     src_user_id: message.userId,
     src_name: message.name,
-    text: message.args.text,
-    args: message.args,
+    embedding,
   };
 }
