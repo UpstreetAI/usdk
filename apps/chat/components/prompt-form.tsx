7bfdaae19a5522508fc26452c7b8c5d18d057538
Avaer Kazmer <a@modules.io>
2024-07-03 19:13:30 +0700

    Hook in new media input ui

--- apps/chat/components/prompt-form.tsx
+++ apps/chat/components/prompt-form.tsx
@@ -4,14 +4,15 @@ import * as React from 'react'
 import Textarea from 'react-textarea-autosize'
 
 import { Button } from '@/components/ui/button'
-import { IconPlus, IconUpstreet } from '@/components/ui/icons'
+import { IconPlus, IconImage, IconAudio, IconCapture, IconUpstreet } from '@/components/ui/icons'
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger
 } from '@/components/ui/tooltip'
 import { useMultiplayerActions } from '@/components/ui/multiplayer-actions'
-import { newChat } from '@/lib/chat/actions'
+// import { newChat } from '@/lib/chat/actions'
+import { cn } from '@/lib/utils'
 
 export function PromptForm({
   input,
@@ -20,10 +21,13 @@ export function PromptForm({
   input: string
   setInput: (value: string) => void
 }) {
-
+  const [mediaPickerOpen, setMediaPickerOpen] = React.useState(false);
   const inputRef = React.useRef<HTMLTextAreaElement>(null)
-
   const { sendChatMessage } = useMultiplayerActions()
+
+  const toggleMediaPicker = () => {
+    setMediaPickerOpen(open => !open)
+  };
 
   const submitMessage = () => {
     const value = input.trim()
@@ -71,22 +75,62 @@ export function PromptForm({
         submitMessage()
       }}
     >
-      <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
+      <div className="relative flex max-h-60 w-full grow flex-col bg-background px-8 sm:rounded-md sm:border sm:px-12">
+        {mediaPickerOpen && (
+          <div className="absolute left-0 bottom-16 py-2 flex flex-col bg-background border rounded">
+            <div className="mx-4 my-2 text-xs text-muted-foreground">Add media...</div>
+            <Button
+              variant="secondary"
+              className="flex justify-start relative rounded bg-background mx-2 p-2 overflow-hidden"
+            >
+              <input className="absolute -top-12 bottom-0 -left-12 right-0 cursor-pointer" type="file" onChange={e => {
+                const files = Array.from((e.target as any).files);
+                const file = files[0];
+                console.log('image file', file);
+
+                toggleMediaPicker();
+                (e.target as any).value = null;
+              }} />
+              <IconImage className="mr-2" />
+              <div>Image</div>
+            </Button>
+            {/* <Button
+              variant="secondary"
+              className="flex justify-start relative rounded bg-background mx-2 p-2 overflow-hidden"
+              onClick={() => {
+                console.log('click audio');
+              }}
+            >
+              <IconAudio className="mr-2" />
+              <div>Audio</div>
+            </Button> */}
+            {/* <Button
+              variant="secondary"
+              className="flex justify-start relative rounded bg-background mx-2 p-2 overflow-hidden"
+              onClick={() => {
+                console.log('click capture');
+              }}
+            >
+              <IconCapture className="mr-2" />
+              <div>Capture</div>
+            </Button> */}
+          </div>
+        )}
         <Tooltip>
           <TooltipTrigger asChild>
             <Button
               variant="outline"
               size="icon"
-              className={`absolute left-0 md:left-4 top-[14px] size-8 rounded-full bg-background p-0`}
+              className={cn(`absolute left-0 md:left-4 top-[14px] size-8 rounded-full bg-background p-0`, mediaPickerOpen && `bg-secondary/80`)}
               onClick={() => {
-                newChat();
+                toggleMediaPicker();
               }}
             >
               <IconPlus />
-              <span className="sr-only">New Chat</span>
+              <span className="sr-only">Add Media</span>
             </Button>
           </TooltipTrigger>
-          <TooltipContent>New Chat</TooltipContent>
+          <TooltipContent>Add Media</TooltipContent>
         </Tooltip>
         <Textarea
           ref={inputRef}
