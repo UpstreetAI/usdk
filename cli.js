21894d2acfba5a21940bf66cc2321b09ff8bd8fd
Avaer Kazmer <a@modules.io>
2024-07-03 13:36:20 +0700

    Add logs command

--- cli.js
+++ cli.js
@@ -1398,14 +1398,7 @@ const getGuidFromPath = async (p) => {
   const wranglerToml = toml.parse(wranglerTomString);
   return wranglerToml.vars.GUID;
 };
-const chat = async (args) => {
-  // console.log('got chat args', JSON.stringify(args));
-  let guidsOrDevPathIndexes = args._[0] ?? [];
-  const dev = !!args.dev;
-  const room = args.room ?? makeRoomName();
-  const debug = !!args.debug;
-
-  // ensure guids
+const normalizeGuidInputs = async (guidsOrDevPathIndexes, { dev }) => {
   if (guidsOrDevPathIndexes.length === 0) {
     if (!dev) {
       const guid = await getGuidFromPath(cwd);
@@ -1428,6 +1421,17 @@ const chat = async (args) => {
       }));
     }
   }
+  return guidsOrDevPathIndexes;
+};
+const chat = async (args) => {
+  // console.log('got chat args', JSON.stringify(args));
+  let guidsOrDevPathIndexes = args._[0] ?? [];
+  const dev = !!args.dev;
+  const room = args.room ?? makeRoomName();
+  const debug = !!args.debug;
+
+  // ensure guids
+  guidsOrDevPathIndexes = await normalizeGuidInputs(guidsOrDevPathIndexes, { dev });
 
   const jwt = await getLoginJwt();
   if (jwt !== null) {
@@ -1553,6 +1557,49 @@ const simulate = async (args) => {
     close: () => {
       for (const ws of webSockets) {
         ws.close();
+      }
+    },
+  };
+};
+const logs = async (args) => {
+  let guidsOrDevPathIndexes = args._[0] ?? [];
+
+  guidsOrDevPathIndexes = await normalizeGuidInputs(guidsOrDevPathIndexes, {
+    dev: false,
+  });
+
+  const eventSources = guidsOrDevPathIndexes.map((guidOrDevPathIndex) => {
+    // const agentHost = getAgentHost(
+    //   !dev ? guidOrDevPathIndex : guidOrDevPathIndex.portIndex,
+    //   {
+    //     dev,
+    //   },
+    // );
+
+    const u = `${deployEndpointUrl}/agents/${guidOrDevPathIndex}/logs`;
+    // console.log('got u', u);
+    const eventSource = new EventSource(u);
+    eventSource.addEventListener('message', (e) => {
+      const j = JSON.parse(e.data);
+      if (typeof j === 'string') {
+        // console.log(JSON.stringify(j));
+        process.stdout.write(j);
+      } else {
+        console.log(j);
+      }
+    });
+    eventSource.addEventListener('error', (e) => {
+      console.warn('error', e);
+    });
+    eventSource.addEventListener('close', (e) => {
+      process.exit(0);
+    });
+  });
+
+  return {
+    close: () => {
+      for (const eventSource of eventSources) {
+        eventSource.close();
       }
     },
   };
@@ -3555,8 +3602,27 @@ const main = async () => {
       });
     });
   program
+    .command('logs')
+    .description(`Stream an agent's logs`)
+    .argument(`[guids...]`, `The guids of the agents to listen to`)
+    // .option(
+    //   `-d, --dev`,
+    //   `Chat with a local development agent`,
+    // )
+    .action(async (guids = [], opts = {}) => {
+      await handleError(async () => {
+        commandExecuted = true;
+        let args;
+        args = {
+          _: [guids],
+          ...opts,
+        };
+        await logs(args);
+      });
+    });
+  program
     .command('listen')
-    .description(`Stream an agent's events`)
+    .description(`Stream an agent's action events`)
     .argument(`[guids...]`, `The guids of the agents to listen to`)
     // .option(
     //   `-d, --dev`,
