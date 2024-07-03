137a67333fba43aa2ab8cfe570b2f265d2659a8e
Avaer Kazmer <a@modules.io>
2024-07-03 16:40:20 +0700

    Update load messages from database

--- sdk/src/util/loadMessagesFromDatabase.js
+++ sdk/src/util/loadMessagesFromDatabase.js
@@ -1,8 +1,14 @@
 export async function loadMessagesFromDatabase(supabaseClient, limit) {
   const { error, data } = await supabaseClient
     .from( 'agent_messages' )
-    .select( '*' )
-    .order( 'created_at', { ascending: false })
+    .select([
+      'method',
+      'args',
+      'src_user_id',
+      'src_name',
+      'created_at',
+    ].join(','))
+    .order( 'created_at', { ascending: true })
     .limit( limit )
   if (!error) {
     return decodeMessage(data);
@@ -14,9 +20,9 @@ export async function loadMessagesFromDatabase(supabaseClient, limit) {
 function decodeMessage(messages) {
   return messages.map( message => ({
     method: message.method,
+    args: message.args,
     userId: message.src_user_id,
     name: message.src_name,
     timestamp: message.created_at,
-    args: message.args,
   }));
 }
