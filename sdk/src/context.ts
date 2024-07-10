0a8cad2a5418106db771bec6f8bb503799be9df4
Avaer Kazmer <a@modules.io>
2024-07-10 21:05:04 +0700

    Add agent context

--- sdk/src/context.ts
+++ sdk/src/context.ts
@@ -1,11 +1,14 @@
 import { createContext } from 'react';
 import {
   AppContextValue,
+  // AgentContextValue,
+  ActiveAgentObject,
   ConfigurationContextValue,
 } from './types';
 
 export const AppContext = createContext<AppContextValue | null>(null);
 export const EpochContext = createContext<number>(0);
+export const AgentContext = createContext<ActiveAgentObject | null>(null);
 export const ConfigurationContext = createContext<ConfigurationContextValue>({
   get: (key: string) => null,
   set: (key: string, value: any) => {},
