a812b11978b44dcb488b3e4aa660749f8f64288e
Avaer Kazmer <a@modules.io>
2024-07-11 19:49:43 +0700

    Update context.ts

--- sdk/src/context.ts
+++ sdk/src/context.ts
@@ -3,12 +3,14 @@ import {
   AppContextValue,
   // AgentContextValue,
   ActiveAgentObject,
+  Conversation,
   ConfigurationContextValue,
 } from './types';
 
 export const AppContext = createContext<AppContextValue | null>(null);
 export const EpochContext = createContext<number>(0);
 export const AgentContext = createContext<ActiveAgentObject | null>(null);
+export const ConversationContext = createContext<Conversation | null>(null);
 export const ConfigurationContext = createContext<ConfigurationContextValue>({
   get: (key: string) => null,
   set: (key: string, value: any) => {},
