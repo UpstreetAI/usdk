7a7a67409cb1df5fa91cabea13e2f3b33a63c2e5
Avaer Kazmer <a@modules.io>
2024-07-12 10:33:12 +0700

    Add new conversations context

--- sdk/src/context.ts
+++ sdk/src/context.ts
@@ -11,6 +11,7 @@ export const AppContext = createContext<AppContextValue | null>(null);
 export const EpochContext = createContext<number>(0);
 export const AgentContext = createContext<ActiveAgentObject | null>(null);
 export const ConversationContext = createContext<Conversation | null>(null);
+export const ConversationsContext = createContext<Conversation[]>([]);
 export const ConfigurationContext = createContext<ConfigurationContextValue>({
   get: (key: string) => null,
   set: (key: string, value: any) => {},
