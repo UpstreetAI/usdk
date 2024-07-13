c538b81b59ef8ce8daaa06ddbf2cb01675ee430f
Avaer Kazmer <a@modules.io>
2024-07-13 16:26:51 +0700

    Break out render registry

---
+++ sdk/src/classes/render-registry.ts
@@ -0,0 +1,101 @@
+import type {
+  ActiveAgentObject,
+  AgentProps,
+  ActionProps,
+  PromptProps,
+  FormatterProps,
+  ParserProps,
+  PerceptionProps,
+  TaskProps,
+  NameProps,
+  PersonalityProps,
+  ServerProps,
+  // InstanceChild,
+} from '../types';
+
+//
+
+export class Instance {
+  type: string;
+  props: any;
+  children: Array<Instance | string>;
+  constructor(
+    type: string = '',
+    props: any = {},
+    children: Array<Instance | string> = [],
+  ) {
+    this.type = type;
+    this.props = props;
+    this.children = children;
+  }
+  recurse(fn: (instance: Instance) => void) {
+    fn(this);
+    for (const child of this.children) {
+      if (child instanceof Instance) {
+        child.recurse(fn);
+      }
+    }
+  }
+}
+export class AgentRegistry {
+  value: ActiveAgentObject;
+  
+  actions: ActionProps[] = [];
+  prompts: PromptProps[] = [];
+  formatters: FormatterProps[] = [];
+  parsers: ParserProps[] = [];
+  perceptions: PerceptionProps[] = [];
+  tasks: TaskProps[] = [];
+  
+  names: NameProps[] = [];
+  personalities: PersonalityProps[] = [];
+  
+  servers: ServerProps[] = [];
+
+  constructor(value: ActiveAgentObject) {
+    this.value = value;
+  }
+}
+export class RenderRegistry {
+  agents: Map<ActiveAgentObject, AgentRegistry> = new Map();
+  load(container: Instance) {
+    this.agents.clear();
+    container.recurse((instance) => {
+      if (instance.type === 'agent') {
+        const agent = instance.props.value as ActiveAgentObject;
+        const agentRegistry = new AgentRegistry(agent);
+        this.agents.set(agent, agentRegistry);
+
+        instance.recurse((childInstance) => {
+          if (childInstance.type === 'action') {
+            agentRegistry.actions.push(childInstance.props.value);
+          }
+          if (childInstance.type === 'prompt') {
+            agentRegistry.prompts.push(childInstance.props.value);
+          }
+          if (childInstance.type === 'formatter') {
+            agentRegistry.formatters.push(childInstance.props.value);
+          }
+          if (childInstance.type === 'parser') {
+            agentRegistry.parsers.push(childInstance.props.value);
+          }
+          if (childInstance.type === 'perception') {
+            agentRegistry.perceptions.push(childInstance.props.value);
+          }
+          if (childInstance.type === 'task') {
+            agentRegistry.tasks.push(childInstance.props.value);
+          }
+          if (childInstance.type === 'name') {
+            agentRegistry.names.push(childInstance.props.value);
+          }
+          if (childInstance.type === 'personality') {
+            agentRegistry.personalities.push(childInstance.props.value);
+          }
+          if (childInstance.type === 'server') {
+            agentRegistry.servers.push(childInstance.props.value);
+          }
+        });
+      }
+    });
+  }
+}
