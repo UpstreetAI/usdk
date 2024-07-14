9f0a6a543aa1e1ad0fcbe87cc4d9551adfe0dd57
Avaer Kazmer <a@modules.io>
2024-07-14 16:06:20 +0700

    Optimize render loader

--- sdk/src/classes/render-loader.tsx
+++ sdk/src/classes/render-loader.tsx
@@ -8,23 +8,42 @@ import {
 export class RenderLoader extends EventTarget {
   private userLoadPromises: Array<Promise<any>> = [];
   useLoad(p: Promise<any>) {
+    // console.log('use load 1');
+    // (p as any).error = new Error();
     this.userLoadPromises.push(p);
-    this.dispatchEvent(new Event('loadadd'));
+    p.finally(() => {
+      const index = this.userLoadPromises.indexOf(p);
+      this.userLoadPromises.splice(index, 1);
+      // console.log('user load promise resolve', this.userLoadPromises.map((p) => (p as any).error.stack));
+    });
+    // console.log('use load 2:', this.userLoadPromises.length);
+    this.dispatchEvent(new MessageEvent('loadadd', {
+      data: null,
+    }));
+    // console.log('use load 3');
   }
   async waitForLoad() {
+    // console.log('wait for load 1');
     if (this.userLoadPromises.length === 0) {
+      // console.log('wait for load 2');
       await new Promise((accept) => {
+        // console.log('wait for load 3');
         this.addEventListener('loadadd', () => {
           accept(null);
         }, {
           once: true,
         });
+        // console.log('wait for load 4');
       });
     }
+    // console.log('wait for load 5:', this.userLoadPromises.map((p) => (p as any).error.stack));
     await Promise.all(this.userLoadPromises);
+    // console.log('wait for load 6');
   }
   clear() {
+    // console.log('clear 1');
     this.userLoadPromises.length = 0;
+    // console.log('clear 2');
   }
 }
 
@@ -51,12 +70,6 @@ export const RenderLoaderProvider = ({
   renderLoader: RenderLoader;
   children?: ReactNode;
 }) => {
-  const p = makePromise();
-  renderLoader.useLoad(p);
-  useEffect(() => {
-    p.resolve(null);
-  });
-
   return (
     <Suspense fallback={
       <RenderLoaderFallback
