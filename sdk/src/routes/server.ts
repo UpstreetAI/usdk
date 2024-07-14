3c20e2aa958be18b0f0d80845e7fd5954454aa30
Avaer Kazmer <a@modules.io>
2024-07-14 11:58:52 +0700

    Update server binding

--- sdk/src/routes/server.ts
+++ sdk/src/routes/server.ts
@@ -1,6 +1,6 @@
 import 'localstorage-polyfill';
 import { AgentRenderer, compileUserAgentServer } from '../runtime';
-import type { AgentObject, ActionMessages, UserHandler, AgentConsole } from '../types';
+import type { AgentObject, ActionMessages, UserHandler, AgentConsole, ActiveAgentObject } from '../types';
 // import { makeAnonymousClient } from '../util/supabase-client.mjs';
 import { getConnectedWalletsFromMnemonic } from '../util/ethereum-utils.mjs';
 import { ConversationContext } from '../classes/conversation-context.mjs';
@@ -10,65 +10,23 @@ import { ConversationContext } from '../classes/conversation-context.mjs';
 
 export default async function handler(
   request: Request,
-  agentRegistry: AgentRenderer,
+  agent: ActiveAgentObject,
 ) {
   // console.log('nudge handler', { enabled });
   // if (typeof enabled !== 'boolean') {
   //   throw new Error('serverHandler: enabled must be a boolean');
   // }
 
-  const prepareUserArgs = async (
-    conversationContext: ConversationContext,
-    env: object,
-    userRender: UserHandler,
-    // enabled: boolean,
-  ) => {
-    // const currentAgentId = (args as any).currentAgentId as string;
-    // const agentIds = (args as any).agentIds as string;
-
-    // const scene = conversationContext.getScene();
-    // const currentAgent = conversationContext.getCurrentAgent();
-    // const agents = conversationContext.getAgents().concat([currentAgent]);
-    // const messages = conversationContext.getMessages();
-
-    // console.log('server prepare user args', {
-    //   // args,
-    //   // argsJson: JSON.stringify(args),
-    //   env,
-    //   agents,
-    //   messages,
-    // });
- 
-    // const host = 'http://localhost';
-    // const proxyRes2 = await getStaticAsset(
-    //   new Request(`${host}/agent.npc`),
-    //   env,
-    // );
-
-    const mnemonic = env.WALLET_MNEMONIC;
-    const wallets = getConnectedWalletsFromMnemonic(mnemonic);
-
-    return {
-      env,
-      console,
-      userRender,
-      conversationContext,
-      // scene,
-      // agents,
-      // currentAgent,
-      // messages,
-      wallets,
-      // enabled,
-    };
-  };
   const handleServer = async () => {
-    const userArgs = await prepareUserArgs(
-      conversationContext,
-      env,
-      userRender,
-      // enabled,
-    );
-    const agentServer = await compileUserAgentServer(userArgs);
+    // const userArgs = await prepareUserArgs(
+    //   conversationContext,
+    //   env,
+    //   userRender,
+    //   // enabled,
+    // );
+    const agentServer = await compileUserAgentServer({
+      agentRenderer,
+    });
     const method = request.method as string;
     // const pathname = (args as any).pathname as string;
 
