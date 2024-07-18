d179e22d137ded15815a5e11bde12c851f843732
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-07-18 16:14:20 +0400

    Add nav items to header and test

--- apps/chat/components/header.tsx
+++ apps/chat/components/header.tsx
@@ -11,21 +11,33 @@ export function Header() {
     <header
       className="sticky top-0 z-50 flex items-center justify-between h-16 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center h-full">
-        <div className='md:m-w-[200px] md:w-[200px]'>
+        <div className='md:m-w-[160px] md:w-[160px]'>
           <Link href="/" rel="nofollow" className='hidden md:block w-44 ml-4'>
-            <IconUpstreetChat className="mr-2 fill-black size-9 w-auto dark:hidden" inverted/>
-            <IconUpstreetChat className="hidden mr-2 fill-white size-9 w-auto dark:block"/>
+            <IconUpstreetChat className="mr-2 fill-black size-9 w-auto dark:hidden" inverted />
+            <IconUpstreetChat className="hidden mr-2 fill-white size-9 w-auto dark:block" />
           </Link>
           <Link href="/" rel="nofollow" className='block md:hidden w-10 ml-4'>
-            <IconUpstreet className="mr-2 fill-black size-9 w-auto dark:hidden" inverted/>
-            <IconUpstreet className="hidden mr-2 fill-white size-9 w-auto dark:block"/>
+            <IconUpstreet className="mr-2 fill-black size-9 w-auto dark:hidden" inverted />
+            <IconUpstreet className="hidden mr-2 fill-white size-9 w-auto dark:block" />
           </Link>
         </div>
+        <Link href="/" rel="nofollow" className='block ml-4'>
+          Chat
+        </Link>
+        <Link href="/" rel="nofollow" className='block ml-4'>
+          SDK
+        </Link>
+        <Link href="/" rel="nofollow" className='block ml-4'>
+          Careers
+        </Link>
+        <Link href="/" rel="nofollow" className='block ml-4'>
+          Waitlist
+        </Link>
       </div>
-      <SearchBar/>
+      <SearchBar />
       <div className='md:m-w-[200px] md:w-[200px]'>
-        <React.Suspense fallback={<div className="flex-1 overflow-auto"/>}>
-          <AccountOrLogin/>
+        <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
+          <AccountOrLogin />
         </React.Suspense>
       </div>
     </header>
