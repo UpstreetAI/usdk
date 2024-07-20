b2049bde94049a79897f683cf4f7bd21e7eec065
Avaer Kazmer <a@modules.io>
2024-07-20 19:43:08 +0800

    Hook in account private ui

--- apps/chat/components/account/index.tsx
+++ apps/chat/components/account/index.tsx
@@ -1,16 +1,17 @@
 import { Bio } from '@/components/account/bio'
 import { Name } from '@/components/account/name'
 import { ProfileImage } from '@/components/account/profile-image'
-import { EditableText } from '@/components/editable-text'
-import { env } from '@/lib/env'
-import { makeAnonymousClient } from '@/utils/supabase/supabase-client'
+import { AccountPrivateUi } from './private-ui';
+// import { EditableText } from '@/components/editable-text'
+// import { env } from '@/lib/env'
+// import { makeAnonymousClient } from '@/utils/supabase/supabase-client'
 import React from 'react'
-import { Button } from '@/components/ui/button'
-import { IconEdit } from '@/components/ui/icons'
+// import { Button } from '@/components/ui/button'
+// import { IconEdit } from '@/components/ui/icons'
 import { redirect } from 'next/navigation'
-import { resolveRelativeUrl } from '@/lib/utils'
+// import { resolveRelativeUrl } from '@/lib/utils'
 import { routes } from '@/routes'
-import { getUserAccount, waitForUser } from '@/utils/supabase/server'
+import { getUserAccount, getUserAccountPrivate, waitForUser } from '@/utils/supabase/server'
 
 
 export interface AccountProps {
@@ -22,7 +23,8 @@ export interface AccountProps {
 export async function Account({ params: { id }}: AccountProps) {
   const currentUser = await waitForUser()
 
-  let user
+  let user = null
+  let userPrivate = null
   let userIsCurrentUser = false
 
   // Display user for given ID if provided, else get current user.
@@ -38,11 +40,19 @@ export async function Account({ params: { id }}: AccountProps) {
     userIsCurrentUser = true
   }
 
+  if (userIsCurrentUser) {
+    userPrivate = await getUserAccountPrivate(user.id, 'stripe_connect_account_id')
+  }
+
   return (
     <div className="flex flex-col flex-nowrap p-4">
       <ProfileImage user={user} userIsCurrentUser={userIsCurrentUser} />
       <Name user={user} userIsCurrentUser={userIsCurrentUser} />
       <Bio user={user} userIsCurrentUser={userIsCurrentUser} />
+      {userIsCurrentUser && <AccountPrivateUi
+        user={user}
+        userPrivate={userPrivate}
+      />}
 
       {/*<div className="whitespace-pre-wrap">
         {JSON.stringify( user, null, ' ' )}
