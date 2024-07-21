62566bfe2d1514dce72ca0e89cfac3bb07358399
Avaer Kazmer <ultimus@gmail.com>
2024-07-21 18:02:04 +0900

    Add audio encoding to audio input

--- sdk/src/devices/audio-input.mjs
+++ sdk/src/devices/audio-input.mjs
@@ -1,46 +1,147 @@
 import { EventEmitter } from 'events';
-// import { Transform } from 'stream';
+import { Transform } from 'stream';
 import child_process from 'child_process';
+import lamejs from 'lamejstmp';
 // import { OpusDecoderWebWorker } from 'opus-decoder';
 // import { OggOpusDecoderWebWorker } from 'ogg-opus-decoder';
 // import {
 //   QueueManager,
 // } from '../util/queue-manager.mjs';
+import {
+  aiHost,
+} from '../util/endpoints.mjs';
 
 /* class OggDecodeTransformStream extends Transform {
   constructor({
     sampleRate,
   }) {
-    const decoder = new OggOpusDecoderWebWorker();
-    const queueManager = new QueueManager();
+    super();
 
-    super({
-      transform: (chunk, encoding, callback) => {
-        (async () => {
-          await queueManager.waitForTurn(async () => {
-            await decoder.ready;
-            const {channelData, samplesDecoded, sampleRate: frameSampleRate} = await decoder.decode(chunk);
-            if (frameSampleRate === sampleRate) {
-              const samples = channelData[0]; // Float32Array
-              if (samples?.length > 0) {
-                this.push(samples);
-                // const b = Buffer.from(samples.buffer, samples.byteOffset, samples.byteLength);
-                // this.push(b);
-              }
-            } else {
-              console.warn('frame sample rate mismatch', frameSampleRate, sampleRate);
-            }
-          });
-          callback();
-        })();
-      },
-    });
+    this.decoder = new OggOpusDecoderWebWorker();
+    this.queueManager = new QueueManager();
 
     this.once('finish', () => {
       decoder.free();
     });
   }
+  _transform(chunk, encoding, callback) {
+      (async () => {
+        await this.queueManager.waitForTurn(async () => {
+          await this.decoder.ready;
+          const {channelData, samplesDecoded, sampleRate: frameSampleRate} = await this.decoder.decode(chunk);
+          if (frameSampleRate === sampleRate) {
+            const samples = channelData[0]; // Float32Array
+            if (samples?.length > 0) {
+              this.push(samples);
+              // const b = Buffer.from(samples.buffer, samples.byteOffset, samples.byteLength);
+              // this.push(b);
+            }
+          } else {
+            console.warn('frame sample rate mismatch', frameSampleRate, sampleRate);
+          }
+        });
+        callback();
+      })();
+    }
 } */
+
+//
+
+// convert Float32Array to Int16Array
+const convertF32I16 = (samples) => {
+  const buffer = new ArrayBuffer(samples.length * Int16Array.BYTES_PER_ELEMENT);
+  const view = new Int16Array(buffer);
+  for (let i = 0; i < samples.length; i++) {
+    view[i] = samples[i] * 0x7fff;
+  }
+  return view;
+};
+
+class Mp3EncodeStream extends Transform {
+  constructor({
+    sampleRate = AudioInput.defaultSampleRate,
+    bitRate = 128,
+  } = {}) {
+    super();
+
+    this.mp3encoder = new lamejs.Mp3Encoder(1, sampleRate, bitRate); // mono
+  }
+  _transform(chunk, encoding, callback) {
+    if (chunk.byteLength % Float32Array.BYTES_PER_ELEMENT !== 0) {
+      throw new Error('wrong byte length', chunk.byteLength);
+    }
+    const float32Array = new Float32Array(chunk.buffer, chunk.byteOffset, chunk.byteLength / Float32Array.BYTES_PER_ELEMENT);
+    const samples = convertF32I16(float32Array);
+    const encodedData = this.mp3encoder.encodeBuffer(samples);
+    this.push(encodedData);
+
+    callback();
+  }
+  _flush(callback) {
+    const encodedData = this.mp3encoder.flush();
+    this.push(encodedData);
+
+    callback();
+  }
+}
+
+export const encodeMp3 = (bs) => {
+  if (!Array.isArray(bs)) {
+    bs = [bs];
+  }
+
+  const encodeStream = new Mp3EncodeStream();
+                  
+  // read encoded result
+  const resultPromise = new Promise((accept, reject) => {
+    const bs = [];
+    encodeStream.on('data', d => {
+      bs.push(d);
+    });
+    encodeStream.on('end', () => {
+      const b = Buffer.concat(bs);
+      accept(b);
+    });
+  });
+
+  // write samples
+  for (const b of bs) {
+    encodeStream.write(b);
+  }
+  encodeStream.end();
+
+  return resultPromise;
+};
+
+//
+
+const defaultTranscriptionModel = 'whisper-1';
+export const transcribe = async (file, {
+  jwt,
+}) => {
+  const fd = new FormData();
+  fd.append('file', file);
+  fd.append('mode', defaultTranscriptionModel);
+  fd.append('language', 'en');
+  // fd.append('response_format', 'json');
+  
+  const res = await fetch(`${aiHost}/api/ai/audio/transcriptions`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${jwt}`,
+    },
+    body: fd,
+  });
+  if (res.ok) {
+    const j = await res.json();
+    const { text } = j;
+    return text;
+  } else {
+    throw new Error('request failed: ' + res.status);
+  }
+};
+
+//
 
 export class AudioInput extends EventEmitter {
   static defaultSampleRate = 48000;
