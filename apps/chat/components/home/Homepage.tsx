3326d934983e8a681928e898e8909f9ffac8bbae
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-07-24 19:10:22 +0400

    Add slider to homepage

--- apps/chat/components/home/Homepage.tsx
+++ apps/chat/components/home/Homepage.tsx
@@ -1,15 +1,21 @@
 'use client';
 
 import { useSupabase } from "@/lib/hooks/use-supabase";
-import { Button } from "../ui/button";
-import { newChat } from "@/lib/chat/actions";
+import BackgroundSlider from 'react-background-slider';
+
+
+const image1 = '/images/hero-1.jpg';
+const image2 = '/images/hero-2.jpg';
+const image3 = '/images/hero-3.jpg';
 
 export default function Home() {
   const { user } = useSupabase();
 
   return (
-    <div className="section section-hero">
-      
+    <div className="section section-hero w-full">
+      <BackgroundSlider
+  images={[image1, image2, image3]}
+  duration={6} transition={3} />
     </div>
   );
 }
