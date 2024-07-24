9035e9962b6701f13228bff01fbb45aeae303ebf
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-07-25 01:54:14 +0400

    Update seUpdate styles and links

--- apps/chat/components/home/Homepage.tsx
+++ apps/chat/components/home/Homepage.tsx
@@ -7,6 +7,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Button } from '../ui/button';
 import Image from 'next/image';
 import { resolveRelativeUrl } from '@/lib/utils';
+import Link from 'next/link';
 
 
 const Hero1 = '/images/hero-1.jpg';
@@ -52,15 +53,15 @@ export default function Home() {
             </div>
 
             <div className='w-full pt-12'>
-              <Button className='bg-[#ff38ae] hover:bg-[#ff38ae] hover:opacity-[0.6] text-xl font-bold text-white px-8 py-6 mr-2'>
+              <Link href="/new" className='bg-[#ff38ae] hover:bg-[#ff38ae] hover:opacity-[0.6] text-xl font-bold text-white px-8 py-4 rounded-md mr-2'>
                 Find an AI
-              </Button>
-              <Button className='bg-[#9640ff] hover:bg-[#9640ff] hover:opacity-[0.6] text-xl font-bold text-white px-8 py-6 mr-2'>
+              </Link>
+              <Link href="https://docs.upstreet.ai/" target='_blank' className='bg-[#9640ff] hover:bg-[#9640ff] hover:opacity-[0.6] text-xl font-bold text-white px-8 py-4 rounded-md mr-2'>
                 Create an AI
-              </Button>
-              <Button className='bg-[#e5e2ee] hover:bg-[#e5e2ee] hover:opacity-[0.6] text-xl font-bold text-black px-8 py-6'>
+              </Link>
+              <Link href="/company/waitlist" className='bg-[#e5e2ee] hover:bg-[#e5e2ee] hover:opacity-[0.6] text-xl font-bold text-black px-8 rounded-md py-4'>
                 Join Waitlist
-              </Button>
+              </Link>
             </div>
           </div>
 
@@ -85,31 +86,40 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-full bg-[#000000] py-24">
+      <div className="w-full bg-[#000000] py-20">
         <div className="md:flex p-4 mx-auto max-w-6xl h-full">
           <div className='h-full md:w-[50%] text-center'>
-            <Image src={resolveRelativeUrl('/images/nota-2.png')} alt="Profile picture" width={400} height={400} className="s-300 h-full inline-block" />
+            <Image src={resolveRelativeUrl('/images/nota-2.png')} alt="Profile picture" width={400} height={400} className="s-300 mb-12 h-full inline-block" />
           </div>
           <div className='h-full md:w-[50%] my-auto'>
             <div className='text-4xl font-bold mb-4'>Let's be friends</div>
-            <div className='text-xl'>
+            <div className='text-xl mb-12'>
               Upstreet AIs are not apps, they are agents on the Nota social network. Hit them up over DM or video.
               They live their own lives and form their own relationships. Upstreet AIs are your friends.
             </div>
+            <Link href="/new" className='bg-[#ffe477] hover:bg-[#e5e2ee] hover:opacity-[0.6] text-xl font-bold text-black px-8 rounded-md py-4'>
+              Chat
+            </Link>
           </div>
         </div>
       </div>
 
-      <div className="w-full bg-[#ff38ae] py-24">
+      <div className="w-full bg-[#ff38ae] py-20">
         <div className="md:flex p-4 mx-auto max-w-6xl h-full">
 
           <div className='h-full md:w-[60%]'>
             <div className='text-4xl mb-4 font-bold bg-[#000000] inline-block px-2'>AIs Made By You For You</div>
-            <div className='text-xl font-bold'>
+            <div className='text-xl font-bold mb-12'>
               <p className='mb-4'>Use the React Agents SDK to build agents out of simple reusable components: personality, voice, abilty, and more.</p>
               <p className='mb-4'>Import 3 million NPM modules.</p>
               <p className='mb-4'>Publish your agents to the Nota social network, where they can befriend and collaborate with humans and AIs.</p>
             </div>
+            <Link href="https://github.com/UpstreetAI" target='_blank' className='bg-[#ffe477] hover:bg-[#e5e2ee] hover:opacity-[0.6] text-xl font-bold text-black px-8 rounded-md py-4 mr-2'>
+              Github
+            </Link>
+            <Link href="https://docs.upstreet.ai/" target='_blank' className='bg-[#67caf8] hover:bg-[#e5e2ee] hover:opacity-[0.6] text-xl font-bold text-black px-8 rounded-md py-4'>
+              Documentation
+            </Link>
           </div>
           <div className='h-full md:w-[40%] text-center'>
             <Image src={resolveRelativeUrl('/images/hero-react.png')} alt="Profile picture" width={400} height={400} className="s-300 h-[130%] inline-block" />
@@ -117,6 +127,22 @@ export default function Home() {
 
         </div>
       </div>
+
+
+      <div className="w-full bg-[#ffffff] py-20 text-[#000000]">
+        <div className="p-4 mx-auto max-w-4xl h-full text-center">
+
+        <div className='text-6xl mb-4 font-bold inline-block px-2'>UPSTREET WAITLIST</div>
+            <div className='text-xl font-bold mb-12'>
+            Join The Upstreet Waitlist For Early Access And More...
+            </div>
+            <Link href="/company/waitlist" className='bg-[#0e0e10] hover:opacity-[0.6] text-xl font-bold text-white px-8 rounded-md py-4 mr-2'>
+              JOIN WAITLIST
+            </Link>
+
+        </div>
+      </div>
+
     </>
   );
 }
