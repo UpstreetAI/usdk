5506f875d5e2c132d34d055d31c6f166cb23613b
Avaer Kazmer <ultimus@gmail.com>
2024-07-24 17:04:48 +0800

    Major editor update

--- apps/chat/app/new/editor.tsx
+++ apps/chat/app/new/editor.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
 import { Button } from '@/components/ui/button';
 
@@ -8,8 +8,9 @@ export default function AgentEditor() {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [deploying, setDeploying] = useState(false);
-  const monaco = useMonaco();
+  const formEl = useRef<HTMLFormElement>(null);
 
+  const monaco = useMonaco();
   useEffect(() => {
     if (monaco) {
       console.log('here is the monaco instance:', monaco);
@@ -17,28 +18,60 @@ export default function AgentEditor() {
   }, [monaco]);
 
   return (
-    <div className="relative flex flex-col flex-1">
+    <form className="relative flex flex-col flex-1" ref={formEl} onSubmit={e => {
+      e.preventDefault();
+
+      // check if the form is validated
+      const valid = formEl.current?.checkValidity();
+      if (valid) {
+        (async () => {
+          setDeploying(true);
+
+          // get the value from monaco editor
+          const value = monaco?.editor.getModels()[0].getValue();
+          console.log('deploy', {
+            name,
+            description,
+            value,
+          });
+
+          await new Promise(resolve => setTimeout(resolve, 2000));
+
+          setDeploying(false);
+        })();
+      }
+    }}>
       <div className="flex m-4">
-        <input type="text" className="p-2 mr-2" value={name} placeholder="Name" onChange={e => {
-          setName(e.target.value);
-        }} />
-        <input type="text" className="p-2 mr-2 flex-1" value={description} placeholder="Description" onChange={e => {
-          setDescription(e.target.value);
-        }} />
-        <Button onClick={e => {
-          (async () => {
-            setDeploying(true);
-
-            await new Promise(resolve => setTimeout(resolve, 2000));
-
-            setDeploying(false);
-          })();
-        }} disabled={deploying}>{!deploying ? `Deploy` : 'Deploying...'}</Button>
+      <input type="text" className="p-2 mr-2" value={name} placeholder="Name" onChange={e => {
+        setName(e.target.value);
+      }} required />
+      <input type="text" className="p-2 mr-2 flex-1" value={description} placeholder="Description" onChange={e => {
+        setDescription(e.target.value);
+      }} required />
+      <Button onClick={e => {
+        // trigger a submit attempt
+        formEl.current?.dispatchEvent(new Event('submit', { bubbles: true, cancelable: true }));
+      }} disabled={deploying}>{!deploying ? `Deploy` : 'Deploying...'}</Button>
       </div>
       <Editor
         theme="vs-dark"
         defaultLanguage="javascript"
-        defaultValue="// some comment"
+        defaultValue={`\
+import React from 'react';
+import {
+  Agent,
+} from 'react-agents';
+
+//
+
+export default function MyAgent(props) {
+  return (
+    <Agent>
+      {/* ... */}
+    </Agent>
+  );
+}
+`}
         options={{
           readOnly: deploying,
         }}
@@ -46,6 +79,6 @@ export default function AgentEditor() {
           (editor as any)._domElement.parentNode.style.flex = 1;
         }}
       />
-    </div>
+    </form>
   );
 };
