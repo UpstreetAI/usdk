06125a5ee51959d198f8315da3160aabf6a7b793
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-07-25 13:34:48 +0400

    Add missing waitlist page

---
+++ apps/chat/app/company/waitlist/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Waitlist() {
+  return (
+    <div className="mx-auto max-w-6xl px-6 pt-8 pb-16 markdown">
+      <form-widget ucid='gX3p2JkNdLHrZSSivBsto3Taies'></form-widget>
+    </div>
+  );
+}
