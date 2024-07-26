d830f775b3b4179bfdf286ad56c74ac22c59b84f
Avaer Kazmer <ultimus@gmail.com>
2024-07-26 17:48:57 +0800

    Support image upload

--- apps/chat/components/prompt-form.tsx
+++ apps/chat/components/prompt-form.tsx
@@ -23,7 +23,7 @@ export function PromptForm({
 }) {
   const [mediaPickerOpen, setMediaPickerOpen] = React.useState(false);
   const inputRef = React.useRef<HTMLTextAreaElement>(null)
-  const { sendChatMessage } = useMultiplayerActions()
+  const { sendChatMessage, sendMediaMessage } = useMultiplayerActions()
 
   const toggleMediaPicker = () => {
     setMediaPickerOpen(open => !open)
@@ -76,9 +76,10 @@ export function PromptForm({
               className="flex justify-start relative rounded bg-background mx-2 p-2 overflow-hidden"
             >
               <input className="absolute -top-12 bottom-0 -left-12 right-0 cursor-pointer" type="file" onChange={e => {
-                const files = Array.from((e.target as any).files);
+                const files: File[] = Array.from((e.target as any).files);
                 const file = files[0];
-                console.log('image file', file);
+                // console.log('image file', file.type, file);
+                sendMediaMessage(file);
 
                 toggleMediaPicker();
                 (e.target as any).value = null;
