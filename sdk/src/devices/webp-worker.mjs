01868fc218118d08d1a903773b63a82052ba6f78
Avaer Kazmer <ultimus@gmail.com>
2024-07-31 15:57:10 +0800

    Implement webp-worker.mjs backend

---
+++ sdk/src/devices/webp-worker.mjs
@@ -0,0 +1,66 @@
+import webp from 'webp-wasm';
+import { zbdecode, zbencode } from '../lib/zjs/encoding.mjs';
+import { QueueManager } from '../util/queue-manager.mjs';
+
+const encodeWebp = async (imageData, {
+  quality = 75,
+  lossless = false,
+} = {}) => {
+  const outputBuffer = await webp.encode(imageData, {
+    quality,
+    lossless: +lossless,
+  });
+  const uint8Array = new Uint8Array(outputBuffer.buffer, outputBuffer.byteOffset, outputBuffer.byteLength);
+  return uint8Array;
+};
+const decodeWebp = async (encodedData) => {
+  const arrayBuffer = encodedData.buffer.slice(encodedData.byteOffset, encodedData.byteOffset + encodedData.byteLength);
+  const imageData = await webp.decode(arrayBuffer);
+  return imageData;
+};
+
+const queueManager = new QueueManager();
+globalThis.onmessage = async (e) => {
+  await queueManager.waitForTurn(async () => {
+    let error, result;
+    try {
+      const b = e.data;
+      const o = zbdecode(b);
+      const {
+        method,
+        args,
+      } = o;
+      switch (method) {
+        case 'encode': {
+          const {
+            imageData,
+            opts,
+          } = args;
+          result = await encodeWebp(imageData, opts);
+          break;
+        }
+        case 'decode': {
+          const {
+            encodedData,
+          } = args;
+          result = await decodeWebp(encodedData);
+          break;
+        }
+        default: {
+          throw new Error('unknown method: ' + method);
+        }
+      }
+    } catch(err) {
+      console.error(err);
+      error = err;
+    }
+
+    let b;
+    if (!error) {
+      b = zbencode({ error: null, result });
+    } else {
+      b = zbencode({ error, result: null });
+    }
+    postMessage(b, [b.buffer]);
+  });
+};
