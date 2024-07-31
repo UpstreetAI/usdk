1fdbf5468b1f2e53ff5a4b9f4ecc7914017b90b9
Avaer Kazmer <ultimus@gmail.com>
2024-07-31 13:53:00 +0800

    Add vision.mjs

---
+++ sdk/src/util/vision.mjs
@@ -0,0 +1,47 @@
+import { aiProxyHost } from './endpoints.mjs';
+
+export const describe = async (frame, query = `What's in this image?`, {
+  jwt,
+}) => {
+  const messages = [
+    {
+      role: 'user',
+      content: [
+        {
+          "type": "text",
+          "text": query,
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": `data:image/webp;base64,${frame.toString('base64')}`,
+          }
+        }
+      ],
+    },
+  ];
+  const res = await fetch(`https://${aiProxyHost}/api/ai/chat/completions`, {
+    method: 'POST',
+
+    headers: {
+      'Content-Type': 'application/json',
+      // 'OpenAI-Beta': 'assistants=v1',
+      Authorization: `Bearer ${jwt}`,
+    },
+
+    body: JSON.stringify({
+      model: 'gpt-4o',
+      messages,
+
+      // stream,
+    }),
+    // signal,
+  });
+  if (res.ok) {
+    const j = await res.json();
+    return j.choices[0].message.content;
+  } else {
+    const text = await res.text();
+    throw new Error('invalid status code: ' + res.status + ': ' + text);
+  }
+};
