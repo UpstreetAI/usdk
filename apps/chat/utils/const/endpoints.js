e3829aa7bc94b6fb0b7260b8d378c2991281fbaf
Avaer Kazmer <ultimus@gmail.com>
2024-08-08 08:50:15 -0700

    Add new endpoints

--- apps/chat/utils/const/endpoints.js
+++ apps/chat/utils/const/endpoints.js
@@ -4,3 +4,7 @@ export const deployEndpointUrl = `https://deploy.upstreet.ai`;
 export const metamaskHost = 'https://metamask.upstreet.ai';
 export const multiplayerEndpointUrl = 'wss://multiplayer.isekaichat.workers.dev';
 export const r2EndpointUrl = `https://r2.upstreet.ai`;
+
+export const dinoEndpoint = `https://jit9ls0uhzmsfn-9000.proxy.runpod.net`;
+export const samEndpoint = `https://jit9ls0uhzmsfn-10000.proxy.runpod.net`;
+export const depthEndpoint = `https://jit9ls0uhzmsfn-9999.proxy.runpod.net`;
