d7685ab5b3358e53882287deea024e080c7da033
Avaer Kazmer <ultimus@gmail.com>
2024-08-13 14:02:03 -0700

    Remove dev guid support

--- sdk/src/util/guid-util.mjs
+++ sdk/src/util/guid-util.mjs
@@ -1,15 +1,4 @@
 export const isGuid = (guid) => {
   return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.test(guid);
 };
-export const isDevGuid = (guid) => {
-  return /^00000000-0000-0000-0000-[0-9a-f]{12}/.test(guid);
-};
-export const makeDevGuid = () => {
-  let guid = crypto.randomUUID();
-  guid = guid.replace(
-    /^([^-]+)-([^-]+)-([^-]+)-([^-]+)/,
-    '00000000-0000-0000-0000',
-  );
-  return guid;
-};
 export const makeZeroGuid = () => '00000000-0000-0000-0000-000000000000';
