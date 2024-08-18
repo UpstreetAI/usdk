3f7b82536df9c89b7bd9c048a2d21ac50a9a896e
Avaer Kazmer <ultimus@gmail.com>
2024-08-18 13:03:13 -0700

    Add coords injection

--- apps/chat/app/land/[id]/page.tsx
+++ apps/chat/app/land/[id]/page.tsx
@@ -1,15 +1,16 @@
 'use client';
 
 import { Land } from '@/components/land'
-import { useSearchParams } from 'next/navigation'
+// import { useSearchParams } from 'next/navigation'
+
+const loadUrl = new URL(location.href);
+const query = loadUrl.searchParams;
 
 export default function LandPage({
   id,
 }: {
   id: string,
 }) {
-  // parse the query string
-  const query = useSearchParams();
   const edit = query.get('edit') !== null;
   
   return (
--- apps/chat/app/map/[id]/page.tsx
+++ apps/chat/app/map/[id]/page.tsx
@@ -1,20 +1,61 @@
 'use client';
 
-import { Map } from '@/components/map'
-import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
+import type { Coord2D } from '@/components/map';
+import { Map } from '@/components/map';
+import { Vector3 } from 'three';
+
+const roundToDecimals = (n: number, decimals: number) => {
+  const p = Math.pow(10, decimals);
+  return Math.round(n * p) / p;
+};
+
+const coordSep = ':';
+
+const loadUrl = new URL(location.href);
+const query = loadUrl.searchParams;
 
 export default function MapPage({
   id,
 }: {
   id: string,
 }) {
-  const query = useSearchParams();
+  const router = useRouter();
+
   const edit = query.get('edit') !== null;
+  const coord = (() => {
+    const q = query.get('coord');
+    if (q !== null) {
+      const [x, z] = q.split(coordSep).map(parseFloat);
+      if (!isNaN(x) && !isNaN(z)) {
+        const coord = { x, z } as Coord2D;
+        return coord;
+      } else {
+        return undefined;
+      }
+    } else {
+      return undefined;
+    }
+  })();
+  const onMove = async (position: Vector3) => {
+    const coord = {
+      x: roundToDecimals(position.x, 2),
+      z: roundToDecimals(position.z, 2),
+    } as Coord2D;
+
+    const u = new URL(location.href);
+    u.searchParams.set('coord', `${coord.x.toFixed(2)}${coordSep}${coord.z.toFixed(2)}`);
+    const s = u.pathname + u.search;
+    console.log('onMove replace', s);
+    await router.replace(s);
+  };
 
   return (
     <Map
       id={id}
       edit={edit}
+      coord={coord}
+      onMove={onMove}
     />
   );
 }
