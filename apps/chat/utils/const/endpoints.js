de4e2bbe063829cd6cdfc68ce21aa6d1a437d4c5
Avaer Kazmer <ultimus@gmail.com>
2024-08-19 12:57:47 -0700

    Update runpod endpoint

--- apps/chat/utils/const/endpoints.js
+++ apps/chat/utils/const/endpoints.js
@@ -5,8 +5,9 @@ export const metamaskHost = 'https://metamask.upstreet.ai';
 export const multiplayerEndpointUrl = 'wss://multiplayer.isekaichat.workers.dev';
 export const r2EndpointUrl = `https://r2.upstreet.ai`;
 
-export const dinoEndpoint = `https://4j7jscv6f41bmr-9000.proxy.runpod.net`;
-export const samEndpoint = `https://4j7jscv6f41bmr-10000.proxy.runpod.net`;
-export const depthEndpoint = `https://4j7jscv6f41bmr-9999.proxy.runpod.net`;
-export const inpaintEndpoint = `https://4j7jscv6f41bmr-11111.proxy.runpod.net`;
-export const backgroundRemoverEndpoint = `https://4j7jscv6f41bmr-4444.proxy.runpod.net/`;
+const runpodEndpointRoot = `k4ym30m9kadkld`;
+export const dinoEndpoint = `https://${runpodEndpointRoot}-9000.proxy.runpod.net`;
+export const samEndpoint = `https://${runpodEndpointRoot}-10000.proxy.runpod.net`;
+export const depthEndpoint = `https://${runpodEndpointRoot}-9999.proxy.runpod.net`;
+export const inpaintEndpoint = `https://${runpodEndpointRoot}-11111.proxy.runpod.net`;
+export const backgroundRemoverEndpoint = `https://${runpodEndpointRoot}-4444.proxy.runpod.net/`;
