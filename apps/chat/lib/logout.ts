9d0e52fb61ceb27c9edcb42ec9480a875fdd883e
Avaer Kazmer <ultimus@gmail.com>
2024-08-19 15:20:27 -0700

    Bugfix logout not working

--- apps/chat/lib/logout.ts
+++ apps/chat/lib/logout.ts
@@ -2,10 +2,11 @@
 
 export function logout() {
   return new Promise<void>((resolve) => {
-    if (typeof document !== 'undefined') {
-      document.cookie = 'auth-jwt=; Max-Age=0';
+    // if (typeof document !== 'undefined') {
+      document.cookie = 'auth-jwt=; Max-Age=0; path=/';
+      // console.log('logged out', [document.cookie]);
       resolve();
-    }
+    // }
   }).then(() => {
     location.replace('/');
   });
