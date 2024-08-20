d71d105bc71c9c8deabaed1233ddfa96a2ee85a0
Avaer Kazmer <ultimus@gmail.com>
2024-08-19 22:12:59 -0700

    Land/map page edit passthrough

--- apps/chat/app/land/page.tsx
+++ apps/chat/app/land/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
 import { redirect } from 'next/navigation';
 
 export default function LandPageDefault() {
+  const loadUrl = new URL(location.href);
+  const query = loadUrl.searchParams;
+  const edit = query.get('edit') !== null;
+
   const guid = crypto.randomUUID();
-  redirect(`/land/${guid}?edit`);
-  // return null;
+  const u = new URL(`http://localhost/land/${guid}`);
+  edit && u.searchParams.set('edit', '');
+  const { pathname } = u;
+  redirect(pathname);
 }
