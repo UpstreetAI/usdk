fa05f665ee12aceb8de6700a7fe28203f6af99ef
Avaer Kazmer <ultimus@gmail.com>
2024-08-20 16:28:24 -0700

    Fix redirect lib

--- apps/chat/lib/redirectToLoginTool.ts
+++ apps/chat/lib/redirectToLoginTool.ts
@@ -4,17 +4,19 @@ import {
 
 const loginPathname = '/login';
 
-export async function redirectToLoginTool() {
-  /* Get the JWT. */
-
-  // Redirect to the login tool.
-  const redirectURL = new URL(location.protocol + '//' + location.host + loginPathname);
-  redirectURL.searchParams.set('referrer_url', encodeURI(location.href));
+export function getLoginRedirectUrl(referrerUrl: string) {
+  // redirect to the login page
+  const redirectURL = new URL(location.href);
+  redirectURL.pathname = loginPathname;
+  redirectURL.searchParams.set('referrer_url', encodeURI(referrerUrl));
   const redirectURLString = redirectURL + '';
 
   const url = new URL(loginEndpointUrl);
   url.searchParams.set('redirect_url', redirectURLString);
   const urlString = url + '';
+  return urlString;
+}
 
-  location.href = urlString;
+export async function redirectToLoginTool() {
+  location.href = getLoginRedirectUrl(location.href);
 }
