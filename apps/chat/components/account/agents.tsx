e2890e5022edbf6b62bfbad222d7ec279b23e4c4
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-08-21 16:37:40 +0400

    Update credits table styles

--- apps/chat/components/account/agents.tsx
+++ apps/chat/components/account/agents.tsx
@@ -40,135 +40,129 @@ export function Agents({ agents: agentsInit, userIsCurrentUser }: AgentsProps) {
       <div className="w-full m-auto my-4 border rounded-md p border-zinc-700">
         <div className="px-5 py-4">
           <div className="w-full">
-            <div className="flex">
-              <div className="w-full">
-                <div className="relative">
-                  <div className="relative shadow-md sm:rounded-lg">
-                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                      
-                      <thead className="text-xs text-gray-50 uppercase bg-border">
-                        <tr>
-                          <th key={'preview'} scope="col" className="px-6 w-[60px] py-3 text-[rgba(255,255,255,0.6)]">PFP</th>
-                          <th key={'info'} scope="col" className="px-6 py-3 text-[rgba(255,255,255,0.6)]">Agent Info</th>
-                          <th key={'creds'} scope="col" className="px-6 py-3 text-[rgba(255,255,255,0.6)] text-center">Credits Used</th>
-                          <th key={'chart'} scope="col" className="px-6 py-3 text-[rgba(255,255,255,0.6)]">Chart</th>
-                          <th key={'actions'} scope="col" className="px-6 py-3 text-[rgba(255,255,255,0.6)]">
-                            Actions
-                          </th>
-                        </tr>
-                      </thead>
+            <div className="relative shadow-md sm:rounded-lg">
+              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
 
-                      <tbody>
-                        {agents.map((agent: object, i: number) => {
-                          const {
-                            id,
-                            name,
-                            description,
-                            start_url,
-                            preview_url,
-                            version,
-                            credits_usage
-                          } = agent as any;
+                <thead className="text-xs text-gray-50 uppercase bg-border">
+                  <tr>
+                    <th key={'preview'} scope="col" className="px-6 w-[60px] py-3 text-[rgba(255,255,255,0.6)]">PFP</th>
+                    <th key={'info'} scope="col" className="px-6 py-3 text-[rgba(255,255,255,0.6)]">Agent Info</th>
+                    <th key={'creds'} scope="col" className="px-6 py-3 text-[rgba(255,255,255,0.6)] text-center">Credits Used</th>
+                    <th key={'chart'} scope="col" className="px-6 py-3 text-[rgba(255,255,255,0.6)]">Chart</th>
+                    <th key={'actions'} scope="col" className="px-6 py-3 text-[rgba(255,255,255,0.6)]">
+                      Actions
+                    </th>
+                  </tr>
+                </thead>
 
-                          const sparklineData = credits_usage
-                            ? credits_usage.map((credit: { created_at: any; amount: number; }) => (credit.amount * 1000))
-                            : [];
+                <tbody>
+                  {agents.map((agent: object, i: number) => {
+                    const {
+                      id,
+                      name,
+                      description,
+                      start_url,
+                      preview_url,
+                      version,
+                      credits_usage
+                    } = agent as any;
 
-                          // Get the sum of all credits used by the agent
-                          const overallCreditsUsed = sparklineData.reduce((total: any, currentValue: any) => total + currentValue, 0) ?? 0;
+                    const sparklineData = credits_usage
+                      ? credits_usage.map((credit: { created_at: any; amount: number; }) => (credit.amount * 1000))
+                      : [];
 
-                          return (
-                            <tr className="hover:bg-border text-white bg-[rgba(255,255,255,0.1)] mt-1" key={i}>
+                    // Get the sum of all credits used by the agent
+                    const overallCreditsUsed = sparklineData.reduce((total: any, currentValue: any) => total + currentValue, 0) ?? 0;
 
-                              <td key={'t-0'} className="px-6 w-[60px] pr-0 py-4 text-md capitalize align-top">
-                                <div className="mr-4 mb-4 size-[60px] min-w-12 bg-[rgba(0,0,0,0.1)] dark:bg-[rgba(255,255,255,0.1)] rounded-[8px] flex items-center justify-center">
-                                  {preview_url && isValidUrl(preview_url) ? (
-                                    <Image className="flex" width={60} height={60} src={preview_url} alt='Profile picture' />
-                                  ) : (
-                                    <div className='uppercase text-lg font-bold'>{name.charAt(0)}</div>
-                                  )}
-                                </div>
-                              </td>
+                    return (
+                      <tr className="hover:bg-border text-white bg-[rgba(255,255,255,0.1)] mt-1" key={i}>
 
-                              <td key={'t-1'} className="px-6 py-4 text-md capitalize align-top">
-                                <div className='font-bold truncate'>{name}</div>
-                                <div className='truncate'>{description}</div>
-                                <div className='mt-1 text-gray-400 font-bold'>Version: {version}</div>
-                              </td>
+                        <td key={'t-0'} className="px-6 w-[60px] pr-0 py-4 text-md capitalize align-top">
+                          <div className="mr-4 mb-4 size-[60px] min-w-12 bg-[rgba(0,0,0,0.1)] dark:bg-[rgba(255,255,255,0.1)] rounded-[8px] flex items-center justify-center">
+                            {preview_url && isValidUrl(preview_url) ? (
+                              <Image className="flex" width={60} height={60} src={preview_url} alt='Profile picture' />
+                            ) : (
+                              <div className='uppercase text-lg font-bold'>{name.charAt(0)}</div>
+                            )}
+                          </div>
+                        </td>
 
-                              <td key={'t-2'} className="px-6 py-4 text-md capitalize text-2xl align-middle text-center">
-                                {Math.round(overallCreditsUsed)}
-                              </td>
+                        <td key={'t-1'} className="px-6 py-4 text-md capitalize align-top">
+                          <div className='font-bold truncate'>{name}</div>
+                          <div className='truncate'>{description}</div>
+                          <div className='mt-1 text-gray-400 font-bold'>Version: {version}</div>
+                        </td>
 
-                              <td key={'chart'} className="px-6 py-4 text-md capitalize align-top">
-                                <SparkLineChart data={sparklineData} colors={['#a855f7']} height={50} width={100} />
-                              </td>
+                        <td key={'t-2'} className="px-6 py-4 text-md capitalize text-2xl align-middle text-center">
+                          {Math.round(overallCreditsUsed)}
+                        </td>
 
-                              <td key={'t-4'} className="relative px-6 py-4 text-md capitalize align-top">
-                                <div className='relative w-8'>
-                                {userIsCurrentUser && <>
-                                  <div className="w-8 text-center flex flex-col px-2 py-1 mb-auto cursor-pointer rounded border bg-primary/10 hover:bg-primary/20 active:bg-primary/30" onClick={e => {
-                                    setOpenAgentIndex(i === openAgentIndex ? -1 : i);
-                                  }}>
-                                    <IconDots />
-                                  </div>
-                                  {i === openAgentIndex && <div className="absolute flex flex-col top-0 right-10 p-2 rounded border cursor-auto bg-primary/10">
-                                    <Button variant="outline" className="text-xs mb-1" onClick={e => {
-                                      setOpenAgentIndex(-1);
+                        <td key={'chart'} className="px-6 py-4 text-md capitalize align-top">
+                          <SparkLineChart data={sparklineData} colors={['#a855f7']} height={50} width={100} />
+                        </td>
 
-                                      (async () => {
-                                        await agentJoin(id);
-                                      })();
-                                    }}>
-                                      Chat
-                                    </Button>
-                                    <Button variant="outline" className="text-xs mb-1" onClick={e => {
-                                      setOpenAgentIndex(-1);
+                        <td key={'t-4'} className="relative px-6 py-4 text-md capitalize align-top">
+                          <div className='relative w-8'>
+                            {userIsCurrentUser && <>
+                              <div className="w-8 text-center flex flex-col px-2 py-1 mb-auto cursor-pointer rounded border bg-primary/10 hover:bg-primary/20 active:bg-primary/30" onClick={e => {
+                                setOpenAgentIndex(i === openAgentIndex ? -1 : i);
+                              }}>
+                                <IconDots />
+                              </div>
+                              {i === openAgentIndex && <div className="absolute flex flex-col top-0 right-10 p-2 rounded border cursor-auto bg-primary/10">
+                                <Button variant="outline" className="text-xs mb-1" onClick={e => {
+                                  setOpenAgentIndex(-1);
 
-                                      router.push(`/agents/${id}/logs`);
-                                    }}>
-                                      Logs
-                                    </Button>
-                                    <Button variant="destructive" className="text-xs" onClick={e => {
-                                      setOpenAgentIndex(-1);
+                                  (async () => {
+                                    await agentJoin(id);
+                                  })();
+                                }}>
+                                  Chat
+                                </Button>
+                                <Button variant="outline" className="text-xs mb-1" onClick={e => {
+                                  setOpenAgentIndex(-1);
 
-                                      (async () => {
-                                        const jwt = await getJWT();
-                                        const res = await fetch(`${deployEndpointUrl}/agent`, {
-                                          method: 'DELETE',
-                                          body: JSON.stringify({
-                                            guid: id,
-                                          }),
-                                          headers: {
-                                            'Content-Type': 'application/json',
-                                            Authorization: `Bearer ${jwt}`,
-                                          },
-                                        });
-                                        if (res.ok) {
-                                          await res.blob();
+                                  router.push(`/agents/${id}/logs`);
+                                }}>
+                                  Logs
+                                </Button>
+                                <Button variant="destructive" className="text-xs" onClick={e => {
+                                  setOpenAgentIndex(-1);
 
-                                          setAgents((agents: object[]) => {
-                                            return agents.filter((agent: any) => agent.id !== id);
-                                          });
-                                        } else {
-                                          console.warn(`invalid status code: ${res.status}`);
-                                        }
-                                      })();
-                                    }}>Delete</Button>
-                                  </div>}
-                                </>}
-                                </div>
-                              </td>
-                            </tr>
+                                  (async () => {
+                                    const jwt = await getJWT();
+                                    const res = await fetch(`${deployEndpointUrl}/agent`, {
+                                      method: 'DELETE',
+                                      body: JSON.stringify({
+                                        guid: id,
+                                      }),
+                                      headers: {
+                                        'Content-Type': 'application/json',
+                                        Authorization: `Bearer ${jwt}`,
+                                      },
+                                    });
+                                    if (res.ok) {
+                                      await res.blob();
 
-                          );
-                        })}
+                                      setAgents((agents: object[]) => {
+                                        return agents.filter((agent: any) => agent.id !== id);
+                                      });
+                                    } else {
+                                      console.warn(`invalid status code: ${res.status}`);
+                                    }
+                                  })();
+                                }}>Delete</Button>
+                              </div>}
+                            </>}
+                          </div>
+                        </td>
+                      </tr>
 
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
+                    );
+                  })}
+
+                </tbody>
+              </table>
             </div>
           </div>
 
--- apps/chat/components/account/credits.tsx
+++ apps/chat/components/account/credits.tsx
@@ -14,30 +14,46 @@ export function Credits({ creditsUsageHistory }: AgentsProps) {
           Credits
         </h1>
         <p className="max-w-2xl m-auto mt-4 text-lg text-zinc-200 sm:text-center sm:text-xl">
-          Credits usage history.
+          Overall credits usage history.
         </p>
       </div>
       <div className="w-full m-auto my-4 border rounded-md p border-zinc-700">
         <div className="px-5 py-4">
-          <div className="flex flex-col m-auto mt-5 w-full max-w-4xl">
-          <div className='flex'>
-                <p className='w-full font-bold'>Date</p>
-                <p className='w-60 font-bold text-right'>Service</p>
-                <p className='w-40 font-bold text-right'>Amount</p>
-                </div>
 
-            {creditsUsageHistory?.map((creditHistoryItem: any, i: number) => {
-              return (
-                <div className='flex text-[#efefef]' key={i}>
-                <p className='w-full'>{creditHistoryItem?.created_at}</p>
-                <p className='w-60 text-right'>{creditHistoryItem?.service}</p>
-                <p className='w-40 text-right'>{creditHistoryItem?.amount}</p>
-                </div>
-              );
-            })}
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+
+            <thead className="text-xs text-gray-50 uppercase bg-border">
+              <tr>
+                <th key={'preview'} scope="col" className="px-6 w-[60px] py-3 text-[rgba(255,255,255,0.6)]">Date</th>
+                <th key={'info'} scope="col" className="px-6 py-3 text-[rgba(255,255,255,0.6)]">Service</th>
+                <th key={'creds'} scope="col" className="px-6 py-3 text-[rgba(255,255,255,0.6)]">Credits Used</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {creditsUsageHistory?.map((creditHistoryItem: any, i: number) => {
+
+                return (
+                  <tr className="hover:bg-border text-white bg-[rgba(255,255,255,0.1)] mt-1" key={i}>
+
+                    <td key={'t-1'} className="px-6 py-4 text-md capitalize align-top">
+                      {creditHistoryItem?.created_at}
+                    </td>
+                    <td key={'t-2'} className="px-6 py-4 text-md capitalize align-top">
+                      {creditHistoryItem?.service}
+                    </td>
+                    <td key={'t-3'} className="px-6 py-4 text-md capitalize align-top">
+                      {creditHistoryItem?.amount}
+                    </td>
+
+                  </tr>
+                );
+              })}
+
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-    </div>
     </div>
   )
 }
