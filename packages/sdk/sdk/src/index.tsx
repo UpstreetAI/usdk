63d7bbc650f2ba801ea156f0e42a9d8699038336
Avaer Kazmer <ultimus@gmail.com>
2024-08-26 12:56:46 -0700

    Updat sdk src

--- packages/sdk/sdk/src/index.tsx
+++ packages/sdk/sdk/src/index.tsx
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import { useState, useEffect, useContext } from 'react';
-// import dedent from 'dedent';
-// import { minimatch } from 'minimatch';
-// import jsAgo from 'js-ago';
-// import type {
-//   AppContextValue,
-//   // AgentProps,
-//   ActionProps,
-//   // PromptProps,
-//   // ParserProps,
-//   // PerceptionProps,
-//   // SchedulerProps,
-//   // ServerProps,
-//   SceneObject,
-//   AgentObject,
-//   ActiveAgentObject,
-//   PendingActionEvent,
-//   ActionMessage,
-//   SdkDefaultComponentArgs,
-// } from './types';
-// import {
-//   AppContext,
-//   EpochContext,
-// } from './context';
-
 // components
 
 export * from './components';
