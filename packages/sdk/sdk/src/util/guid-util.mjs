f747f78e6031620f4755e03e69b98a36fb3a5a7b
Avaer Kazmer <ultimus@gmail.com>
2024-08-26 12:56:29 -0700

    Update sdk guid util

--- packages/sdk/sdk/src/util/guid-util.mjs
+++ packages/sdk/sdk/src/util/guid-util.mjs
@@ -5,7 +5,7 @@ import {
 export const isGuid = (guid) => {
   return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.test(guid);
 };
-export const makeZeroGuid = () => '00000000-0000-0000-0000-000000000000';
+// export const makeZeroGuid = () => '00000000-0000-0000-0000-000000000000';
 export const createAgentGuid = async ({
   jwt,
 }) => {
