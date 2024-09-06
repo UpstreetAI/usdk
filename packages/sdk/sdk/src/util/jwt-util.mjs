784b3a0a0b88bd3d64b40bfde938bc313b025e81
Avaer Kazmer <ultimus@gmail.com>
2024-09-06 02:41:01 -0700

    Remove old jwt-util.mjs in favor of jwt-utils.mjs

--- packages/sdk/sdk/src/util/jwt-util.mjs
+++
@@ -1,9 +0,0 @@
-export const getCleanJwt = () => {
-  if (typeof localStorage === 'undefined') {
-    return '';
-  }
-
-  let jwt = localStorage.getItem('jwt');
-  jwt = jwt.slice(1, jwt.length - 1);
-  return jwt;
-};
