2f31361c2108ad18b9d26b8585de92a2b397118f
Avaer Kazmer <ultimus@gmail.com>
2024-09-10 08:17:27 -0700

    Use cached messages update hook

--- packages/sdk/sdk/src/hooks.ts
+++ packages/sdk/sdk/src/hooks.ts
@@ -111,6 +111,18 @@ export const useCachedMessages = (opts?: ActionHistoryQuery) => {
   const agent = useAgent();
   const supabase = agent.useSupabase();
   const conversation = useConversation();
+  const [cachedMessagesEpoch, setCachedMessagesEpoch] = useState(0);
+
+  // listen for messasge cache updates
+  const update = () => {
+    setCachedMessagesEpoch(cachedMessagesEpoch => cachedMessagesEpoch + 1);
+  };
+  useEffect(() => {
+    conversation.messageCache.addEventListener('update', update);
+    return () => {
+      conversation.messageCache.removeEventListener('update', update);
+    };
+  }, []);
 
   if (!conversation.messageCache.loadPromise) {
     conversation.messageCache.loadPromise = (async () => {
