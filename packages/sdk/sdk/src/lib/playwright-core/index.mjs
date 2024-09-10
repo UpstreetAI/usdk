55f4eb6b7fc203142595c0352e23fcb32f23d465
Avaer Kazmer <ultimus@gmail.com>
2024-09-10 01:51:00 -0700

    Trim playwright exports

--- packages/sdk/sdk/src/lib/playwright-core/index.mjs
+++ packages/sdk/sdk/src/lib/playwright-core/index.mjs
@@ -17,12 +17,12 @@
 import playwright from './index.js';
 
 export const chromium = playwright.chromium;
-export const firefox = playwright.firefox;
-export const webkit = playwright.webkit;
-export const selectors = playwright.selectors;
-export const devices = playwright.devices;
-export const errors = playwright.errors;
-export const request = playwright.request;
-export const _electron = playwright._electron;
-export const _android = playwright._android;
+// export const firefox = playwright.firefox;
+// export const webkit = playwright.webkit;
+// export const selectors = playwright.selectors;
+// export const devices = playwright.devices;
+// export const errors = playwright.errors;
+// export const request = playwright.request;
+// export const _electron = playwright._electron;
+// export const _android = playwright._android;
 export default playwright;
