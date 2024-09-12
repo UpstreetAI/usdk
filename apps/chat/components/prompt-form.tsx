e4f31e4fcbef9cc4026ba305e2931162413e7ff7
Avaer Kazmer <ultimus@gmail.com>
2024-09-11 20:05:22 -0700

    Small prompt form cleanup

--- apps/chat/components/prompt-form.tsx
+++ apps/chat/components/prompt-form.tsx
@@ -108,7 +108,7 @@ export function PromptForm({
       // ref={formRef}
       onSubmit={async (e: any) => {
         e.preventDefault()
-        submitMessage()
+        // submitMessage()
       }}
     >
       <div className="relative flex max-h-60 w-full grow flex-col bg-background px-8 sm:rounded-md sm:border sm:px-12">
