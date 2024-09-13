8434465ddd394f6ad49b89b27414c7082f37f2ba
Avaer Kazmer <ultimus@gmail.com>
2024-09-13 04:23:24 -0700

    Another path update

--- packages/usdk/packages/upstreet-agent/main.jsx
+++ packages/usdk/packages/upstreet-agent/main.jsx
@@ -1,5 +1,5 @@
 export { DurableObject } from './durable-object.jsx';
-import { headers } from './src/constants.mjs';
+import { headers } from './packages/react-agents/constants.mjs';
 
 async function handleAgentRequest(request, env) {
   const guid = env.GUID;
