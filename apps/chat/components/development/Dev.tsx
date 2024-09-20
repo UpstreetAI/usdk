4a291c665221a79e94306b2864fd4704dc93d82a
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-09-20 14:29:09 +0400

    Add dev redirect

--- apps/chat/components/development/Dev.tsx
+++ apps/chat/components/development/Dev.tsx
@@ -1,13 +1,20 @@
 'use client';
 
 import { useGlobalState } from "@/contexts/GlobalContext";
+import { redirect } from 'next/navigation';
 
 interface ChatLayoutProps {
   children: React.ReactNode
 }
 
-export function Dev({ children }: ChatLayoutProps)  {
+export function Dev({ children }: ChatLayoutProps) {
   const [globalState] = useGlobalState();
 
   return globalState.isDevMode ? children : null;
+}
+
+export function DevRedirect() {
+  const [globalState] = useGlobalState();
+
+  return !globalState.isDevMode ? redirect('/') : null;
 }
