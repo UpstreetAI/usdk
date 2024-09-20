0222fba853f90b72ade72396910beb0cef5559b1
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-09-20 14:29:23 +0400

    Redirect from /new to home in production

--- apps/chat/app/new/page.tsx
+++ apps/chat/app/new/page.tsx
@@ -1,6 +1,7 @@
 import { LoginRedirect } from '@/components/login-redirect';
 import AgentEditor from './editor';
 import { waitForUser } from '@/utils/supabase/server';
+import { DevRedirect } from '@/components/development/Dev';
 
 async function NewPageInner() {
   const user = await waitForUser();
@@ -12,6 +13,7 @@ async function NewPageInner() {
 export default async function NewPage() {
   return (
     <>
+      <DevRedirect />
       <LoginRedirect />
       <NewPageInner />
     </>
