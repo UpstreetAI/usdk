33f1471472d156d696ffb8665e041b9884e2954f
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-09-20 13:54:59 +0400

    Update styles, add assets

--- apps/chat/app/accounts/[id]/page.tsx
+++ apps/chat/app/accounts/[id]/page.tsx
@@ -1,11 +1,14 @@
 
 import { type Metadata } from 'next';
-import { notFound } from 'next/navigation'
+import { notFound } from 'next/navigation';
+import Image from 'next/image';
 import { AgentProfile } from '@/components/agent';
 // import { createClient } from '@/utils/supabase/server';
 import { makeAnonymousClient } from '@/utils/supabase/supabase-client';
 import { env } from '@/lib/env'
 import { AgentRow } from '@/components/agents/AgentRow';
+import { isValidUrl } from '@/lib/utils';
+import HeaderMaskFrame from '@/components/masks/HeaderMaskFrame';
 
 type Params = {
   params: {
@@ -83,12 +86,45 @@ export default async function AccountProfilePage({ params }: Params) {
     .eq('id', accountId)
     .single();
 
-  if (!agentData?.id) return <div className="w-full max-w-2xl mx-auto p-8 text-center">Account Not Found</div>;
-
+  if (!agentData?.id) {
+    return (
+      <div className="w-full max-w-2xl mx-auto p-8 text-center">
+        Account Not Found
+      </div>
+    );
+  }
   return (
-    <>
-      <h1>Account Public Profile: {agentData.name}</h1>
-      {agentData.agents.map((agent: { id: string; name: string }) => <AgentRow key={agent.id} agent={agent} author={agentData.name} />)}
-    </>
+    <div className="w-full mx-auto">
+
+      <HeaderMaskFrame>
+        <div className="w-full bg-blue-500 h-52" />
+      </HeaderMaskFrame>
+
+      <div className="w-full max-w-6xl mx-auto pt-20 relative">
+        <div className="flex">
+          <div className="mr-4 mb-4 size-32 min-w-12 bg-gray-200 p-4 overflow-hidden rounded-[8px] flex items-center justify-center">
+            {isValidUrl(agentData.preview_url) ? (
+              <Image src={agentData?.preview_url} alt="Profile picture" width={160} height={160} />
+            ) : (
+              <div className="uppercase text-lg font-bold">{agentData.name.charAt(0)}</div>
+            )}
+          </div>
+
+          <div>
+            <h2 className="text-[28px] uppercase font-bold">{agentData.name}</h2>
+            <h3 className="text-sm mb-6">{agentData.id}</h3>
+
+          </div>
+
+        </div>
+
+        <h1>Account Public Profile: {agentData.name}</h1>
+        <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4`}>
+          {agentData.agents.map((agent: { id: string; name: string }) => <AgentRow key={agent.id} agent={agent} author={agentData.name} />)}
+        </div>
+
+      </div>
+
+    </div>
   );
 }
--- apps/chat/components/masks/HeaderMaskFrame.tsx
+++ apps/chat/components/masks/HeaderMaskFrame.tsx
@@ -2,8 +2,9 @@ import styles from './HeaderMaskFrame.module.css';
 
 const HeaderMaskFrame = ({ children }: any) => {
   return (
-    <div style={{ zIndex: '0', position: 'relative' }}>
+    <div style={{ zIndex: '0', position: 'absolute' }} className="w-full">
         <div className={styles.maskFrameContentWrap}>{children}</div>
+        <div className={styles.maskFrameOverlay} />
     </div>
   );
 };
