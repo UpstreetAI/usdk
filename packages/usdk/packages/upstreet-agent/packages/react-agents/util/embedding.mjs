f105c8f5743bd8c883be12ddd04e87f5619409ad
Avaer Kazmer <ultimus@gmail.com>
2024-09-20 11:36:28 -0700

    Add embeddings client retries

--- packages/usdk/packages/upstreet-agent/packages/react-agents/util/embedding.mjs
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/util/embedding.mjs
@@ -1,29 +1,35 @@
 import { aiProxyHost } from './endpoints.mjs';
+import {
+  retry,
+} from './util.mjs';
 
+const numRetries = 3;
 export const embed = async (s, opts) => {
   const { signal, jwt } = opts;
   if (!jwt) {
     throw new Error('no jwt');
   }
 
-  const fd = new FormData();
-  fd.append('s', s);
-  const url = `https://${aiProxyHost}/embedding`;
-  const fetchData = {
-    method: 'POST',
-    body: fd,
-    signal,
-    headers: {
-      Authorization: `Bearer ${jwt}`,
-    },
-  };
-  const res = await fetch(url, fetchData);
-  if (res.ok) {
-    const j = await res.json();
-    return j;
-  } else {
-    throw new Error(`invalid embed response: ${res.status}`);
-  }
+  return await retry(async () => {
+    const fd = new FormData();
+    fd.append('s', s);
+    const url = `https://${aiProxyHost}/embedding`;
+    const fetchData = {
+      method: 'POST',
+      body: fd,
+      signal,
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    };
+    const res = await fetch(url, fetchData);
+    if (res.ok) {
+      const j = await res.json();
+      return j;
+    } else {
+      throw new Error(`invalid embed response: ${res.status}`);
+    }
+  }, numRetries);
 };
 
 export const oembed = async (s, opts) => {
@@ -32,29 +38,31 @@ export const oembed = async (s, opts) => {
     throw new Error('no jwt');
   }
 
-  const body = {
-    input: s,
-    model: 'text-embedding-3-small',
-  };
-  const url = `https://${aiProxyHost}/api/ai/embeddings`;
-  const fetchData = {
-    method: 'POST',
-    body: JSON.stringify(body),
-    signal,
-    headers: {
-      Authorization: `Bearer ${jwt}`,
-    },
-  };
-  const res = await fetch(url, fetchData);
-  if (res.ok) {
-    const j = await res.json();
-    const data = j.data;
-    if (data && data.length) {
-      return data[0].embedding;
+  return await retry(async () => {
+    const body = {
+      input: s,
+      model: 'text-embedding-3-small',
+    };
+    const url = `https://${aiProxyHost}/api/ai/embeddings`;
+    const fetchData = {
+      method: 'POST',
+      body: JSON.stringify(body),
+      signal,
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    };
+    const res = await fetch(url, fetchData);
+    if (res.ok) {
+      const j = await res.json();
+      const data = j.data;
+      if (data && data.length) {
+        return data[0].embedding;
+      }
+    } else {
+      throw new Error(`invalid embed response: ${res.status}`);
     }
-  } else {
-    throw new Error(`invalid embed response: ${res.status}`);
-  }
+  }, numRetries);
 };
 export const lembed = async (s, opts) => {
   const { signal, jwt } = opts;
@@ -62,29 +70,31 @@ export const lembed = async (s, opts) => {
     throw new Error('no jwt');
   }
 
-  const body = {
-    input: s,
-    model: 'text-embedding-3-large',
-  }
-  const url = `https://${aiProxyHost}/api/ai/embeddings`;
-  const fetchData = {
-    method: 'POST',
-    body: JSON.stringify(body),
-    signal,
-    headers: {
-      Authorization: `Bearer ${jwt}`,
-    },
-  };
-  const res = await fetch(url, fetchData);
-  if (res.ok) {
-    const j = await res.json();
-    const data = j.data;
-    if (data && data.length) {
-      return data[0].embedding;
+  return await retry(async () => {
+    const body = {
+      input: s,
+      model: 'text-embedding-3-large',
     }
-  } else {
-    throw new Error(`invalid embed response: ${res.status}`);
-  }
+    const url = `https://${aiProxyHost}/api/ai/embeddings`;
+    const fetchData = {
+      method: 'POST',
+      body: JSON.stringify(body),
+      signal,
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    };
+    const res = await fetch(url, fetchData);
+    if (res.ok) {
+      const j = await res.json();
+      const data = j.data;
+      if (data && data.length) {
+        return data[0].embedding;
+      }
+    } else {
+      throw new Error(`invalid embed response: ${res.status}`);
+    }
+  }, numRetries);
 };
 
 /* export const split = async (s, opts) => {
