a798caea0f9272b509652b7a6fb9aa594757773d
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-09-23 21:39:26 +0400

    Fix imports and add a correct url to account page

--- apps/chat/app/accounts/[id]/page.tsx
+++ apps/chat/app/accounts/[id]/page.tsx
@@ -1,10 +1,8 @@
-
 import { type Metadata } from 'next';
 import { makeAnonymousClient } from '@/utils/supabase/supabase-client';
-import { env } from '@/lib/env'
-import { AgentRow } from '@/components/agents/list/AgentRow';
-import { isValidUrl } from '@/lib/utils';
-import HeaderMaskFrame from '@/components/masks/HeaderMaskFrame';
+import { env } from '@/lib/env';
+import { Account } from '@/components/account';
+import { AccountProfile } from '@/components/accounts/profile';
 
 type Params = {
   params: {
@@ -15,14 +13,8 @@ type Params = {
 export async function generateMetadata({
   params
 }: Params): Promise<Metadata> {
-  // const supabase = createClient();
-  const supabase = makeAnonymousClient(env);
-  // const {
-  //   data: { user }
-  // } = await supabase.auth.getUser();
 
-  // decode the uri for usernames that have spaces in them
-  // const agentName = decodeURIComponent(params.id)
+  const supabase = makeAnonymousClient(env);
   const agentId = decodeURIComponent(params.id)
 
   const { data: accountData } = await supabase
@@ -82,8 +74,6 @@ export default async function AccountProfilePage({ params }: Params) {
     .eq('id', accountId)
     .single();
 
-    const fullUrl = `${window.location.origin}/accounts/${accountId}`;
-
   if (!accountData?.id) {
     return (
       <div className="w-full max-w-2xl mx-auto p-8 text-center">
@@ -91,61 +81,5 @@ export default async function AccountProfilePage({ params }: Params) {
       </div>
     );
   }
-  return (
-    <div className="w-full mx-auto">
-
-      <HeaderMaskFrame>
-        <div className="w-full bg-blue-500 h-52" />
-        <div
-          className="w-full h-52 absolute top-0 left-0 bg-cover bg-center opacity-20"
-          style={{
-            backgroundImage: accountData.agents.length > 0
-              ? `url(${accountData.agents[Math.floor(Math.random() * accountData.agents.length)].images[0].url})`
-              : 'none',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center'
-          }}
-        />
-      </HeaderMaskFrame>
-
-      <div className="w-full max-w-6xl mx-auto pt-24 relative">
-        <div className="flex">
-          <div className="mr-4 size-40 min-w-12 bg-gray-100 p-4 overflow-hidden flex items-center justify-center border-2 border-gray-900">
-            <div
-              className="w-full h-full bg-cover bg-center"
-              style={{
-                backgroundImage: isValidUrl(accountData.preview_url)
-                  ? `url(${accountData.preview_url})`
-                  : 'none',
-                backgroundColor: isValidUrl(accountData.preview_url) ? 'transparent' : '#ccc',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                fontSize: '2rem',
-                fontWeight: 'bold',
-                color: '#fff',
-              }}
-            >
-              {!isValidUrl(accountData.preview_url) && accountData.name.charAt(0)}
-            </div>
-          </div>
-
-          <div>
-            <h2 className="text-4xl uppercase text-stroke font-bold">{accountData.name}</h2>
-            <a href={`/accounts/${accountData.id}`} className="text-primary hover:underline">
-              {accountData.name}
-              </a>
-
-          </div>
-
-        </div>
-
-        <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-8`}>
-          {accountData.agents.map((agent: { id: string; name: string }) => <AgentRow key={agent.id} agent={agent} author={accountData.name} />)}
-        </div>
-
-      </div>
-
-    </div>
-  );
+  return (<AccountProfile account={accountData} />);
 }
