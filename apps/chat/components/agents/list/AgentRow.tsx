a81f6963d52e743a3333e05cd6b4b1713b520fa1
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-09-26 01:17:23 +0400

    Update skeleton

--- apps/chat/components/agents/list/AgentRow.tsx
+++ apps/chat/components/agents/list/AgentRow.tsx
@@ -70,24 +70,21 @@ export function AgentRow({ agent, author }: AgentListProps) {
 
 export function SkeletonAgentRow() {
   return (
-    <div className="bg-[rgba(255,255,255,0.1)] border rounded-lg p-6 animate-pulse">
-      <div className="flex mb-2">
-        <div className="mr-4 mb-2 size-[80px] min-w-[80px] bg-[rgba(255,255,255,0.2)] rounded-full flex items-center justify-center overflow-hidden">
-          <div className="h-full w-full bg-gray-300 rounded-full"></div>
-        </div>
-        <div className="min-w-40 text-md capitalize w-full">
-          <div className="bg-gray-300 rounded h-3 mb-2 w-3/4"></div>
-          <div className="bg-gray-300 rounded h-2 w-1/2"></div>
-        </div>
+    <div className="bg-gray-100 border p-4 text-black">
+    <div className="flex">
+      <div className="mr-4 size-[160px] min-w-[160px] flex items-center justify-center bg-cover">
+        <div className="h-full w-full bg-gray-300"></div>
       </div>
-      <div className="flex">
-        <div className="mt-2 text-gray-400 line-clamp-1 w-full">
-          <div className="bg-gray-300 rounded h-4 w-1/3 inline-block"></div>
-        </div>
-        <div className="mt-2 text-right">
-          <div className="bg-gray-300 rounded h-4 w-1/5 inline-block"></div>
+      <div className="min-w-40 text-md capitalize w-full relative">
+        <div className="bg-gray-300 rounded h-6 mb-4 w-3/4"></div>
+        <div className="bg-gray-300 rounded h-4 mb-2 w-full"></div>
+        <div className="bg-gray-300 rounded h-4 mb-2 w-full"></div>
+        <div className="bg-gray-300 rounded h-4 mb-2 w-full"></div>
+        <div className="text-center ml-6 absolute bottom-0 right-0">
+          <div className="bg-gray-300 rounded h-3 w-10" />
         </div>
       </div>
     </div>
+  </div>
   );
 }
