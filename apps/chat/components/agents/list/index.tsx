2f5df49611c1e80194b7dff61bdb0c7cbad109fc
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-09-26 01:37:21 +0400

    Fix load more duplication
    Issue was the fetchData was called before the range states had time to update

--- apps/chat/components/agents/list/index.tsx
+++ apps/chat/components/agents/list/index.tsx
@@ -4,11 +4,10 @@ import React, { useEffect, useState } from 'react';
 import { AgentList } from './AgentList';
 import { useSupabase } from '@/lib/hooks/use-supabase';
 import { Button } from '@/components/ui/button';
-import { SkeletonAgentRow } from './AgentRow';
 
 export interface AgentsProps {
-  loadmore: boolean
-  range: number
+  loadmore: boolean;
+  range: number;
 }
 
 export interface Agent {
@@ -28,18 +27,23 @@ export function Agents({ loadmore = false, range = 5 }: AgentsProps) {
   const [searchTerm, setSearchTerm] = useState('');
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState(searchTerm);
 
-  const fetchData = async (reset = false) => {
-
-    setLoading(true);
+  const fetchData = async (
+    reset = false,
+    customRangeFrom = rangeFrom,
+    customRangeTo = rangeTo
+  ) => {
+    if (searchTerm !== '') {
+      setLoading(true);
+    }
 
     const { data, error } = await supabase
       .from('assets')
       .select('*, author: accounts ( id, name )')
       .eq('origin', 'sdk')
       .ilike('name', `%${debouncedSearchTerm}%`)
-      .range(rangeFrom, rangeTo - 1)
+      .range(customRangeFrom, customRangeTo - 1)
       .order('created_at', { ascending: false });
-      
+
     if (!error) {
       if (reset) {
         setAgents(data);
@@ -73,46 +77,53 @@ export function Agents({ loadmore = false, range = 5 }: AgentsProps) {
   }, [searchTerm]);
 
   useEffect(() => {
-    fetchData(true);
+    // Reset the range when search term changes
+    setRangeFrom(0);
+    setRangeTo(range);
+    fetchData(true, 0, range);
   }, [debouncedSearchTerm]);
- 
+
   const handleLoadMore = () => {
+    const newRangeFrom = rangeTo;
+    const newRangeTo = rangeTo + range;
+
+    setRangeFrom(newRangeFrom);
+    setRangeTo(newRangeTo);
+
     setLoadingMore(true);
-    setRangeFrom(rangeTo);
-    setRangeTo(rangeTo + range);
-    fetchData();
+    fetchData(false, newRangeFrom, newRangeTo);
   };
 
   return (
     <>
       <div className='flex mb-4'>
-        <h1 className='text-3xl font-bold text-left text-[#2D4155] w-full'>Agents</h1>
+        <h1 className='text-3xl font-bold text-left text-[#2D4155] w-full'>
+          Agents
+        </h1>
         <input
-          type="text"
-          placeholder="Search agents..."
+          type='text'
+          placeholder='Search agents...'
           value={searchTerm}
           className='w-60 px-4 py-2 bg-gray-100 border-2 border-gray-900 text-gray-900 text-sm'
           onChange={(e) => {
             setSearchTerm(e.target.value);
-            setRangeFrom(0);
-            setRangeTo(range);
           }}
         />
       </div>
 
-      <div className={`grid grid-cols-1 md:grid-cols-2 gap-4`}>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
         <AgentList agents={agents} loading={loading} range={range} />
       </div>
       <div className='text-center'>
-        {!agents.length || showLoadMore &&
+        {agents.length > 0 && showLoadMore && (
           <Button
             disabled={loadingMore}
             className='cursor-pointer h-auto mt-10 bg-[#ff38ae] inline-block hover:opacity-[0.6] text-xl font-bold text-white px-8 py-4 rounded-md mr-2 mb-2'
             onClick={handleLoadMore}
           >
-            {loadingMore ? "Loading agents..." : "Load More"}
+            {loadingMore ? 'Loading agents...' : 'Load More'}
           </Button>
-        }
+        )}
       </div>
     </>
   );
