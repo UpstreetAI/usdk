717edd05e217ca691bcf2bcf4a6b7a2d6d6f81a5
Avaer Kazmer <ultimus@gmail.com>
2024-09-27 11:22:16 -0700

    Major update to telnyx client

--- packages/usdk/packages/upstreet-agent/packages/react-agents/lib/telnyx/telnyx-client.ts
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/lib/telnyx/telnyx-client.ts
@@ -2,27 +2,27 @@ import {
   zbencode,
   zbdecode,
 } from '../../lib/zjs/encoding.mjs';
-import {
-  QueueManager,
-} from '../../util/queue-manager.mjs';
+// import {
+//   QueueManager,
+// } from '../../util/queue-manager.mjs';
 // import {
 //   OpusDecoder,
 // } from 'opus-decoder';
 // import {
 //   whisperTranscribe,
 // } from '../clients/whisper-client.js';
-import {
-  transcribe,
-} from '../../util/audio-perception.mjs';
+// import {
+//   transcribe,
+// } from '../../util/audio-perception.mjs';
 // import audioBufferToWav from 'audiobuffer-to-wav';
-import {
-  createOpusDecodeTransformStream,
-  createMp3ReadableStreamSource,
-} from '../../lib/multiplayer/public/audio/audio-client.mjs';
-import {
-  makePromise,
-  makeId,
-} from '../../util/util.mjs';
+// import {
+//   createOpusDecodeTransformStream,
+//   createMp3ReadableStreamSource,
+// } from '../../lib/multiplayer/public/audio/audio-client.mjs';
+// import {
+//   makePromise,
+//   makeId,
+// } from '../../util/util.mjs';
 import {
   telnyxEndpointUrl,
 } from '../../util/endpoints.mjs';
@@ -39,15 +39,23 @@ export class TelnyxClient extends EventTarget {
     this.apiKey = apiKey;
   }
   async status() {
+    console.log('get status', {
+      apiKey: this.apiKey,
+    });
     const res = await fetch(`${telnyxEndpointUrl}/status`, {
       headers: {
         'Authorization': `Bearer ${this.apiKey}`,
       },
     });
-    const j = await res.json();
-    return j as {
-      phoneNumbers: string[],
-    };
+    if (res.ok) {
+      const j = await res.json();
+      return j as {
+        phoneNumbers: string[],
+      };
+    } else {
+      const text = await res.text();
+      throw new Error('invalid status code: ' + res.status + ': ' + text);
+    }
   }
   async connect({
     phoneNumbers,
@@ -57,7 +65,7 @@ export class TelnyxClient extends EventTarget {
     const u = (() => {
       const u = new URL(telnyxEndpointUrl.replace(/^http/, 'ws'));
       u.searchParams.set('apiKey', this.apiKey);
-      u.searchParams.set('phoneNumbers', JSON.stringify(phoneNumbers));
+      phoneNumbers.length > 0 && u.searchParams.set('phoneNumbers', JSON.stringify(phoneNumbers));
       return u;
     })();
     const ws = new WebSocket(u);
@@ -78,6 +86,14 @@ export class TelnyxClient extends EventTarget {
               from,
               to,
             });
+            // const o = {
+            //   method: 'message',
+            //   args: {
+            //     to: from.phone_number,
+            //     text: text && `Reply: ${text}`,
+            //     media_urls: media.map((m) => m.url),
+            //   },
+            // };
             break;
           }
           case 'call.initiated': {
@@ -147,37 +163,46 @@ export class TelnyxClient extends EventTarget {
       ws.addEventListener('close', handleClose);
     });
   }
-
-  send(text: string, {
-    callId,
+  send(text: string, mediaUrls: string[] = [], {
+    fromPhoneNumber,
+    toPhoneNumber,
   }: {
-    callId: string,
+    fromPhoneNumber: string,
+    toPhoneNumber: string,
   }) {
     const o = {
       method: 'message',
       args: {
-        to: from.phone_number,
-        text: text && `Reply: ${text}`,
-        media_urls: media.map((m) => m.url),
+        from: fromPhoneNumber,
+        to: toPhoneNumber,
+        text: text ? text : undefined,
+        media_urls: mediaUrls.length > 0 ? mediaUrls : undefined,
       },
     };
-    console.log('reply with', o);
     this.ws.send(JSON.stringify(o));
   }
-
-  call(phoneNumber: string) {
-    console.log('call 1');
+  call({
+    toPhoneNumber,
+    fromPhoneNumber,
+  }: {
+    toPhoneNumber: string,
+    fromPhoneNumber: string,
+  }) {
+    console.log('call 1', {
+      toPhoneNumber,
+      fromPhoneNumber,
+    });
     this.ws.send(
       JSON.stringify({
         method: 'call',
         args: {
-          to: phoneNumber,
+          from: fromPhoneNumber,
+          to: toPhoneNumber,
         },
       }),
     );
     console.log('call 2');
   }
-
   destroy() {
     this.ws && this.ws.close();
   }
