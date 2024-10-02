ab22ab034af10c7d15a5760f42809bf9e91f99f5
Avaer Kazmer <ultimus@gmail.com>
2024-10-01 19:21:44 -0700

    Bugfix missing execFile await

--- packages/usdk/lib/npm-util.mjs
+++ packages/usdk/lib/npm-util.mjs
@@ -4,7 +4,7 @@ import util from 'util';
 const execFile = util.promisify(child_process.execFile);
 
 export const npmInstall = async (dstDir) => {
-  execFile('npm', ['install'], {
+  await execFile('npm', ['install'], {
     cwd: dstDir,
     stdio: 'inherit',
   });  
