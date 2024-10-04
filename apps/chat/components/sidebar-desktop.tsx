5f76af27e327a692dec7b07507292e97caf7dd6d
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-04 15:56:49 +0400

    Update styles

--- apps/chat/components/sidebar-desktop.tsx
+++ apps/chat/components/sidebar-desktop.tsx
@@ -15,7 +15,7 @@ export async function SidebarDesktopLeft() {
 
 export async function SidebarDesktopRight() {
   return (
-    <Sidebar position="right" className="hidden md:block peer peerRight absolute inset-y-0 z-30 hidden translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:w-[250px] xl:w-[300px] right-0">
+    <Sidebar position="right" className="hidden md:block peer peerRight absolute inset-y-0 z-30 hidden translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 w-[300px] lg:w-[300px] xl:w-[300px] right-0">
       <RightSidebarToggle />
       <RoomUi />
     </Sidebar>
