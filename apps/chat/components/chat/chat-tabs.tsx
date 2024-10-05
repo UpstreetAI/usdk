d7cb66feea451c500415dc3a918277812c6c5dc4
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-05 20:07:53 +0400

    Update tab styles

--- apps/chat/components/chat/chat-tabs.tsx
+++ apps/chat/components/chat/chat-tabs.tsx
@@ -14,18 +14,21 @@ export function ChatTabs({ user }: TabsProps) {
 
   const [ tab, setTab ] = useHash('members');
 
-  const activeClass = 'text-purple-600 hover:text-purple-600 dark:text-purple-500 dark:hover:text-purple-500 border-purple-600 dark:border-purple-500';
-  const inactiveClass = 'hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 cursor-pointer';
+  const activeClass = 'text-[#2D4155] bg-[#c3c9d1]';
+  const inactiveClass = 'hover:border-b-gray-300 hover:text-[#2D4155] cursor-pointer';
 
   return (
     <div className='pb-16'>
-      <div className="mb-4 border-b border-gray-200 dark:border-gray-700 max-w-4xl w-full">
-        <ul className="flex overflow-x-scroll md:overflow-x-auto md:flex-wrap whitespace-nowrap -mb-px text-sm font-medium text-center" role="tablist">
+      <div className="mb-4 border-b-2 border-gray-400 max-w-4xl w-full">
+        <div className='h-40'>
+
+        </div>
+        <ul className="flex overflow-x-scroll md:overflow-x-auto md:flex-wrap whitespace-nowrap -mb-[2px] text-sm font-medium text-center px-4" role="tablist">
           <li className="me-2" role="presentation">
-            <button onClick={() => { setTab('info'); }} className={`inline-block p-4 border-b-2 rounded-t-lg ${tab === 'profile' ? activeClass : inactiveClass}`}>Info</button>
+            <button onClick={() => { setTab('info'); }} className={`inline-block uppercase py-2 px-4 border-2 border-b-0 border-gray-400 ${tab === 'info' ? activeClass : inactiveClass}`}>Info</button>
           </li>
           <li className="me-2" role="presentation">
-            <button onClick={() => { setTab('members'); }} className={`inline-block p-4 border-b-2 rounded-t-lg ${tab === 'agents' ? activeClass : inactiveClass}`}>Members</button>
+            <button onClick={() => { setTab('members'); }} className={`inline-block uppercase py-2 px-4 border-2 border-b-0 border-gray-400 ${tab === 'members' ? activeClass : inactiveClass}`}>Members</button>
           </li>
         </ul>
       </div>
