ee9430f07d389ad6ca3a51c35111a0cd74cb2224
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-06 20:30:58 +0400

    Update header mask

--- apps/chat/components/masks/HeaderMaskFrame.tsx
+++ apps/chat/components/masks/HeaderMaskFrame.tsx
@@ -1,10 +1,19 @@
 import styles from './HeaderMaskFrame.module.css';
 
-const HeaderMaskFrame = ({ children }: any) => {
+const HeaderMaskFrame = ({ children, background }: any) => {
   return (
-    <div style={{ zIndex: '0', position: 'absolute' }} className="w-full">
-        <div className={styles.maskFrameContentWrap}>{children}</div>
-        <div className={styles.maskFrameOverlay} />
+    <div className="relative w-full">
+      <div 
+        className="w-full h-full absolute top-0 left-0 bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          zIndex: '0'
+        }} 
+      />
+      <div className={styles.maskFrameOverlay} />
+      <div className='relative z-2'>{children}</div>
     </div>
   );
 };
