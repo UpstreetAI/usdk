07d22a3c9a5c7b5cbcb5d5e2660f194aa9513428
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-06 20:31:08 +0400

    update chat-head styles

--- apps/chat/components/chat/chat-head.tsx
+++ apps/chat/components/chat/chat-head.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { useMultiplayerActions } from '@/components/ui/multiplayer-actions';
 import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip';
+import HeaderMaskFrame from '../masks/HeaderMaskFrame';
 
 export function ChatHead() {
 
@@ -17,36 +18,41 @@ export function ChatHead() {
   const roomLink = typeof window !== 'undefined' ? window.location.href : '';
 
   return (
-    <div className="flex mt-5 flex-col justify-start px-4 pb-4 gap-1 rounded-md border-1">
-      {roomName && (
-        <span className="select-none font-black text-2xl flex justify-between w-full items-center">
-          {roomName}
-        </span>
-      )}
-      {roomDescription && (
-        <span className="select-none text-sm font-medium flex justify-between w-full items-center">
-          {roomDescription}
-        </span>
-      )}
+    <HeaderMaskFrame
+      background="/images/backgrounds/rooms/default-bg.jpg"
+    >
+      <div className="flex flex-col justify-start gap-1 rounded-md border-1 mb-2 p-4 pt-32">
 
-      <span className="select-none opacity-70 text-xs font-medium flex justify-between w-full items-center">
-        Chat joining link
-      </span>
-      <Tooltip open={showRoomLinkTooltip}>
-        <TooltipTrigger>
-          <span
-            onClick={() => {
-              navigator.clipboard.writeText(roomLink)
-              setShowRoomLinkTooltip(true)
-              setTimeout(() => setShowRoomLinkTooltip(false), 1500)
-            }}
-            className="text-sm overflow-hidden text-ellipsis line-clamp-1"
-          >
-            {roomLink}
+        <span className="select-none font-black text-2xl flex justify-between w-full items-center">
+          {roomName ? roomName : '156th Street'}
+        </span>
+
+        {roomDescription && (
+          <span className="select-none text-sm font-medium flex justify-between w-full items-center">
+            {roomDescription}
           </span>
-        </TooltipTrigger>
-        <TooltipContent className="z-50">Copied chat link!</TooltipContent>
-      </Tooltip>
-    </div>
+        )}
+
+        <span className="select-none opacity-70 text-xs font-medium flex justify-between w-full items-center">
+          Chat joining link
+        </span>
+        <Tooltip open={showRoomLinkTooltip}>
+          <TooltipTrigger>
+            <span
+              onClick={() => {
+                navigator.clipboard.writeText(roomLink)
+                setShowRoomLinkTooltip(true)
+                setTimeout(() => setShowRoomLinkTooltip(false), 1500)
+              }}
+              className="text-sm overflow-hidden text-ellipsis line-clamp-1"
+            >
+              {roomLink}
+            </span>
+          </TooltipTrigger>
+          <TooltipContent className="z-50 bg-gray-800">Copied chat link!</TooltipContent>
+        </Tooltip>
+      </div>
+
+    </HeaderMaskFrame>
   );
 };
