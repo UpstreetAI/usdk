90deb3559bbb29673504dbe86ce626a550945d12
Avaer Kazmer <ultimus@gmail.com>
2024-10-06 16:04:47 -0700

    Add react-agents-client secrets.mjs

---
+++ packages/usdk/packages/upstreet-agent/packages/react-agents-client/secrets.mjs
@@ -0,0 +1,4 @@
+export default {
+  "SUPABASE_URL": "https://friddlbqibjnxjoxeocc.supabase.co",
+  "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyaWRkbGJxaWJqbnhqb3hlb2NjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM2NjE3NDIsImV4cCI6MjAxOTIzNzc0Mn0.jnvk5X27yFTcJ6jsCkuXOog1ZN825md4clvWuGQ8DMI"
+};
