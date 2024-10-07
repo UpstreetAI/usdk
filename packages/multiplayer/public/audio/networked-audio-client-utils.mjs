7c533020626ed537aa517ca5a1dc8c61fd75f5b0
Avaer Kazmer <ultimus@gmail.com>
2024-10-06 17:38:12 -0700

    Remove m=root multiplayer lib

--- packages/multiplayer/public/audio/networked-audio-client-utils.mjs
+++
@@ -1,9 +0,0 @@
-import {UPDATE_METHODS} from '../update-types.mjs';
-
-export const handlesMethod = method => {
-  return [
-    UPDATE_METHODS.AUDIO,
-    UPDATE_METHODS.AUDIO_START,
-    UPDATE_METHODS.AUDIO_END,
-  ].includes(method);
-};
