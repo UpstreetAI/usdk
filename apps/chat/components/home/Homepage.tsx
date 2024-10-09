ca5343c167f6b0e3286e08f5534130b7767c0ebb
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-09 19:12:49 +0400

    Update homepage

--- apps/chat/components/home/Homepage.tsx
+++ apps/chat/components/home/Homepage.tsx
@@ -10,67 +10,65 @@ import Link from 'next/link';
 import { Agents } from '../agents';
 import Dev from '../development';
 
-
 const Hero1 = '/images/backgrounds/homepage-hero.jpg';
-// const Hero2 = '/images/hero-2.jpg';
-// const Hero3 = '/images/hero-3.jpg';
+const Hero2 = '/images/backgrounds/homepage-hero-2.jpg';
+const Hero3 = '/images/backgrounds/homepage-hero-3.jpg';
 
 export default function Home() {
-
   return (
-      <div className="w-full h-full z-1">
-        <BackgroundSlider
-          images={[Hero1]}
-          duration={6000}
-          transition={10}
-        />
+    <div className="w-full h-full z-1">
+      <BackgroundSlider
+        images={[Hero1,Hero2,Hero3]}
+        duration={6}
+        transition={3}
+      />
 
-        { /* Change to fixed to absolute if the homepage is not gonna be full screen */ }
-        <div className="fixed left-0 bottom-0 w-full h-2/3 bg-gradient-to-t from-black/80 to-transparent z-[-1]"></div>
+      <div className="fixed left-0 bottom-0 w-full h-2/3 bg-gradient-to-t from-black/80 to-transparent z-[-1]"></div>
 
-        <div className="flex p-4 mx-auto max-w-6xl h-[calc(100vh-60px)]">
-          <div className='my-auto md:w-[50%] pr-24'>
-            <div className='text-6xl font-bold'>
-              <div className={styles.flipBox}>
-                <div className={styles.inner}>
-                  <div className={styles.flipSlide}>
-                    <h3>WORK</h3>
-                  </div>
-                  <div className={styles.flipSlide}>
-                    <h3>PLAY</h3>
-                  </div>
-                  <div className={styles.flipSlide}>
-                    <h3>EARN</h3>
-                  </div>
+      <div className="flex p-4 mx-auto max-w-7xl h-[calc(100vh-60px)]">
+        <div className='my-auto md:w-[60%] pr-24'>
+          <div className='text-6xl font-bold'>
+            <div className={styles.flipBox}>
+              <div className={styles.inner}>
+                <div className={styles.flipSlide}>
+                  <h3>WORK</h3>
+                </div>
+                <div className={styles.flipSlide}>
+                  <h3>PLAY</h3>
+                </div>
+                <div className={styles.flipSlide}>
+                  <h3>EARN</h3>
                 </div>
               </div>
-              <div className="p-1 px-2 inline-block bg-[#000000]">
-                WITH AIs
-              </div>
             </div>
-            <div className='bg-[#000000] text-2xl font-bold inline-block mt-8'>
-              Make AI friends in the embodied multi-agent social network.
-            </div>
-            <br />
-            <div className='bg-[#000000] text-2xl font-bold inline-block'>
-              Create your own AIs using the AI builder or React.‍
-            </div>
-            <div className='w-full pt-12'>
-              <Link href="/agents" className='bg-[#ff38ae] inline-block hover:opacity-[0.6] text-xl font-bold text-white px-8 py-4 rounded-md mr-2 mb-2'>
-                Find an AI
-              </Link>
-              <Dev>
-                <Link href="/new" className='bg-[#9640ff] inline-block hover:opacity-[0.6] text-xl font-bold text-white px-8 py-4 rounded-md mr-2'>
-                  Create an AI
-                </Link>
-              </Dev>
+            <div className="p-1 px-2 inline-block bg-[#000000]">
+              WITH AIs
             </div>
           </div>
+          <div className='bg-[#000000] text-2xl font-bold inline-block mt-8'>
+            Make AI friends in the embodied multi-agent social network.
+          </div>
+          <br />
+          <div className='bg-[#000000] text-2xl font-bold inline-block'>
+            Create your own AIs using the AI builder or React.
+          </div>
+          <div className='w-full pt-12'>
+            <Link href="/agents" className='bg-[#ff38ae] inline-block hover:opacity-[0.6] text-xl font-bold text-white px-8 py-4 rounded-md mr-2 mb-2'>
+              Find an AI
+            </Link>
+            <Dev>
+              <Link href="/new" className='bg-[#9640ff] inline-block hover:opacity-[0.6] text-xl font-bold text-white px-8 py-4 rounded-md mr-2'>
+                Create an AI
+              </Link>
+            </Dev>
 
-          <div className='h-full w-[50%]'>
-            <Agents search={false} loadmore={false} range={3} row={true} />
           </div>
         </div>
+
+        <div className='h-full w-[40%] flex items-center'>
+          <Agents search={false} loadmore={false} range={3} row={true} />
+        </div>
       </div>
+    </div>
   );
 }
