5bdb3e47bde8ee96eff0a85891b4786bd0266075
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-12 02:42:10 +0400

    Add back button

--- apps/chat/components/chat/chat-menu.tsx
+++ apps/chat/components/chat/chat-menu.tsx
@@ -25,9 +25,14 @@ export function ChatMenu({ players, roomName }: ChatMenuProps) {
 
   return (
     <div
-      className={`fixed z-[100] bg-gray-300 top-18 left-0 w-full ease-in-out duration-300 animate-in border-b ${isLeftSidebarOpen ? 'lg:pl-[250px] xl:pl-[300px]' : ''
+      className={`fixed z-[100] bg-gray-300 left-0 w-full ease-in-out duration-300 animate-in border-b ${isLeftSidebarOpen ? 'lg:pl-[250px] xl:pl-[300px]' : ''
         } ${isRightSidebarOpen ? 'lg:pr-[250px] xl:pr-[300px]' : ''}`}
     >
+
+      <div className='absolute z-[100] left-3 md:left-4 top-1/2 transform -translate-y-1/2 mt-1'>
+        <IconButton href={document.referrer || "/"} icon={'BackArrow'}  />
+      </div>
+
       <div className="space-y-4 px-2 md:px-0 sm:max-w-2xl mx-auto relative flex">
         <div className="flex flex-row p-1 md:p-4 border-b border-gray-300 w-full">
           <div className="flex w-full items-center mb-4 md:mb-0">
@@ -46,7 +51,7 @@ export function ChatMenu({ players, roomName }: ChatMenuProps) {
                 }}
               />
             </div>
-            <h2 className="text-lg md:text-2xl font-semibold flex mt-4 md:mt-0">{roomName ? roomName : '156 Starlight Street'}</h2>
+            <h2 className="text-lg md:text-2xl font-semibold flex mt-4 md:mt-0 ml-14 md:ml-0">{roomName ? roomName : '156 Starlight Street'}</h2>
           </div>
 
           <div className="flex items-center">
