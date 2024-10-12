d656b291e4a37976b5addfa5d37c7389c6648485
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-12 12:17:48 +0400

    Update responsive styles

--- apps/chat/components/ui/Header/header-menu.tsx
+++ apps/chat/components/ui/Header/header-menu.tsx
@@ -10,15 +10,15 @@ export function HeaderMenu() {
   const pathname = usePathname();
 
   return (
-    <>
+    <div className="flex items-center h-full -mt-6 md:mt-0">
       <div className='md:m-w-[160px] md:w-[160px] pr-4 md:pr-0'>
         <a href="/" rel="nofollow" className='hidden md:block w-44 ml-4'>
           <IconUpstreetChatStroke className="mr-2 fill-black size-12 w-auto dark:hidden" inverted />
           <IconUpstreetChatStroke className="hidden mr-2 fill-white size-12 w-auto dark:block" />
         </a>
-        <a href="/" rel="nofollow" className='block md:hidden w-10 ml-4'>
-          <IconUpstreetStroke className="mr-2 fill-black size-12 w-auto dark:hidden" inverted />
-          <IconUpstreetStroke className="hidden mr-2 fill-white size-12 w-auto dark:block" />
+        <a href="/" rel="nofollow" className='block md:hidden w-6 ml-4'>
+          <IconUpstreetStroke className="mr-2 fill-black size-8 w-auto dark:hidden" inverted />
+          <IconUpstreetStroke className="hidden mr-2 fill-white size-8 w-auto dark:block" />
         </a>
       </div>
       <div className='hidden md:flex gap-4 ml-6'>
@@ -27,9 +27,9 @@ export function HeaderMenu() {
         <IconButton href="/accounts" icon="Head" label="Users" active={pathname.startsWith('/accounts')} />
         <IconButton href="https://docs.upstreet.ai/docs/sdk/intro" target="_blank" icon="Sdk" label="SDK" />
       </div>
-      <div className='flex md:hidden gap-4 ml-6'>
-        <IconButton icon="Menu" />
+      <div className='flex md:hidden gap-4 md:ml-6'>
+        <IconButton variant='ghost' icon="BurgerMenu" />
       </div>
-    </>
+    </div>
   );
 }
--- apps/chat/components/ui/Header/header-navigation.tsx
+++ apps/chat/components/ui/Header/header-navigation.tsx
@@ -17,16 +17,11 @@ export function HeaderNavigation({ user, credits }: HeaderNavigationProps) {
   if(pathname.startsWith('/new') || pathname.startsWith('/rooms/')) return null;
 
   return (
-    <header
-      className="sticky top-0 z-[150] flex items-center justify-between h-12 pt-6 border-b shrink-0 bg-background">
-      <div className="flex items-center h-full">
-        <HeaderMenu />
-      </div>
-      {/* <div className='md:m-w-[250px] md:w-[250px]'> */}
+    <header className="sticky top-0 z-[150] flex items-center justify-between h-12 pt-6 border-b shrink-0 bg-background">
+      <HeaderMenu />
       <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
         <AccountOrLogin user={user} credits={credits} />
       </React.Suspense>
-      {/* </div> */}
     </header>
   )
 }
