8a1077293214baece4283102bb90ba84eecb9d91
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-12 19:44:41 +0400

    fix z-index on navigation bar

--- apps/chat/components/ui/Header/header-navigation.tsx
+++ apps/chat/components/ui/Header/header-navigation.tsx
@@ -17,7 +17,7 @@ export function HeaderNavigation({ user, credits }: HeaderNavigationProps) {
   if(pathname.startsWith('/new') || pathname.startsWith('/rooms/')) return null;
 
   return (
-    <header className="sticky top-0 z-[150] flex items-center justify-between h-12 pt-6 border-b shrink-0 bg-background">
+    <header className="sticky top-0 z-[10] flex items-center justify-between h-12 pt-6 border-b shrink-0 bg-background">
       <HeaderMenu />
       <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
         <AccountOrLogin user={user} credits={credits} />
