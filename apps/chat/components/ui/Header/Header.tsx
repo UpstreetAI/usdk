e84cc57769faaf515dee79c175dfac5d9b979d84
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-12 11:50:19 +0400

    Code cleanup

--- apps/chat/components/ui/Header/Header.tsx
+++ apps/chat/components/ui/Header/Header.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 
-import { IconUpstreet, IconUpstreetChatStroke } from '@/components/ui/icons';
-import { AccountOrLogin } from '@/components/ui/Header/account-or-login';
 import { getUserForJwt } from '@/utils/supabase/supabase-client'
 import { getCredits, getJWT } from '@/utils/supabase/server';
 import { HeaderNavigation } from './header-navigation';
 
 
 export async function Header() {
-
   const user = await (async () => {
     const jwt = getJWT();
     if (jwt) {
