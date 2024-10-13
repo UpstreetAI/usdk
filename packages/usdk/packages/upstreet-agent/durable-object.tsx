3114f5d9587e1d3eb6c11b9da3802de4fc73d28d
Avaer Kazmer <ultimus@gmail.com>
2024-10-12 18:50:53 -0700

    Update durableObjectImpl abstraction

--- packages/usdk/packages/upstreet-agent/durable-object.tsx
+++ packages/usdk/packages/upstreet-agent/durable-object.tsx
@@ -1,5 +1,8 @@
 import { DurableObjectImpl } from './durable-object-impl.tsx';
 
+import userRender from '../../agent.tsx'; // note: this will be overwritten by the build process
+import * as codecs from 'react-agents/lib/multiplayer/public/audio/ws-codec-runtime-edge.mjs';
+
 Error.stackTraceLimit = 300;
 
 //
@@ -9,7 +12,11 @@ export class DurableObject {
   durableObjectImpl: DurableObjectImpl;
 
   constructor(state: any, env: any) {
-    this.durableObjectImpl = new DurableObjectImpl(state, env);
+    this.durableObjectImpl = new DurableObjectImpl({
+      ...state,
+      userRender,
+      codecs,
+    }, env);
   }
   async fetch(request: Request) {
     return await this.durableObjectImpl.fetch(request);
