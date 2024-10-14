1aa683a463da38ec6acb497cf8a6386d7af923c8
Avaer Kazmer <ultimus@gmail.com>
2024-10-13 17:53:29 -0700

    Update agent interview flow

--- packages/usdk/lib/interactor.js
+++ packages/usdk/lib/interactor.js
@@ -6,7 +6,19 @@ import { QueueManager } from '../packages/upstreet-agent/packages/react-agents/u
 
 //
 
-const generateEmptyObjectFromSchema = (schema) => {
+const makeCleanObjectFromSchema = (object, schema) => {
+  if (schema instanceof z.ZodObject) {
+    const shape = schema.shape;
+    const result = {};
+    for (const key in shape) {
+      result[key] = object[key];
+    }
+    return result;
+  } else {
+    throw new Error('invalid schema');
+  }
+}
+const makeEmptyObjectFromSchema = (schema) => {
   if (schema instanceof z.ZodObject) {
     const shape = schema.shape;
     const result = {};
@@ -28,7 +40,8 @@ export class Interactor extends EventTarget {
   messages;
   queueManager;
   constructor({
-    prompt,
+    systemPrompt,
+    userPrompt,
     object,
     objectFormat,
     jwt,
@@ -37,21 +50,38 @@ export class Interactor extends EventTarget {
 
     this.jwt = jwt;
     this.objectFormat = objectFormat;
-    this.object = object || generateEmptyObjectFromSchema(objectFormat);
+    this.object = object ?
+      makeCleanObjectFromSchema(object, objectFormat)
+    :
+      makeEmptyObjectFromSchema(objectFormat);
     this.messages = [
       {
         role: 'system',
-        content: prompt + '\n\n' +
+        content:
           dedent`\
             You are an interactive configuration assistant designed to update a JSON configuration object on behalf of the user.
             Prompt the user for a question you need answered to update the configuration object.
-            Be informal and succinct; do not simply ask for the form fields. Hide the complexity and internal state, and auto-fill details where you can.
+            Be informal and succinct; do not directly ask for the fields. Hide the complexity and internal state, and auto-fill details where you can.
             Feel free to use artistic license or ask clarifying questions.
             
             Reply with a JSON object including a response to the user, an optional update object to merge with the existing one, and a done flag when you think it's time to end the conversation.
-          `,
+          ` + '\n\n' +
+          dedent`\
+            # Instructions
+          ` + '\n' +
+          systemPrompt + '\n\n' +
+          dedent`\
+            # Initial state
+          ` + '\n' +
+          JSON.stringify(this.object, null, 2),
       },
     ];
+    if (userPrompt) {
+      this.messages.push({
+        role: 'user',
+        content: userPrompt,
+      });
+    }
     this.queueManager = new QueueManager();
   }
   async write(text = '') {
