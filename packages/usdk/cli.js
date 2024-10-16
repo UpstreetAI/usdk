76252a6210de4c942353b8f173638aa90d85baf7
Avaer Kazmer <ultimus@gmail.com>
2024-10-16 14:08:44 -0700

    Bugfix cli imports

--- packages/usdk/cli.js
+++ packages/usdk/cli.js
@@ -68,9 +68,9 @@ import {
 import { NetworkRealms } from './packages/upstreet-agent/packages/react-agents/lib/multiplayer/public/network-realms.mjs'; // XXX should be a deduplicated import, in a separate npm module
 
 import { AutoVoiceEndpoint, VoiceEndpointVoicer } from './packages/upstreet-agent/packages/react-agents/lib/voice-output/voice-endpoint-voicer.mjs';
-import { AudioDecodeStream } from 'codecs/audio-decode.mjs';
+import { AudioDecodeStream } from './packages/upstreet-agent/packages/react-agents/lib/codecs/audio-decode.mjs';
 
-import * as codecs from 'codecs/ws-codec-runtime-fs.mjs';
+import * as codecs from './packages/upstreet-agent/packages/react-agents/lib/codecs/ws-codec-runtime-fs.mjs';
 
 import { webbrowserActionsToText } from './packages/upstreet-agent/packages/react-agents/util/browser-action-utils.mjs';
 
