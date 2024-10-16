041d297fd9ee79d328694143349765f749b32505
Avaer Kazmer <ultimus@gmail.com>
2024-10-16 14:56:19 -0700

    Update jest to use piping

--- packages/usdk/lib/jest-util.mjs
+++ packages/usdk/lib/jest-util.mjs
@@ -1,19 +1,33 @@
 import child_process from 'child_process';
-import util from 'util';
 import { ensureNpmRoot } from './npm-util.mjs';
 import {
   jestBin,
 } from './locations.mjs';
 
-const execFile = util.promisify(child_process.execFile);
-
 export const runJest = async (directory) => {
   const npmRoot = await ensureNpmRoot();
-  await execFile(process.argv[0], ['--experimental-vm-modules', jestBin], {
-    stdio: 'inherit',
+
+  const child = child_process.spawn(process.argv[0], [jestBin], {
+    stdio: 'pipe',
     cwd: directory,
     env: {
       NODE_PATH: npmRoot, // needed to import modules
     },
+  });
+
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+
+  await new Promise((resolve, reject) => {
+    child.on('close', (code) => {
+      if (code !== 0) {
+        reject(new Error(`Process exited with code ${code}`));
+      } else {
+        resolve();
+      }
+    });
+    child.on('error', (err) => {
+      reject(err);
+    });
   });
 };
