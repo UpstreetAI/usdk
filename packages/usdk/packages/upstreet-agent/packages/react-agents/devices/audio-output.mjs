745a7c501738dfe85cd4508e54ab11521da600b7
Avaer Kazmer <ultimus@gmail.com>
2024-10-16 14:17:40 -0700

    Dead code cleanup

--- packages/usdk/packages/upstreet-agent/packages/react-agents/devices/audio-output.mjs
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/devices/audio-output.mjs
@@ -4,13 +4,9 @@ export class SpeakerOutputStream extends WritableStream {
   static defaultSampleRate = 44100;
 
   constructor({
-    // metadata,
     sampleRate = SpeakerOutputStream.defaultSampleRate,
   } = {}) {
     super({
-      // start: c => {
-      //   controller = c;
-      // },
       write: (chunk, controller) => {
         // console.log('speaker write', chunk);
         let uint8Array;
@@ -29,26 +25,7 @@ export class SpeakerOutputStream extends WritableStream {
       },
     });
 
-    // this.metadata = metadata;
     this.sampleRate = sampleRate;
-
-    // // let controller;
-    // // const donePromise = makePromise();
-    // const inputStream = new TransformStream({
-    //   // start: c => {
-    //   //   controller = c;
-    //   // },
-    //   transform: (chunk, controller) => {
-    //     // console.log('decode data 1', chunk);
-    //     // audioDecoder.decode(chunk);
-    //     controller.enqueue(chunk);
-    //   },
-    //   // flush: async controller => {
-    //   //   audioDecoder.decode(null);
-    //   //   await donePromise;
-    //   // },
-    // });
-    // this.inputStream = inputStream;
 
     // create the speaker
     this.speaker = new Speaker({
@@ -56,31 +33,5 @@ export class SpeakerOutputStream extends WritableStream {
       bitDepth: 16,         // 16-bit samples
       sampleRate,
     });
-    // // pump the decoder stream to the speaker
-    // (async () => {
-    //   for await (const chunk of this.decoderStream.readable) {
-    //     this.speaker.write(chunk);
-    //   }
-    //   this.speaker.end();
-    // })();
   }
-  /* #createDecoderStream() {
-    const { type, sampleRate } = this;
-    switch (type) {
-      case 'audio/mpeg': {
-        return createMp3DecodeTransformStream({
-          sampleRate,
-        });
-      }
-      default: {
-        throw new Error(`unhandled audio mime type: ${type}`);
-      }
-    }
-  } */
-  // write(data) {
-  //   this.inputStream.writable.write(data);
-  // }
-  // end() {
-  //   this.inputStream.writable.end();
-  // }
 }
