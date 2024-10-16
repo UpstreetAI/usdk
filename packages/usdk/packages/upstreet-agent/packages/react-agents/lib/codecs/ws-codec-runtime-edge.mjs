8bf6fe7d0d7c5a4bb123b83c762ef4de10ec5808
Avaer Kazmer <ultimus@gmail.com>
2024-10-16 14:01:05 -0700

    Update packages paths

--- packages/usdk/packages/upstreet-agent/packages/react-agents/lib/codecs/ws-codec-runtime-edge.mjs
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/lib/codecs/ws-codec-runtime-edge.mjs
@@ -1,9 +1,9 @@
-import libopus from './libopusjs/libopus.wasm.js';
+import libopus from './packages/libopusjs/libopus.wasm.js';
 import { makeOpusCodec } from './ws-opus-codec.mjs';
 export const WsOpusCodec = makeOpusCodec(libopus);
 
 export { WsMp3Encoder } from './ws-mp3-encoder.mjs';
 
-import MPEGDecoder from './mpg123-decoder/src/MPEGDecoder.js';
+import MPEGDecoder from './packages/mpg123-decoder/src/MPEGDecoder.js';
 import { makeMp3Decoder } from './ws-mp3-decoder.mjs';
 export const WsMp3Decoder = makeMp3Decoder(MPEGDecoder);
