8fb23aa5ee02719c7ede0a09e7efbe2835ed94a1
Avaer Kazmer <ultimus@gmail.com>
2024-10-17 12:35:48 -0700

    Move to react-agents-client.mjs

---
+++ packages/usdk/packages/upstreet-agent/packages/react-agents-client/react-agents-client.mjs
@@ -0,0 +1,27 @@
+export class ReactAgentsClient {
+  ref;
+  constructor(ref) {
+    this.ref = ref;
+  }
+  async join(room, {
+    only = false,
+  } = {}) {
+    const joinReq = await fetch(u, {
+      method: 'POST',
+      body: JSON.stringify({
+        room,
+        only: true,
+      }),
+    });
+    if (joinReq.ok) {
+      const joinJson = await joinReq.json();
+      // console.log('join json', joinJson);
+    } else {
+      const text = await joinReq.text();
+      console.warn(
+        'failed to join, status code: ' + joinReq.status + ': ' + text,
+      );
+      process.exit(1);
+    }
+  }
+}
