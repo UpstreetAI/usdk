91ee6537f011b64c39fc0d2bd402e69bd865aaa4
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-24 20:49:25 +0400

    Dead code cleanup

--- apps/chat/components/chat/chat-panel.tsx
+++ apps/chat/components/chat/chat-panel.tsx
@@ -67,75 +67,9 @@ export function ChatPanel({
 
       <div className="relative mx-auto border-t bg-gray-300 sm:px-4">
         {room && (<>
-          {/*<div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
-            {messages.length === 0 &&
-              exampleMessages.map((example, index) => (
-                <div
-                  key={example.heading}
-                  className={`cursor-pointer rounded-lg border p-4 bg-zinc-950 hover:bg-zinc-900 ${
-                    index > 1 && 'hidden md:block'
-                  }`}
-                  onClick={async () => {
-                    sendChatMessage(example.message);
-
-                    // setMessages(currentMessages => [
-                    //   ...currentMessages,
-                    //   {
-                    //     id: nanoid(),
-                    //     display: <UserMessage>{example.message}</UserMessage>
-                    //   }
-                    // ])
-
-                    // const responseMessage = await submitUserMessage(
-                    //   example.message
-                    // )
-
-                    // setMessages(currentMessages => [
-                    //   ...currentMessages,
-                    //   responseMessage
-                    // ])
-                  }}
-                >
-                  <div className="text-sm font-semibold">{example.heading}</div>
-                  <div className="text-sm text-zinc-600">
-                    {example.subheading}
-                  </div>
-                </div>
-              ))}
-          </div>*/}
-
-          {/* messages?.length >= 2 ? (
-            <div className="flex h-12 items-center justify-center">
-              <div className="flex space-x-2">
-                {title ? (
-                  <>
-                    <Button
-                      variant="outline"
-                      onClick={() => setShareDialogOpen(true)}
-                    >
-                      <IconShare className="mr-2" />
-                      Share
-                    </Button>
-                    {/*<ChatShareDialog
-                      open={shareDialogOpen}
-                      onOpenChange={setShareDialogOpen}
-                      onCopy={() => setShareDialogOpen(false)}
-                      shareChat={shareChat}
-                      chat={{
-                        id,
-                        title,
-                        messages: aiState.messages
-                      }}
-                    />}
-                  </>
-                ) : null}
-              </div>
-            </div>
-          ) : null */}
 
           <div className="space-y-4 px-4 py-2 sm:max-w-2xl mx-auto md:py-3 relative">
             <PromptForm input={input} setInput={setInput} />
-            {/* <FooterText className="hidden sm:block" /> */}
           </div>
 
           <DirectMessages />
