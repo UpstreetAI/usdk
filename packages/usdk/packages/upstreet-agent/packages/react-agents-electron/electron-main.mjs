c10d99d3c0c5aefa75984f557f06d9dc412e26c3
Avaer Kazmer <ultimus@gmail.com>
2024-10-23 18:59:54 -0700

    Rewrite electron main

--- packages/usdk/packages/upstreet-agent/packages/react-agents-electron/electron-main.mjs
+++ packages/usdk/packages/upstreet-agent/packages/react-agents-electron/electron-main.mjs
@@ -1,4 +1,4 @@
-import { BrowserWindow, desktopCapturer, screen, session } from 'electron';
+import { app, screen, session, BrowserWindow, desktopCapturer } from 'electron';
 import { Button, Key, keyboard, mouse, Point } from '@nut-tree-fork/nut-js';
 import { ReactAgentsLocalRuntime } from '../react-agents-local/local-runtime.mjs';
 
@@ -23,61 +23,49 @@ const createOTP = async (jwt) => {
 };
 
 const _main = async () => {
-  // console.log('got args', process.argv);
-  const agentSpec = JSON.parse(process.argv[2]);
-  const room = process.argv[3];
-  const jwt = process.argv[4];
-  const debug = process.argv[5] === '1';
+  const room = process.argv[2];
+  const jwt = process.argv[3];
+  const debug = process.argv[4] === '1';
 
   // console.log('got args', {
-  //   agentSpec,
   //   room,
   //   jwt,
   //   debug,
   // });
 
-  if (agentSpec.directory) {
-    // start the agent
-    const runtime = new ReactAgentsLocalRuntime(agentSpec);
-    await runtime.start({
-      debug,
+  // wait for the electron app to be ready
+  await app.whenReady();
+
+  // create the window
+  {
+    const primaryDisplay = screen.getPrimaryDisplay();
+    const { width, height } = primaryDisplay.workAreaSize;
+
+    // trade the jwt for an otp auth token
+    const authToken = await createOTP(jwt);
+    const u = new URL(`${host}/login`);
+    u.searchParams.set('auth_token', authToken);
+    u.searchParams.set('referrer_url', `${host}/rooms/${room}`);
+
+    // main window
+    const win = new BrowserWindow({
+      width,
+      height,
+      x: 0,
+      y: 0,
+      frame: false,
+      webPreferences: {
+        session: session.fromPartition('login'),
+      },
     });
-
-    console.log('electron main ready');
-
-    // create the window
-    {
-      const primaryDisplay = screen.getPrimaryDisplay();
-      const { width, height } = primaryDisplay.workAreaSize;
-
-      // trade the jwt for an otp auth token
-      const authToken = await createOTP(jwt);
-      const u = new URL(`${host}/login`);
-      u.searchParams.set('auth_token', authToken);
-      u.searchParams.set('referrer_url', `${host}/rooms/${room}`);
-
-      // main window
-      const win = new BrowserWindow({
-        width,
-        height,
-        x: 0,
-        y: 0,
-        frame: false,
-        webPreferences: {
-          session: session.fromPartition('login'),
-        },
-      });
-      if (debug) {
-        win.webContents.openDevTools();
-      }
-      win.loadURL(u.href);
+    if (debug) {
+      win.webContents.openDevTools();
     }
-
-    // this will start signal that the electron process has successfully started
-    // console.log('electron main ready');
-  } else {
-    throw new Error('no directory in agent spec');
+    win.loadURL(u.href);
   }
+
+  // this will start signal that the electron process has successfully started
+  console.log('electron main ready');
 };
 _main()
   .catch((err) => {
