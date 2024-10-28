c35d8e6819d98d892aa369afa131e48794bd8160
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-29 01:02:59 +0400

    Remove old title

--- apps/chat/components/account/subscriptions.tsx
+++ apps/chat/components/account/subscriptions.tsx
@@ -208,14 +208,6 @@ const Subscriptions = ({
 }) => {
   return (
     <>
-      <div className="sm:flex sm:flex-col sm:align-center py-2 md:py-4">
-        <h1 className="text-2xl font-extrabold text-white sm:text-center sm:text-4xl">
-          Subscription Plans
-        </h1>
-        <p className="max-w-2xl m-auto md:mt-4 text-lg text-zinc-200 sm:text-center sm:text-xl">
-          Subscribe to a plan to get monthly credits.
-        </p>
-      </div>
       {user ?
         <SubscriptionPlans user={user} userPrivate={userPrivate} setUserPrivate={setUserPrivate} />
         :
