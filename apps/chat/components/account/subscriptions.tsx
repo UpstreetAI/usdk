54d21677eb38841ed8a5b1c6bd7dc1d8693e68fb
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-10-29 01:22:41 +0400

    Format code and update subscription boxes

--- apps/chat/components/account/subscriptions.tsx
+++ apps/chat/components/account/subscriptions.tsx
@@ -1,51 +1,45 @@
 'use client';
 
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { getJWT } from '@/lib/jwt';
-import { cn } from '@/lib/utils'
+import { cn } from '@/lib/utils';
 import { LoginButton } from '../ui/Header/login-button';
 import { createSession, cancelPlan } from '@/lib/stripe';
 import { environment } from '@/lib/env';
 import { Button, Icon } from 'ucom';
 
-//
-
 export interface AccountPrivateUiProps {
-  user: any
-  userPrivate: any
+  user: any;
+  userPrivate: any;
 }
-
-//
 
 const plans = [
   {
     name: 'free',
     value: 0,
     currency: `$`,
-    interval: 'mo'
+    interval: 'month',
   },
   {
     name: 'hobby',
     value: 20,
     currency: `$`,
-    interval: 'mo'
+    interval: 'month',
   },
   {
     name: 'developer',
     value: 50,
     currency: `$`,
-    interval: 'mo'
+    interval: 'month',
   },
   {
     name: 'business',
     value: 200,
     currency: `$`,
-    interval: 'mo'
+    interval: 'month',
   },
 ];
 const creditUnit = 1000; // is multiplied by the value in plans array
-
-//
 
 const SubscriptionPlans = ({
   user,
@@ -56,36 +50,45 @@ const SubscriptionPlans = ({
   userPrivate: any;
   setUserPrivate: (userPrivate: any) => void;
 }) => {
-  const {
-    plan: currentPlan,
-  }: {
-    plan: string;
-  } = userPrivate;
+  const { plan: currentPlan }: { plan: string } = userPrivate;
   const [selectedPlan, setSelectedPlan] = useState(() => currentPlan);
 
-  console.log(currentPlan)
+  console.log(currentPlan);
 
   return (
     <div>
       <div className="mt-4 md:mt-8 space-y-4 sm:mt-8 sm:space-y-0 md:flex md:flex-wrap justify-center gap-6 lg:mx-auto xl:max-w-none xl:mx-0">
+        {currentPlan && (
+          <Button
+            className="w-full mt-8"
+            onClick={async (e) => {
+              const jwt = await getJWT();
+              await cancelPlan({
+                environment,
+                jwt,
+              });
+              setUserPrivate((userPrivate: object) => {
+                return {
+                  ...userPrivate,
+                  stripe_subscription_id: null,
+                  plan: null,
+                };
+              });
+            }}
+          >
+            Cancel
+          </Button>)}
+
         {plans.map((plan, i) => {
-          const {
-            name,
-            currency,
-            value,
-            interval
-          } = plan;
+          const { name, currency, value, interval } = plan;
 
           const isFreePlan = !selectedPlan && name === 'free';
-          
+
           return (
             <div
               key={i}
               className={cn(
                 'flex flex-col shadow-sm divide-y divide-zinc-600 bg-zinc-100 border border-[2px] border-zinc-800',
-                {
-                  // 'border border-pink-500': name === selectedPlan || !selectedPlan && name === 'free',
-                },
                 'flex-1',
                 'basis-1/6',
                 'md:max-w-xs'
@@ -95,74 +98,55 @@ const SubscriptionPlans = ({
                 <h2 className="text-2xl font-semibold leading-6 capitalize">
                   {name}
                 </h2>
-                <p className="mt-4 flex items-center justify-center text-zinc-800 font-bold text-4xl">
-                  <Icon icon='Credits' className="size-12 text-[#00C0FF]" /> {value ? (value * creditUnit) : 5000}
+                <p className="mt-6 flex items-center justify-center text-zinc-800 font-bold text-4xl">
+                  <Icon icon="Credits" className="size-12 text-[#00C0FF]" />{' '}
+                  {value ? value * creditUnit : 5000}
                 </p>
-                {isFreePlan ? (<>
-                  <Button
-                    className='w-full mt-8'
-                    disabled={true}
-                    variant='ghost'
-                  >
+                {isFreePlan ? (
+                  <Button className="w-full mt-8" disabled={true} variant="ghost">
                     Active
                   </Button>
-                </>) : (
-                  
-                <>
-                {value > 0 ?
-                  <Button
-                    className='w-full mt-8'
-                    disabled={currentPlan === name}
-                    onClick={async (e) => {
-                      const jwt = await getJWT();
-                      const success_url = location.href;
-                      const j = await createSession({
-                        plan: name,
-                        args: {
-                          success_url,
-                        },
-                      }, {
-                        environment,
-                        jwt,
-                      });
-                      const {
-                        // id,
-                        url,
-                      } = j;
-                      location.href = url;
-                    }}
-                  >
-                    {currentPlan !== name ? 'Choose' : 'Current'}
-                  </Button>
-                  :
-                  (currentPlan && <Button
-                    className='w-full mt-8'
-                    onClick={async (e) => {
-                      const jwt = await getJWT();
-                      await cancelPlan({
-                        environment,
-                        jwt,
-                      });
-                      setUserPrivate((userPrivate: object) => {
-                        return {
-                          ...userPrivate,
-                          stripe_subscription_id: null,
-                          plan: null,
-                        };
-                      });
-                    }}
-                  >
-                    Cancel
-                  </Button>)
-                }
-                </>
-              )}
+                ) : (
+                  <>
+                    {value > 0 ? (
+                      <Button
+                        className="w-full mt-8"
+                        disabled={currentPlan === name}
+                        onClick={async (e) => {
+                          const jwt = await getJWT();
+                          const success_url = location.href;
+                          const j = await createSession(
+                            {
+                              plan: name,
+                              args: {
+                                success_url,
+                              },
+                            },
+                            {
+                              environment,
+                              jwt,
+                            }
+                          );
+                          const { url } = j;
+                          location.href = url;
+                        }}
+                      >
+                        {currentPlan !== name ? 'Choose' : 'Current'}
+                      </Button>
+                    ) : (
+                      <Button className="w-full mt-8" disabled={true} variant="ghost">
+                        Active
+                      </Button>
+                    )
+                    }
+                  </>
+                )}
                 <p className="mt-8">
-                  <span className="text-5xl font-extrabold">
+                  <span className="text-4xl font-bold">
                     {value > 0 ? `${currency}${value}` : '$0'}
                   </span>
-                  <span className="text-base font-medium">
-                    {value > 0 ? `/${interval}` : '/mo'}
+                  <span className="text-base font-bold">
+                    {value > 0 ? `/${interval}` : '/month'}
                   </span>
                 </p>
               </div>
@@ -194,7 +178,9 @@ const SubscriptionPlansPublic = () => {
                 <h2 className="text-2xl font-semibold leading-6 text-white capitalize">
                   {name}
                 </h2>
-                <p className="mt-4 text-zinc-300">{value ? (value * creditUnit) + ' Credits' : '5000 Credits'}</p>
+                <p className="mt-4 text-zinc-300">
+                  {value ? value * creditUnit + ' Credits' : '5000 Credits'}
+                </p>
                 <p className="mt-8">
                   <span className="text-5xl font-extrabold white">
                     {value > 0 ? `${currency}${value}` : '$0'}
@@ -207,8 +193,8 @@ const SubscriptionPlansPublic = () => {
             </div>
           );
         })}
-        <div className='w-full text-center'>
-          <LoginButton className='text-xl' text="Login to you account to subscribe!" />
+        <div className="w-full text-center">
+          <LoginButton className="text-xl" text="Login to you account to subscribe!" />
         </div>
       </div>
     </div>
@@ -220,20 +206,24 @@ const Subscriptions = ({
   userPrivate,
   setUserPrivate,
 }: {
-  user: any,
-  userPrivate: any,
+  user: any;
+  userPrivate: any;
   setUserPrivate: (userPrivate: any) => void;
 }) => {
   return (
     <>
-      {user ?
-        <SubscriptionPlans user={user} userPrivate={userPrivate} setUserPrivate={setUserPrivate} />
-        :
+      {user ? (
+        <SubscriptionPlans
+          user={user}
+          userPrivate={userPrivate}
+          setUserPrivate={setUserPrivate}
+        />
+      ) : (
         <SubscriptionPlansPublic />
-      }
+      )}
     </>
   );
-}
+};
 
 export function AccountSubscriptions({
   user,
@@ -241,8 +231,12 @@ export function AccountSubscriptions({
 }: AccountPrivateUiProps) {
   const [userPrivate, setUserPrivate] = useState(() => initUserPrivate);
   return (
-    <div className='w-full'>
-      <Subscriptions user={user} userPrivate={userPrivate} setUserPrivate={setUserPrivate} />
+    <div className="w-full">
+      <Subscriptions
+        user={user}
+        userPrivate={userPrivate}
+        setUserPrivate={setUserPrivate}
+      />
     </div>
   );
 }
