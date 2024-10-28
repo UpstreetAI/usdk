182c910117cea5b713a88f8867b74017b0d71ccb
Avaer Kazmer <ultimus@gmail.com>
2024-10-28 13:16:19 -0700

    Update node-twitter-api-v2 endpoints to proxy for CORS

--- packages/usdk/packages/upstreet-agent/packages/react-agents/packages/node-twitter-api-v2/src/globals.ts
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/packages/node-twitter-api-v2/src/globals.ts
@@ -1,5 +1,5 @@
-export const API_V2_PREFIX = 'https://api.x.com/2/';
-export const API_V2_LABS_PREFIX = 'https://api.x.com/labs/2/';
-export const API_V1_1_PREFIX = 'https://api.x.com/1.1/';
-export const API_V1_1_UPLOAD_PREFIX = 'https://upload.x.com/1.1/';
-export const API_V1_1_STREAM_PREFIX = 'https://stream.x.com/1.1/';
+export const API_V2_PREFIX = 'https://ai.upstreet.ai/api/twitter/2/';
+export const API_V2_LABS_PREFIX = 'https://ai.upstreet.ai/api/twitter/labs/2/';
+export const API_V1_1_PREFIX = 'https://ai.upstreet.ai/api/twitter/1.1/';
+export const API_V1_1_UPLOAD_PREFIX = 'https://ai.upstreet.ai/api/twitter/upload/1.1/';
+export const API_V1_1_STREAM_PREFIX = 'https://ai.upstreet.ai/api/twitter/stream/1.1/';
