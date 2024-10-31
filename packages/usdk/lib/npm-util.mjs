4d6cd4db555ab450b9f4c11fab6a54b750473a64
Abdurrehman Subhani <aarrsubhani@gmail.com>
2024-10-31 15:18:41 +0500

    remove spawn usage for windows support while installing agent deps, use execFile

--- packages/usdk/lib/npm-util.mjs
+++ packages/usdk/lib/npm-util.mjs
@@ -2,48 +2,15 @@ import child_process from 'child_process';
 import util from 'util';
 import { platform } from 'os';
 
-const { spawn } = child_process;
 const execFile = util.promisify(child_process.execFile);
 
-const npmInstallWindows = async (dstDir) => {
-  const npmCommand = 'npm.cmd';
-  return new Promise((resolve, reject) => {
-    const childProcess = spawn(npmCommand, ['install'], {
-      cwd: dstDir,
-      stdio: 'inherit',
-      shell: true,  // Use shell on Windows
-    });
-
-    childProcess.on('error', (error) => {
-      console.error('Failed to start npm install:', error);
-      reject(error);
-    });
-
-    childProcess.on('close', (code) => {
-      if (code === 0) {
-        resolve();
-      } else {
-        const error = new Error(`npm install exited with code ${code}`);
-        console.error(error.message);
-        reject(error);
-      }
-    });
-  });
-};
-
-const npmInstallUnix = async (dstDir) => {
-  await execFile('npm', ['install'], {
+export const npmInstall = async (dstDir) => {
+  const npmCommand = platform() === 'win32' ? 'npm.cmd' : 'npm';
+  await execFile(npmCommand, ['install'], {
     cwd: dstDir,
     stdio: 'inherit',
+    shell: platform() === 'win32',
   });
-};
-
-export const npmInstall = async (dstDir) => {
-  if (platform() === 'win32') {
-    await npmInstallWindows(dstDir);
-  } else {
-    await npmInstallUnix(dstDir);
-  }
 };
 
 const getNpmRoot = async () => {
