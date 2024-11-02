d64e2d6576d7199643450d2ac9fca94f7acac5e1
Avaer Kazmer <ultimus@gmail.com>
2024-11-02 16:45:29 -0700

    Break out agent spec parsing

--- packages/usdk/cli.js
+++ packages/usdk/cli.js
@@ -24,6 +24,7 @@ import { QueueManager } from './packages/upstreet-agent/packages/queue-manager/q
 // import { lembed } from './packages/upstreet-agent/packages/react-agents/util/embedding.mjs';
 import { makeId } from './packages/upstreet-agent/packages/react-agents/util/util.mjs';
 import { packZip, extractZip } from './lib/zip-util.mjs';
+import { parseAgentSpecs } from './lib/agent-spec-utils.mjs';
 import {
   getAgentPublicUrl,
   getCloudAgentHost,
@@ -1071,75 +1072,6 @@ const connect = async (args) => {
   } else {
     console.log('no room name provided');
     process.exit(1);
-  }
-};
-const getGuidFromPath = async (p) => {
-  const makeEnoent = () => new Error('not an agent directory: ' + p);
-
-  const wranglerTomlPath = path.join(p, 'wrangler.toml');
-  try {
-    const wranglerTomString = await fs.promises.readFile(wranglerTomlPath, 'utf8');
-    const wranglerToml = toml.parse(wranglerTomString);
-    const agentJsonString = wranglerToml.vars.AGENT_JSON;
-    const agentJson = agentJsonString && JSON.parse(agentJsonString);
-    const id = agentJson?.id;
-    if (id) {
-      return id;
-    } else {
-      throw makeEnoent();
-    }
-  } catch (err) {
-    if (err.code === 'ENOENT') {
-      throw makeEnoent();
-    } else {
-      throw err;
-    }
-  }
-};
-const parseAgentSpecs = async (agentRefSpecs = []) => {
-  if (!Array.isArray(agentRefSpecs)) {
-    throw new Error('expected agent ref specs to be an array; got ' + JSON.stringify(agentRefSpecs));
-  }
-  if (!agentRefSpecs.every((agentRefSpec) => typeof agentRefSpec === 'string')) {
-    throw new Error('expected agent ref specs to be strings; got ' + JSON.stringify(agentRefSpecs));
-  }
-
-  if (agentRefSpecs.length === 0) {
-    // if no agent refs are provided, use the current directory
-    const directory = cwd;
-    const guid = await getGuidFromPath(directory);
-    return [
-      {
-        ref: directory,
-        guid,
-        directory,
-        portIndex: 0,
-      },
-    ];
-  } else {
-    // treat each agent ref as a guid or directory
-    const agentSpecsPromises = agentRefSpecs.map(async (agentRefSpec, index) => {
-      if (isGuid(agentRefSpec)) {
-        // if it's a cloud agent
-        return {
-          ref: agentRefSpec,
-          guid: agentRefSpec,
-          directory: null,
-          portIndex: index,
-        };
-      } else {
-        // if it's a directory agent
-        const directory = agentRefSpec;
-        const guid = await getGuidFromPath(directory);
-        return {
-          ref: directory,
-          guid,
-          directory,
-          portIndex: index,
-        };
-      }
-    });
-    return await Promise.all(agentSpecsPromises);
   }
 };
 const chat = async (args) => {
