4a784391eaf674aaf1fd21140b5c00ab3e030d16
Avaer Kazmer <ultimus@gmail.com>
2024-11-10 16:10:24 -0800

    Add node runtime worker

---
+++ packages/usdk/packages/upstreet-agent/packages/react-agents-node/worker.mjs
@@ -0,0 +1,87 @@
+import fs from 'fs';
+import toml from 'toml';
+import { AgentMain } from 'upstreet-agent/packages/react-agents/entry.ts';
+import * as codecs from 'codecs/ws-codec-runtime-worker.mjs';
+import userRender from './agent.tsx';
+
+//
+
+['uncaughtException', 'unhandledRejection'].forEach(event => {
+  process.on(event, err => {
+    console.error(err);
+  });
+});
+
+//
+
+const getEnv = async () => {
+  // load the wrangler.toml
+  const wranglerTomlPath = './wrangler.toml';
+  const wranglerTomlString = await fs.promises.readFile(wranglerTomlPath, 'utf8');
+  const wranglerToml = toml.parse(wranglerTomlString);
+
+  const agentJsonString = wranglerToml.vars.AGENT_JSON;
+  if (!agentJsonString) {
+    throw new Error('missing AGENT_JSON in wrangler.toml');
+  }
+  const agentJson = JSON.parse(agentJsonString);
+
+  const apiKey = wranglerToml.vars.AGENT_TOKEN;
+  if (!apiKey) {
+    throw new Error('missing AGENT_TOKEN in wrangler.toml');
+  }
+
+  const mnemonic = wranglerToml.vars.WALLET_MNEMONIC;
+  if (!mnemonic) {
+    throw new Error('missing WALLET_MNEMONIC in wrangler.toml');
+  }
+
+  const {
+    SUPABASE_URL,
+    SUPABASE_PUBLIC_API_KEY,
+  } = wranglerToml.vars;
+  if (!SUPABASE_URL || !SUPABASE_PUBLIC_API_KEY) {
+    throw new Error('missing SUPABASE_URL or SUPABASE_PUBLIC_API_KEY in wrangler.toml');
+  }
+
+  // send init message
+  const env = {
+    AGENT_JSON: JSON.stringify(agentJson),
+    AGENT_TOKEN: apiKey,
+    WALLET_MNEMONIC: mnemonic,
+    SUPABASE_URL,
+    SUPABASE_PUBLIC_API_KEY,
+    WORKER_ENV: 'development', // 'production',
+  };
+  return env;
+};
+
+//
+
+const main = async () => {
+  const env = await getEnv();
+
+  // XXX need to handle command line args e.g.
+  // '--var', 'WORKER_ENV:development',
+  // '--ip', '0.0.0.0',
+  // '--port', devServerPort + portIndex,
+
+  let alarmTimestamp = null;
+  const state = {
+    userRender,
+    codecs,
+    storage: {
+      async getAlarm() {
+        return alarmTimestamp;
+      },
+      setAlarm(timestamp) {
+        alarmTimestamp = timestamp;
+      },
+    },
+  };
+  const agentMain = new AgentMain(state, env);
+  // return agentMain;
+};
+main().catch(err => {
+  console.error(err);
+});
