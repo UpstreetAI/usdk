23a7129651d708f1b7297058e975511612eee96e
Avaer Kazmer <ultimus@gmail.com>
2024-11-12 11:10:26 -0800

    Use hono in react agents node

--- packages/usdk/packages/upstreet-agent/packages/react-agents-node/worker.mjs
+++ packages/usdk/packages/upstreet-agent/packages/react-agents-node/worker.mjs
@@ -2,6 +2,8 @@ import path from 'path';
 import os from 'os';
 import { program } from 'commander';
 import { createServer as createViteServer } from 'vite';
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server';
 
 //
 
@@ -14,13 +16,82 @@ const loadModule = async (directory, p) => {
   // console.log('get agent module 2', entryModule);
   return entryModule.default;
 };
-//
+const startAgentMainServer = async ({
+  agentMain,
+  ip,
+  port,
+}) => {
+  console.log('startAgentMainServer', { agentMain, ip, port });
+
+  const app = new Hono();
+
+  app.all('*', (c) => {
+    const req = c.req.raw;
+    console.log('got fetch', {
+      url: req.url,
+      method: req.method,
+      headers: Object.fromEntries(req.headers),
+    });
+    return agentMain.fetch(req);
+  });
+
+  console.log('create server', {
+    hostname: ip,
+    port: parseInt(port, 10),
+  });
+
+  // create server
+  const server = serve({
+    fetch: app.fetch,
+    // hostname: ip,
+    port: parseInt(port, 10),
+  });
+  // wait for server to start
+  await new Promise((resolve, reject) => {
+    server.on('listening', () => {
+      resolve(null);
+    });
+    server.on('error', (err) => {
+      reject(err);
+    });
+  });
+  console.log(`Agent server listening on http://${ip}:${port}`);
+
+  /* // XXX try to fetch
+  try {
+    const u = `http://127.0.0.1:${port}`;
+    console.log('agentMain fetch 1', u);
+    const res = await fetch(u);
+    console.log('agentMain fetch 2', res);
+    const text = await res.text();
+    console.log('agentMain fetch text', text);
+  } catch (err) {
+    console.warn('agentMain fetch error', err);
+  } */
+};
 const runAgent = async (directory, opts) => {
   const p = '/packages/upstreet-agent/packages/react-agents-node/entry.mjs';
   const main = await loadModule(directory, p);
   console.log('worker loaded module', main);
   const agentMain = await main();
   console.log('agentMain', agentMain);
+
+  const {
+    ip,
+    port,
+  } = opts;
+  await startAgentMainServer({
+    agentMain,
+    ip,
+    port,
+  });
+
+  console.log('worker send 1');
+  process.send({
+    method: 'ready',
+    args: [],
+  });
+  console.log('worker send 2');
 };
 const makeViteServer = (directory) => {
   return createViteServer({
