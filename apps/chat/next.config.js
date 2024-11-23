c24514d984b1211431777d4b69634b25aa14050d
mavisakalyan <55106546+mavisakalyan@users.noreply.github.com>
2024-11-24 02:49:17 +0400

    Update next.config.js
    - allow all origins, testing all routes first

--- apps/chat/next.config.js
+++ apps/chat/next.config.js
@@ -86,5 +86,22 @@ module.exports = {
     });
 
     return config;
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *;",
+          },
+        ],
+      },
+    ];
+  },
 }
