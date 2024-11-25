6a064290540e70d848e7f9e744dab99d74d6c7bd
Avaer Kazmer <ultimus@gmail.com>
2024-11-25 02:02:57 -0800

    Link electron to desktop page

--- packages/usdk/packages/upstreet-agent/packages/react-agents-electron/electron-main.mjs
+++ packages/usdk/packages/upstreet-agent/packages/react-agents-electron/electron-main.mjs
@@ -182,7 +182,7 @@ const openFrontend = async ({
     // trade the jwt for an otp auth token
     const authToken = await createOTP(jwt);
     // construct the destination url
-    const dstUrl = new URL(`${host}/rooms/${room}`);
+    const dstUrl = new URL(`${host}/desktop/${room}`);
     dstUrl.searchParams.set('desktop', 1 + '');
     // construct the final url
     const u = new URL(`${host}/login`);
