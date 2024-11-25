96043e5d2526dfec2bf8b76c8b032f5c474898f2
Avaer Kazmer <ultimus@gmail.com>
2024-11-24 16:01:42 -0800

    Update electron main

--- packages/usdk/packages/upstreet-agent/packages/react-agents-electron/electron-main.mjs
+++ packages/usdk/packages/upstreet-agent/packages/react-agents-electron/electron-main.mjs
@@ -1,10 +1,145 @@
+import path from 'path';
+import os from 'os';
+import { program } from 'commander';
+import { createServer as createViteServer } from 'vite';
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server';
 import { app, screen, session, BrowserWindow, desktopCapturer } from 'electron';
 import { Button, Key, keyboard, mouse, Point } from '@nut-tree-fork/nut-js';
 
+//
+
 console.log('electron start script!');
 
-// const host = 'https://chat.upstreet.ai';
-const host = 'http://localhost:3000';
+['uncaughtException', 'unhandledRejection'].forEach(event => {
+  process.on(event, err => {
+    process.send({
+      method: 'error',
+      args: [err.stack],
+    });
+  });
+});
+
+// agent code
+
+const homeDir = os.homedir();
+
+const loadModule = async (directory, p) => {
+  const viteServer = await makeViteServer(directory);
+  // console.log('get agent module 1');
+  const entryModule = await viteServer.ssrLoadModule(p);
+  // console.log('get agent module 2', entryModule);
+  return entryModule.default;
+};
+const startAgentMainServer = async ({
+  agentMain,
+  ip,
+  port,
+}) => {
+  const app = new Hono();
+
+  let opened = false;
+  app.post('/open', async (c) => {
+    if (!opened) {
+      opened = true;
+      
+      const req = c.req.raw;
+      const j = await req.json();
+      const {
+        room,
+        jwt,
+      } = j;
+
+      await openFrontend({
+        room,
+        jwt,
+      });
+
+      return new Response(JSON.stringify({
+        ok: true,
+      }), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    } else {
+      return new Response(JSON.stringify({
+        error: 'already opened',
+      }), {
+        status: 409,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+  });
+  app.all('*', (c) => {
+    const req = c.req.raw;
+    return agentMain.fetch(req);
+  });
+
+  // create server
+  const server = serve({
+    fetch: app.fetch,
+    // hostname: ip,
+    port: parseInt(port, 10),
+  });
+  // wait for server to start
+  await new Promise((resolve, reject) => {
+    server.on('listening', () => {
+      resolve(null);
+    });
+    server.on('error', (err) => {
+      reject(err);
+    });
+  });
+  // console.log(`Agent server listening on http://${ip}:${port}`);
+};
+const runAgent = async (directory, opts) => {
+  const p = '/packages/upstreet-agent/packages/react-agents-node/entry.mjs';
+  const main = await loadModule(directory, p);
+  // console.log('worker loaded module', main);
+  const agentMain = await main();
+  // console.log('agentMain', agentMain);
+
+  const {
+    ip,
+    port,
+  } = opts;
+  await startAgentMainServer({
+    agentMain,
+    ip,
+    port,
+  });
+
+  // console.log('worker send 1');
+  process.send({
+    method: 'ready',
+    args: [],
+  });
+  // console.log('worker send 2');
+};
+const makeViteServer = (directory) => {
+  return createViteServer({
+    root: directory,
+    server: { middlewareMode: 'ssr' },
+    cacheDir: path.join(homeDir, '.usdk', 'vite'),
+    esbuild: {
+      jsx: 'transform',
+      // jsxFactory: 'React.createElement',
+      // jsxFragment: 'React.Fragment',
+    },
+    optimizeDeps: {
+      entries: [
+        './packages/upstreet-agent/packages/react-agents-node/entry.mjs',
+      ],
+    },
+  });
+};
+
+// frontend code
+
+const host = 'https://chat.upstreet.ai';
 
 const createOTP = async (jwt) => {
   const res = await fetch(
@@ -21,17 +156,10 @@ const createOTP = async (jwt) => {
   }
 };
 
-const _main = async () => {
-  const room = process.argv[2];
-  const jwt = process.argv[3];
-  const debug = process.argv[4] === '1';
-
-  // console.log('got args', {
-  //   room,
-  //   jwt,
-  //   debug,
-  // });
-
+const openFrontend = async ({
+  room,
+  jwt,
+}) => {
   // wait for the electron app to be ready
   await app.whenReady();
 
@@ -66,12 +194,42 @@ const _main = async () => {
     }
     win.loadURL(u.href);
   }
-
-  // this will start signal that the electron process has successfully started
-  console.log('electron main ready');
 };
-_main()
-  .catch((err) => {
-    console.error('error in electron start script', err);
+
+// main code
+
+const main = async () => {
+  let commandExecuted = false;
+
+  program
+    .command('run')
+    .description('Run the agent')
+    .argument(`[directory]`, `Agent directory`)
+    .option('--var <vars...>', 'Environment variables in format KEY:VALUE')
+    .requiredOption('--ip <ip>', 'IP address to bind to')
+    .requiredOption('--port <port>', 'Port to bind to')
+    .action(async (directory, opts) => {
+      commandExecuted = true;
+
+      try {
+        await runAgent(directory, opts);
+
+        // this will start signal that the electron process has successfully started
+        console.log('electron main ready');
+      } catch (err) {
+        console.warn(err);
+        process.exit(1);
+      }
+    });
+
+  await program.parseAsync();
+
+  if (!commandExecuted) {
+    console.error('Command missing');
     process.exit(1);
-  });
+  }
+};
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
