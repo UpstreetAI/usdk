902101cf9e75be46eb78c7f9dc6e208bd5f7edef
Avaer Kazmer <ultimus@gmail.com>
2024-11-27 11:16:59 -0800

    Clean up error logging

--- packages/usdk/packages/upstreet-agent/packages/react-agents/classes/twitter-spaces-manager.ts
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/classes/twitter-spaces-manager.ts
@@ -1005,7 +1005,7 @@ class TwitterSpacesBot {
     (async () => {
       await _start();
     })().catch(err => {
-      console.warn('twitter bot error', err);
+      console.warn('twitter spaces bot error', err);
     });
   }
 
