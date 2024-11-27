022a6b1f3fb87bb4ed26aa85da3e6b26fec9a00c
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-11-27 23:31:14 +0400

    add header & footer to users list page

--- apps/chat/app/accounts/page.tsx
+++ apps/chat/app/accounts/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Accounts } from '@/components/accounts';
+import Footer from '@/components/ui/Footer';
+import Header from '@/components/ui/Header';
 
 export default function AccountsPage() {
   return (
-    <div className="relative w-full mx-auto max-w-6xl px-4 py-8">
-      <Accounts loadmore={true} range={12} />
+    <div className='w-full'>
+      <Header />
+      <div className="relative w-full mx-auto max-w-6xl px-4 py-8">
+        <Accounts loadmore={true} range={12} />
+      </div>
+      <Footer />
     </div>
   );
 }
