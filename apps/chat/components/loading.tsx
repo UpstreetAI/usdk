eece6a15af3e70cb8556ac6a673a35ba6b1e557f
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-12-03 20:11:24 +0400

    fix loading bar

--- apps/chat/components/loading.tsx
+++ apps/chat/components/loading.tsx
@@ -4,7 +4,7 @@ import { IconSpinner } from '@/components/ui/icons'
 
 export function Loading() {
   return (
-    <div className="flex flex-1 justify-center items-center">
+    <div className="flex flex-1 justify-center items-center h-full w-full fixed top-0 left-0 z-100">
       <IconSpinner className="h-[50px] w-[50px]"/>
     </div>
   )
