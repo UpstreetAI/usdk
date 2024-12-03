82e1dc2df1f55b1387030e55fb7c91d9e4aa5892
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-12-03 19:39:37 +0400

    update backgrounds on different routes

--- apps/chat/components/body.tsx
+++ apps/chat/components/body.tsx
@@ -4,6 +4,7 @@ import { Loading } from '@/components/loading'
 import { useSupabase } from '@/lib/hooks/use-supabase'
 import { cn } from '@/lib/utils'
 import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 
@@ -15,18 +16,21 @@ interface MainProps {
 export function Body( {children}: MainProps) {
 
   const pathname = usePathname()
+  const router = useRouter()
+
+  console.log('pathname', pathname, router)
 
   const getWrapperClass = () => {
     // Wrapper class is used to set a main background for now in different use cases
     if (pathname.startsWith('/embed')) {
       // Embed route styles
-      return 'embed'
+      return 'bg-[url("/images/backgrounds/main-background.jpg")] bg-center bg-cover'
     } else if (pathname.startsWith('/desktop')) {
       // Desktop route styles
       return 'desktop'
     } else {
       // Web route styles
-      return 'web'
+      return 'bg-[url("/images/backgrounds/main-background.jpg")] bg-center bg-cover'
     }
   }
   const {isFetchingUser} = useSupabase();
