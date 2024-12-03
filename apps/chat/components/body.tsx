37c0b927f84ee7afb2cfefc3e7d46ba9085f992f
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-12-03 17:49:45 +0400

    Add getWrapperClass to the Body component to set a background for each usecase, desktop, embed and web for now

--- apps/chat/components/body.tsx
+++ apps/chat/components/body.tsx
@@ -2,6 +2,8 @@
 
 import { Loading } from '@/components/loading'
 import { useSupabase } from '@/lib/hooks/use-supabase'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 
@@ -11,10 +13,26 @@ interface MainProps {
 
 
 export function Body( {children}: MainProps) {
+
+  const pathname = usePathname()
+
+  const getWrapperClass = () => {
+    // Wrapper class is used to set a main background for now in different use cases
+    if (pathname.startsWith('/embed')) {
+      // Embed route styles
+      return 'embed'
+    } else if (pathname.startsWith('/desktop')) {
+      // Desktop route styles
+      return 'desktop'
+    } else {
+      // Web route styles
+      return 'web'
+    }
+  }
   const {isFetchingUser} = useSupabase();
 
   return (
-    <main className="flex flex-col flex-1">
+    <main className={cn("flex flex-col flex-1", getWrapperClass())}>
       {isFetchingUser ? (
         <Loading />
       ) : children}
