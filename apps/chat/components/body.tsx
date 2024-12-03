fc2361151fc58bc74a0b348c89a33bad6e10eba1
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-12-03 20:14:56 +0400

    update loading background per mode

--- apps/chat/components/body.tsx
+++ apps/chat/components/body.tsx
@@ -4,7 +4,6 @@ import { Loading } from '@/components/loading'
 import { useSupabase } from '@/lib/hooks/use-supabase'
 import { cn } from '@/lib/utils'
 import { usePathname } from 'next/navigation'
-import { useRouter } from 'next/navigation'
 import React from 'react'
 
 
@@ -16,20 +15,21 @@ interface MainProps {
 export function Body( {children}: MainProps) {
 
   const pathname = usePathname()
-  const router = useRouter()
 
-  console.log('pathname', pathname, router)
+  let mode = 'web';
 
   const getWrapperClass = () => {
     // Wrapper class is used to set a main background for now in different use cases
     if (pathname.startsWith('/embed')) {
       // Embed route styles
+      mode = 'embed'
       return 'bg-[url("/images/backgrounds/main-background.jpg")] bg-center bg-cover'
     } else if (pathname.startsWith('/desktop')) {
       // Desktop route styles
       return 'desktop'
     } else {
       // Web route styles
+      mode = 'web'
       return 'bg-[url("/images/backgrounds/main-background.jpg")] bg-center bg-cover'
     }
   }
@@ -38,7 +38,7 @@ export function Body( {children}: MainProps) {
   return (
     <main className={cn("flex flex-col flex-1", getWrapperClass())}>
       {isFetchingUser ? (
-        <Loading />
+        <Loading mode={mode} />
       ) : children}
     </main>
   )
--- apps/chat/components/loading.tsx
+++ apps/chat/components/loading.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { IconSpinner } from '@/components/ui/icons'
+import { cn } from '@/lib/utils'
 
 
-export function Loading() {
+export function Loading({mode}: {mode: string}) {
   return (
-    <div className="flex flex-1 justify-center items-center h-full w-full fixed top-0 left-0 z-100">
+    <div className={cn("flex flex-1 justify-center items-center h-full w-full fixed top-0 left-0 z-100", mode === 'web' ? 'bg-[url("/images/backgrounds/main-background.jpg")] bg-center bg-cover' : '')}>
       <IconSpinner className="h-[50px] w-[50px]"/>
     </div>
   )
