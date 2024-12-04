b1a114637cb4329b0964bda7a30635cdd9fb43f8
Saad A. Bazaz <saadbazaz@hotmail.com>
2024-12-04 19:42:49 +0500

    fix: update npm-util to directly use pnpm binary, and not depend on import.meta

--- packages/usdk/lib/npm-util.mjs
+++ packages/usdk/lib/npm-util.mjs
@@ -1,26 +1,17 @@
-import path from 'path';
-import fs from 'fs';
 import spawn from 'cross-spawn';
 
-const pnpmPackageJsonPath = import.meta.resolve('pnpm').replace('file://', '');
-const pnpmPackageJsonString = fs.readFileSync(pnpmPackageJsonPath, 'utf8');
-const pnpmPackageJson = JSON.parse(pnpmPackageJsonString);
-const pnpmPath = path.resolve(path.dirname(pnpmPackageJsonPath), pnpmPackageJson.bin.pnpm);
+const pnpmPath = 'pnpm'; // Directly reference the binary name
 
 export const hasNpm = async () => {
-  // check if the npm command exists
-  return await new Promise((resolve) => {
+  return new Promise((resolve) => {
     const child = spawn(pnpmPath, ['--version'], {
       stdio: 'ignore',
     });
+    
     child.on('close', (code) => {
-      if (code === 0) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
+      resolve(code === 0);
     });
-    child.on('error', (err) => {
+    child.on('error', () => {
       resolve(false);
     });
   });
@@ -42,33 +33,3 @@ export const npmInstall = async (dstDir) => {
     child.on('error', reject);
   });
 };
-
-const getNpmRoot = async () => {
-  const { stdout } = await new Promise((resolve, reject) => {
-    const child = spawn(pnpmPath, ['root', '--quiet', '-g']);
-
-    let output = '';
-    child.stdout.on('data', (data) => {
-      output += data;
-    });
-
-    child.on('close', (code) => {
-      if (code !== 0) {
-        reject(new Error(`pnpm root failed with code ${code}`));
-      } else {
-        resolve({ stdout: output });
-      }
-    });
-  });
-  return stdout.trim();
-};
-
-export const ensureNpmRoot = (() => {
-  let npmRootPromise = null;
-  return () => {
-    if (npmRootPromise === null) {
-      npmRootPromise = getNpmRoot();
-    }
-    return npmRootPromise;
-  };
-})();
