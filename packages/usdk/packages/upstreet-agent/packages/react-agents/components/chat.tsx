0a591eac725a21088cddec4ca1f113136051bd46
Avaer Kazmer <ultimus@gmail.com>
2024-12-04 21:39:27 -0800

    Add chat component

---
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/components/chat.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Action } from './action';
+
+export const ChatActions = () => {
+  return (
+    <>
+      <Action
+        name="say"
+        description={dedent`\
+          Say something in the chat.
+        `}
+        schema={
+          z.object({
+            text: z.string(),
+          })
+        }
+        examples={[
+          {
+            text: 'Hello, there! How are you doing?',
+          },
+        ]}
+        // handler={async (e: PendingActionEvent) => {
+        //   await e.commit();
+        // }}
+      />
+    </>
+  );
+};
