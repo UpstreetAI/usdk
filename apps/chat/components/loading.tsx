80bc58de93443559ad2da873b95cd8584a1f4475
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-12-06 16:23:03 +0400

    cleanup loading overlay

--- apps/chat/components/loading.tsx
+++ apps/chat/components/loading.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import { IconSpinner } from '@/components/ui/icons'
 import { cn } from '@/lib/utils'
 
-
 export function Loading({mode}: {mode: string}) {
-  return (
-    <div className={cn("flex flex-1 justify-center items-center h-full w-full fixed top-0 left-0 z-100", mode === 'web' ? 'bg-[url("/images/backgrounds/main-background.jpg")] bg-center bg-cover' : '')}>
-      <IconSpinner className="h-[50px] w-[50px]"/>
+  return mode && (
+    <div className="flex items-center justify-center h-screen">
+      <IconSpinner className="h-[50px] w-[50px]" />
     </div>
   )
 }
