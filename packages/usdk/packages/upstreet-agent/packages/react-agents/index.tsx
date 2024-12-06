647c3be76b3eef8df76aed6d7f0751955aeec3d2
Avaer Kazmer <ultimus@gmail.com>
2024-12-05 17:53:55 -0800

    More dead imports cleanup

--- packages/usdk/packages/upstreet-agent/packages/react-agents/index.tsx
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/index.tsx
@@ -1,16 +1,2 @@
-// components
-
-export * from './components/base-components';
 export * from './components/default-components';
 export * from './hooks';
-
-// types
-
-// export type {
-//   AgentEvent,
-//   PendingActionEvent,
-//   PerceptionEvent,
-//   ActionMessage,
-//   SubtleAiCompleteOpts,
-//   AgentAppProps,
-// } from './types';
