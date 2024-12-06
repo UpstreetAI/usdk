c6956794996941fea23f45f5361bbb98d3ddadba
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2024-12-06 16:56:06 +0400

    remove loading check from body

--- apps/chat/components/body.tsx
+++ apps/chat/components/body.tsx
@@ -3,43 +3,22 @@
 import { Loading } from '@/components/loading'
 import { useSupabase } from '@/lib/hooks/use-supabase'
 import { cn } from '@/lib/utils'
-import { usePathname } from 'next/navigation'
-import React, { useMemo } from 'react'
+import React from 'react'
 
 interface MainProps {
   children: React.ReactNode
 }
 
 export function Body({ children }: MainProps) {
-  const pathname = usePathname();
   const { isFetchingUser } = useSupabase();
-
-  console.log('pathname', pathname);
-  console.log('isFetchingUser', isFetchingUser);
-
-  const { mode } = useMemo(() => {
-    if (pathname.startsWith('/embed')) {
-      return {
-        mode: 'embed',
-      };
-    } else if (pathname.startsWith('/desktop')) {
-      return {
-        mode: 'desktop',
-      };
-    } else {
-      return {
-        mode: 'web',
-      };
-    }
-  }, [pathname]);
 
   return (
     <main className={cn("flex flex-col flex-1")}>
       {isFetchingUser ? (
-        <Loading mode={mode} />
+        <Loading />
       ) : (
         React.Children.map(children, child =>
-          React.isValidElement(child) ? React.cloneElement(child, { ...child.props, mode }) : child
+          React.isValidElement(child) ? React.cloneElement(child, { ...child.props, mode: 'mode', wrapperClass: 'wrapperClass' }) : child
         )
       )}
     </main>
