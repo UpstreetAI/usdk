c87d09a2b6a7111e86a880c66c74d0da2e131fde
Avaer Kazmer <ultimus@gmail.com>
2024-12-08 11:44:02 -0800

    Load config in wrangler runtime

--- packages/usdk/packages/upstreet-agent/durable-object.tsx
+++ packages/usdk/packages/upstreet-agent/durable-object.tsx
@@ -2,20 +2,28 @@ import dotenv from 'dotenv';
 import { AgentMain } from 'react-agents/entry.ts';
 import userRender from '../../agent.tsx'; // note: this will be overwritten by the build process
 import envTxt from '../../.env.txt';
+import configTxt from '../../config.txt';
 import * as codecs from 'codecs/ws-codec-runtime-edge.mjs';
 
 Error.stackTraceLimit = 300;
-
-//
 
 // CloudFlare Worker Durable Object class
 export class DurableObject {
   agentMain: AgentMain;
 
   constructor(state: any, env: any) {
+    const config = (() => {
+      try {
+        return JSON.parse(configTxt);
+      } catch (e) {
+        console.warn('Warning: failed to parse config.txt:', e);
+        return {};
+      }
+    })();
     const state2 = {
       ...state,
       userRender,
+      config,
       codecs,
     };
     const auth2 = dotenv.parse(envTxt);
