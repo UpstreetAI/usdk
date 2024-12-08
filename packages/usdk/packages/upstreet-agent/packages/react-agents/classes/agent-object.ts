997f982024ce3f961d1b58e14955b5623db04902
Avaer Kazmer <ultimus@gmail.com>
2024-12-08 11:45:44 -0800

    Update agent object implementation

--- packages/usdk/packages/upstreet-agent/packages/react-agents/classes/agent-object.ts
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/classes/agent-object.ts
@@ -1,3 +1,5 @@
+import { AgentObjectData } from '../types';
+
 export class AgentObject extends EventTarget {
   id: string;
   ownerId: string;
@@ -6,10 +8,16 @@ export class AgentObject extends EventTarget {
   bio: string;
   previewUrl: string;
   model: string;
+  smallModel: string;
+  largeModel: string;
   address: string;
   stripeConnectAccountId: string;
 
-  constructor({
+  constructor(config: AgentObjectData) {
+    super();
+    this.setConfig(config);
+  }
+  setConfig({
     id,
     ownerId,
     name,
@@ -17,21 +25,11 @@ export class AgentObject extends EventTarget {
     bio,
     previewUrl,
     model,
+    smallModel,
+    largeModel,
     address,
     stripeConnectAccountId,
-  }: {
-    id: string;
-    ownerId: string;
-    name: string;
-    description: string;
-    bio: string;
-    previewUrl: string;
-    model: string;
-    address: string;
-    stripeConnectAccountId: string;
-  }) {
-    super();
-
+  }: AgentObjectData) {
     this.id = id;
     this.ownerId = ownerId;
     this.name = name;
@@ -39,6 +37,8 @@ export class AgentObject extends EventTarget {
     this.bio = bio;
     this.previewUrl = previewUrl;
     this.model = model;
+    this.smallModel = smallModel;
+    this.largeModel = largeModel;
     this.address = address;
     this.stripeConnectAccountId = stripeConnectAccountId;
   }
