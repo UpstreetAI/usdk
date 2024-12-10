a77ca09f501f12c1a5975b6c592d47483c4f2444
Saad A. Bazaz <saadbazaz@hotmail.com>
2024-12-10 16:06:34 +0500

    feat(wip): load envs on runtime to catch errors

--- packages/usdk/packages/upstreet-agent/durable-object.tsx
+++ packages/usdk/packages/upstreet-agent/durable-object.tsx
@@ -1,12 +1,21 @@
 import dotenv from 'dotenv';
 import { AgentMain } from 'react-agents/entry.ts';
 import userRender from '../../agent.tsx'; // note: this will be overwritten by the build process
-import envTxt from '../../.env.txt';
 import * as codecs from 'codecs/ws-codec-runtime-edge.mjs';
 
 Error.stackTraceLimit = 300;
 
-//
+function getEnvConfig(): Record<string, string> {
+  let parsedEnv;
+  try {
+    const envTxt = require('../../.env.txt'); // Load the file only once
+    parsedEnv = dotenv.parse(envTxt);
+  } catch (err) {
+    console.warn('No .env.txt found. Your environment variables may become undefined in the code.', err);
+    parsedEnv = {};
+  }
+  return parsedEnv;
+}
 
 // CloudFlare Worker Durable Object class
 export class DurableObject {
@@ -18,12 +27,14 @@ export class DurableObject {
       userRender,
       codecs,
     };
-    const auth2 = dotenv.parse(envTxt);
+    const auth2 = getEnvConfig(); // Lazily load environment variables
     this.agentMain = new AgentMain(state2, env, auth2);
   }
+
   async fetch(request: Request) {
     return await this.agentMain.fetch(request);
   }
+
   async alarm() {
     return await this.agentMain.alarm();
   }
