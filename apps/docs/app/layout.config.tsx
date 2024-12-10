b0b4fd715e91bb6e781b827570269f928b95fef3
Saad A. Bazaz <saadbazaz@hotmail.com>
2024-12-10 15:34:33 +0500

    docs: extract github repo card to another component

--- apps/docs/app/layout.config.tsx
+++ apps/docs/app/layout.config.tsx
@@ -1,8 +1,10 @@
-import Logo from '@/components/logo';
-import { RootToggle } from 'fumadocs-ui/layouts/docs.client';
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import pkgJson from 'usdk/package.json';
-import Image from 'next/image';
+import Logo from '@/components/logo'
+import { RootToggle } from 'fumadocs-ui/layouts/docs.client'
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
+import pkgJson from 'usdk/package.json'
+import Image from 'next/image'
+import Link from 'next/link'
+import GitHubRepoCard from '@/components/github-repo-card'
 
 /**
  * Shared layout configurations
@@ -13,30 +15,35 @@ import Image from 'next/image';
  */
 export const baseOptions = {
   nav: {
-    title: <Logo className='h-10' />,
-    children: <div className='flex w-full justify-end items-end'>
-      <RootToggle
-        defaultValue={0}
-        options={[
-          {
-            title: `v${pkgJson.version}`,
-            description: 'Latest',
-            url: '',
-            'props': {
-              // @ts-expect-error
-              'href': 'https://www.npmjs.com/package/usdk'
+    title: <Logo className="h-10" />,
+    children: (
+      <div className="flex w-full justify-end items-end">
+        <RootToggle
+          defaultValue={0}
+          options={[
+            {
+              title: `v${pkgJson.version}`,
+              description: 'Latest',
+              url: '',
+              props: {
+                // @ts-expect-error
+                href: 'https://www.npmjs.com/package/usdk'
+              }
             }
-          }
-          // {
-          //   title: 'Folder 2',
-          //   description: 'Pages in folder 2',
-          //   url: '/path/to/page-tree-2',
-          // },
-        ]}
-      />
-    </div>
+            // {
+            //   title: 'Folder 2',
+            //   description: 'Pages in folder 2',
+            //   url: '/path/to/page-tree-2',
+            // },
+          ]}
+        />
+      </div>
+    )
   },
   sidebar: {
+    footer: (
+      <GitHubRepoCard repo={"upstreetAI/upstreet-core"} />
+    ),
     banner: (
       <RootToggle
         defaultValue={0}
@@ -46,27 +53,51 @@ export const baseOptions = {
             title: 'Scillia',
             description: 'Your personal assistant',
             url: '/scillia',
-            icon: <Image alt="product logo" src="/images/general/Scillia_Logo.png" width={30} height={30} className="rounded-md" />,
+            icon: (
+              <Image
+                alt="product logo"
+                src="/images/general/Scillia_Logo.png"
+                width={30}
+                height={30}
+                className="rounded-md"
+              />
+            )
           },
           {
             title: 'Platform',
             description: 'The Agent Platform',
             url: '/platform',
-            icon: <Image alt="product logo" src="/images/general/Platform_Logo.png" width={30} height={30} className="rounded-md" />,
+            icon: (
+              <Image
+                alt="product logo"
+                src="/images/general/Platform_Logo.png"
+                width={30}
+                height={30}
+                className="rounded-md"
+              />
+            )
           },
           {
             title: 'USDK',
             description: 'Upstreet Agents SDK',
             url: '',
-            icon: <Image alt="product logo" src="/images/general/USDK_Logo.png" width={30} height={30} className="rounded-md" />,
-            'props': {
+            icon: (
+              <Image
+                alt="product logo"
+                src="/images/general/USDK_Logo.png"
+                width={30}
+                height={30}
+                className="rounded-md"
+              />
+            ),
+            props: {
               // @ts-expect-error
-              'href': '/'
+              href: '/'
             }
-          },
+          }
         ]}
       />
-    ),
+    )
   }
   // links: [
   //   {
@@ -75,4 +106,4 @@ export const baseOptions = {
   //     active: 'nested-url',
   //   },
   // ],
-};
+}
