1d7735910da461284981d2cfaf3dcb7158d72a31
Abdurrehman Subhani <aarrsubhani@gmail.com>
2024-12-12 13:36:05 +0500

    reply via discord reply action

--- packages/usdk/packages/upstreet-agent/packages/react-agents/components/plugins/discord.tsx
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/components/plugins/discord.tsx
@@ -1,12 +1,16 @@
+import React from 'react';
 import { useContext, useEffect } from 'react';
-import { useAgent, useAuthToken, useConversation } from 'react-agents';
+import { Action, ConversationProvider, useAgent, useAuthToken, useConversation } from 'react-agents';
 import type {
   DiscordArgs,
   DiscordProps,
+  PendingActionEvent,
 } from '../../types';
 import {
   AppContext,
 } from '../../context';
+import dedent from 'dedent';
+import { z } from 'zod';
 
 export const Discord: React.FC<DiscordProps> = (props: DiscordProps) => {
   const {
@@ -45,5 +49,50 @@ export const Discord: React.FC<DiscordProps> = (props: DiscordProps) => {
     conversation,
   ]);
 
-  return null;
+  return (
+    <>
+     <ConversationProvider>
+      <Action
+        type="discordMessageReply"
+        description={dedent`
+          Use this Action to reply to a specific message within a Discord channel or direct message (DM) when you feel it is necessary to respond according to the context of the message.
+          Additionally, use this Action in most cases where you are mentioned using '@' in order to respond back directly to that message.
+        `}
+        schema={
+          z.object({
+          messageReference: z.string(),
+          content: z.string(),
+          })
+        }
+        examples={[
+          {
+            messageReference: '1234567890',
+            content: 'Yea, I got it.',
+          },
+        ]}
+        handler={
+          async (e: PendingActionEvent) => {
+            const {
+              message,
+              agent,
+            } = e.data;
+
+            const {
+              messageReference,
+              content,
+            } = message.args;
+
+            const replyMessage = {
+              content: content,
+              reply: {
+                messageReference: messageReference,
+              }
+            }
+            await agent.say(replyMessage);
+          }
+        }
+      />
+     </ConversationProvider>
+    </>
+  );
 };
