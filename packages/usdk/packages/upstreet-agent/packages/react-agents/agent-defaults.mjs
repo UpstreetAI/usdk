19c1f8bd7302e634ee6069dceb930d09c8125c68
Abdurrehman Subhani <aarrsubhani@gmail.com>
2024-12-20 15:59:57 +0500

    update agent name format

--- packages/usdk/packages/upstreet-agent/packages/react-agents/agent-defaults.mjs
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/agent-defaults.mjs
@@ -1,5 +1,5 @@
 import { workersHost } from './util/endpoints.mjs';
 
-export const getAgentName = (guid) => `user-agent-${guid}`;
+export const getAgentName = (guid) => `${guid}`;
 export const getAgentPublicUrl = (guid) => `https://upstreet.ai/agents/${guid}`;
 export const getCloudAgentHost = (guid) => `https://${getAgentName(guid)}.${workersHost}`;
