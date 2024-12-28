a1413c03e5f497061e9aacef55af8e6db4e4e7d0
Avaer Kazmer <ultimus@gmail.com>
2024-12-28 13:58:13 -0800

    Add @elizaos/core proxy

---
+++ packages/usdk/packages/upstreet-agent/packages/elizaos-core/elizaos-core.ts
@@ -0,0 +1,8 @@
+export const elizaLogger = console.log;
+
+export const Action = {};
+export const HandlerCallback = {};
+export const IAgentRuntime = {};
+export const Memory = {};
+export const Plugin = {};
+export const State = {};
