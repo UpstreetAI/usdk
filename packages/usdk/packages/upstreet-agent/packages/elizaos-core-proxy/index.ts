d36558290a3bb29a2dcb34c2afc60840eae3e8bf
Avaer Kazmer <ultimus@gmail.com>
2024-12-29 02:09:28 -0800

    Add elizaos-core-proxy index

---
+++ packages/usdk/packages/upstreet-agent/packages/elizaos-core-proxy/index.ts
@@ -0,0 +1,25 @@
+export * from "./types.ts";
+export * from "./context.ts";
+export * from "./generation.ts";
+export * from "./memory.ts";
+export * from "./parsing.ts";
+
+//
+
+type State = {};
+type IAgentRuntime = {};
+
+//
+
+export const elizaLogger = console;
+export const settings = {
+  SOL_ADDRESS: 'So11111111111111111111111111111111111111112',
+  SLIPPAGE: '1',
+};
+
+export const Action = {};
+export const HandlerCallback = {};
+export const IAgentRuntime = {};
+export const Memory = {};
+export const Plugin = {};
+export const State = {};
