fd1c9adaebc26d4e63a791246ea79ef38a343dee
Avaer Kazmer <ultimus@gmail.com>
2024-12-29 18:50:04 -0800

    Dead code cleanup

--- packages/usdk/packages/upstreet-agent/packages/react-agents/index.tsx
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/index.tsx
@@ -11,21 +11,6 @@ export * from './components/core/features';
 export * from './components/core/clients';
 export * from './components/core/plugins';
 
-// export * from './components/features/browser';
-// export * from './components/features/discord';
-// export * from './components/features/live-mode';
-// export * from './components/features/media-generator';
-// export * from './components/features/rag-memory';
-// export * from './components/features/rate-limit';
-// export * from './components/features/status-updates';
-// export * from './components/features/store';
-// export * from './components/features/telnyx';
-// export * from './components/features/tts';
-// export * from './components/features/twitter-spaces';
-// export * from './components/features/twitter';
-// export * from './components/features/video-perception';
-// export * from './components/features/auto-task';
-
 export * from './components/features/index';
 
 export * from './components/plugins/index';
