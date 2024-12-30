05ca8c8b1bf514b5ca1e4fb39df5756de2c12cd1
Avaer Kazmer <ultimus@gmail.com>
2024-12-29 20:53:17 -0800

    Hook in gitbook plugin

--- packages/usdk/packages/upstreet-agent/packages/react-agents/components/plugins/index.tsx
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/components/plugins/index.tsx
@@ -5,6 +5,7 @@ import util from 'util';
 import { ThreeDGenerationPlugin } from '@elizaos/plugin-3d-generation';
 import { imageGenerationPlugin } from '@elizaos/plugin-image-generation';
 import { nftGenerationPlugin } from '@elizaos/plugin-nft-generation';
+import gitbookPlugin from '@elizaos/plugin-gitbook';
 import evmPlugin from '@elizaos/plugin-evm';
 import { solanaPlugin } from '@elizaos/plugin-solana';
 import { teePlugin } from '@elizaos/plugin-tee';
@@ -207,6 +208,7 @@ export const plugins = {
   '@elizaos/plugin-3d-generation': pluginWrap(ThreeDGenerationPlugin),
   '@elizaos/plugin-image-generation': pluginWrap(imageGenerationPlugin),
   '@elizaos/plugin-nft-generation': pluginWrap(nftGenerationPlugin),
+  '@elizaos/plugin-gitbook': pluginWrap(gitbookPlugin),
   '@elizaos/plugin-evm': pluginWrap(evmPlugin),
   '@elizaos/plugin-solana': pluginWrap(solanaPlugin),
   '@elizaos/plugin-tee': pluginWrap(teePlugin),
