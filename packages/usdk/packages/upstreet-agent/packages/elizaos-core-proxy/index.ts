852f7a9134f724d6872687da6698578b10655ac5
Avaer Kazmer <ultimus@gmail.com>
2024-12-31 19:31:42 -0800

    Hook in more core imports

--- packages/usdk/packages/upstreet-agent/packages/elizaos-core-proxy/index.ts
+++ packages/usdk/packages/upstreet-agent/packages/elizaos-core-proxy/index.ts
@@ -3,6 +3,12 @@ export * from "./context.ts";
 export * from "./generation.ts";
 export * from "./memory.ts";
 export * from "./parsing.ts";
+export * from "./embedding.ts";
+export * from "./uuid.ts";
+export { default as knowledge } from "./knowledge.ts";
+export * from "./models.ts";
+export * from "./messages.ts";
+export * from "./goals.ts";
 
 export const elizaLogger = console;
 export const settings = {
