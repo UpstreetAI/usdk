f6e30ed5d45e199e8910635316e1f9873c3c87a0
Avaer Kazmer <ultimus@gmail.com>
2024-12-31 20:17:37 -0800

    Fix import.meta.url hack

--- packages/usdk/packages/upstreet-agent/init.ts
+++ packages/usdk/packages/upstreet-agent/init.ts
@@ -3,5 +3,5 @@ globalThis.__filename = 'node.js';
 globalThis.__dirname = '/';
 (globalThis.process as any).version = '20.0.0';
 if (typeof import.meta.url === 'undefined') {
-  import.meta.url = `file://${__filename}`;
+  import.meta.url = `file://localhost/${globalThis.__filename}`;
 }
