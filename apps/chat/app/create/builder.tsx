21b7c26f530a2b477871529c72b5a3bae387846c
ꈤꍟꎭꍟꌗꀤꌗ <55106546+mavisakalyan@users.noreply.github.com>
2025-01-02 22:59:18 +0400

    Update styles and modal

--- apps/chat/app/create/builder.tsx
+++ apps/chat/app/create/builder.tsx
@@ -927,7 +927,7 @@ export default function AgentEditor({
               <div className={cn('flex flex-row h-full items-center justify-center', step !== 2 && 'hidden')}>
                 <div className={`flex flex-col w-[400px] h-full`}>
 
-                  <div className='text-lg font-bold flex flex-row items-center justify-center mb-4'><Icon icon="Chat" className='size-5 mr-2' /> <h2>Test Agent</h2></div>
+                  {/* <div className='text-lg font-bold flex flex-row items-center justify-center mb-4'><Icon icon="Chat" className='size-5 mr-2' /> <h2>Test Agent</h2></div> */}
                   <div className='h-full w-full relative'>
                     <Chat
                       room={room}
@@ -977,7 +977,7 @@ export default function AgentEditor({
 
           {/* assistant */}
 
-          <div className={`flex-col relative right-4 bottom-4 h-full w-[300px] bg-white ${isAssistantVisible ? '' : 'hidden'}`}>
+          <div className={`flex-col relative right-4 bottom-4 h-full w-[300px] ${isAssistantVisible ? '' : ''}`}>
             <div className="flex flex-col flex-1 h-[calc(100%-40px)] overflow-scroll px-1 pb-8 pt-6">
               {builderMessages.map((message, index) => (
                 <div key={index} className={cn('flex gap-2 mb-4 px-4')}>
