6b89f4dd1164599ed22b48c76d8145c0f61336b1
Avaer Kazmer <ultimus@gmail.com>
2025-01-03 17:17:48 -0800

    Update test to load react-agents

--- packages/usdk/test/basic.test.ts
+++ packages/usdk/test/basic.test.ts
@@ -1,7 +1,13 @@
+import { createRoot } from 'react-agents';
+
 // A simple function to test
 export const sum = (a: number, b: number): number => a + b;
 
 // Unit test for the sum function
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('createRoot', () => {
+  expect(createRoot).toBeDefined();
 });
