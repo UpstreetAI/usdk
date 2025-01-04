e587caea6a01e61e9eb91ae3ee4bfbf95f5aa0a1
Avaer Kazmer <ultimus@gmail.com>
2025-01-03 22:35:19 -0800

    Add core ping component

---
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/components/core/ping.tsx
@@ -0,0 +1,25 @@
+//   userId: this.id,
+//   supabase: this.useSupabase(),
+
+import React, { useContext } from 'react';
+import type {
+  PromptProps,
+} from '../../types';
+import {
+  ConversationContext,
+} from '../../context';
+
+export const Prompt: React.FC<PromptProps> = /*memo(*/(props: PromptProps) => {
+  // const agent = useContext(AgentContext);
+  const conversation = useContext(ConversationContext).conversation;
+
+  // const deps = [
+  //   props.children,
+  // ];
+  // agent.useEpoch(deps);
+
+  return <prompt value={{
+    ...props,
+    conversation,
+  }} />;
+}//);
