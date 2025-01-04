23bb5b7a9e6a5db3cba2088d4833a4eface3b904
Avaer Kazmer <ultimus@gmail.com>
2025-01-03 19:41:23 -0800

    Update jest config

--- packages/usdk/jest.config.js
+++ packages/usdk/jest.config.js
@@ -1,15 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
-//   testEnvironment: 'jest-environment-node',
-  transform: {
-    "^.+\\.(ts|tsx)$": [
-      "ts-jest",
-      {
-        useESM: true,
-      },
-    ],
-  },
+  transformIgnorePatterns: [],
   testMatch: [
     '**/test/?(*.)+(spec|test).[jt]s?(x)',
   ],
